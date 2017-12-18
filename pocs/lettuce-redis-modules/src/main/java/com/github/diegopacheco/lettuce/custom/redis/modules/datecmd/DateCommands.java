8dbf024520753cf52ff70428c4615217bca6c15e
Diego Pacheco <diego.pacheco.it@gmail.com>
2017-12-18 15:29:32 -0800

    .

--- pocs/lettuce-redis-modules/src/main/java/com/github/diegopacheco/lettuce/custom/redis/modules/datecmd/DateCommands.java
+++ pocs/lettuce-redis-modules/src/main/java/com/github/diegopacheco/lettuce/custom/redis/modules/datecmd/DateCommands.java
@@ -1,12 +1,12 @@
 package com.github.diegopacheco.lettuce.custom.redis.modules.datecmd;
 
 import io.lettuce.core.dynamic.Commands;
-import io.lettuce.core.dynamic.annotation.Command;
 import io.lettuce.core.dynamic.annotation.CommandNaming;
 import io.lettuce.core.dynamic.annotation.CommandNaming.Strategy;
+import io.lettuce.core.output.StringListOutput;
 
 @CommandNaming(strategy=Strategy.DOT)
 public interface DateCommands extends Commands {
 	//@Command("DP.DATE")
-	public String dpDate();
+	public StringListOutput dpDate();
 }
--- pocs/lettuce-redis-modules/src/main/java/com/github/diegopacheco/lettuce/custom/redis/modules/datecmd/TrullyCustomCmdMain.java
+++ pocs/lettuce-redis-modules/src/main/java/com/github/diegopacheco/lettuce/custom/redis/modules/datecmd/TrullyCustomCmdMain.java
@@ -1,8 +1,13 @@
 package com.github.diegopacheco.lettuce.custom.redis.modules.datecmd;
 
+import java.util.Arrays;
+
 import io.lettuce.core.RedisClient;
 import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.codec.ByteArrayCodec;
+import io.lettuce.core.codec.StringCodec;
 import io.lettuce.core.dynamic.RedisCommandFactory;
+import io.lettuce.core.protocol.LettuceCharsets;
 
 public class TrullyCustomCmdMain {
 
@@ -12,11 +17,11 @@ public class TrullyCustomCmdMain {
 		RedisClient redisClient = RedisClient.create("redis://localhost:6379/0");
 		StatefulRedisConnection connection = redisClient.connect();
 
-		RedisCommandFactory cmdFactory = new RedisCommandFactory(connection);
+		RedisCommandFactory cmdFactory = new RedisCommandFactory(connection, Arrays.asList(new ByteArrayCodec(), new StringCodec(LettuceCharsets.UTF8)));
 		DateCommands dateCmd = cmdFactory.getCommands(DateCommands.class);
 
 		//RedisFuture<String> aResult = dateCmd.dpDate();
-		String aResult = dateCmd.dpDate();
+		Object aResult = dateCmd.dpDate();
 		System.out.println("Real Custom Command[dp.DATE]: " + aResult);
 
 		connection.close();
