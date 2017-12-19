6658f42f626f90410c8b97ca3c0a94dd7b299cfe
Diego Pacheco <diego.pacheco.it@gmail.com>
2017-12-18 16:25:17 -0800

    Custom redis-module workign with Lettuce

--- pocs/lettuce-redis-modules/src/main/java/com/github/diegopacheco/lettuce/custom/redis/modules/datecmd/TrullyCustomCmdMain.java
+++ pocs/lettuce-redis-modules/src/main/java/com/github/diegopacheco/lettuce/custom/redis/modules/datecmd/TrullyCustomCmdMain.java
@@ -2,14 +2,35 @@ package com.github.diegopacheco.lettuce.custom.redis.modules.datecmd;
 
 import io.lettuce.core.ReadFrom;
 import io.lettuce.core.RedisClient;
+import io.lettuce.core.RedisFuture;
 import io.lettuce.core.RedisURI;
+import io.lettuce.core.api.async.RedisAsyncCommands;
+import io.lettuce.core.codec.RedisCodec;
+import io.lettuce.core.codec.StringCodec;
 import io.lettuce.core.codec.Utf8StringCodec;
-import io.lettuce.core.dynamic.RedisCommandFactory;
 import io.lettuce.core.masterslave.MasterSlave;
 import io.lettuce.core.masterslave.StatefulRedisMasterSlaveConnection;
+import io.lettuce.core.output.StatusOutput;
+import io.lettuce.core.protocol.CommandArgs;
+import io.lettuce.core.protocol.LettuceCharsets;
+import io.lettuce.core.protocol.ProtocolKeyword;
 
 public class TrullyCustomCmdMain {
+	
+	public enum DP implements ProtocolKeyword{
+		DATE;
 
+	    public final byte[] bytes;
+
+	    private DP() {
+	        bytes = "dp.DATE".getBytes(LettuceCharsets.ASCII);
+	    }
+	    @Override
+	    public byte[] getBytes() {
+	        return bytes;
+	    }
+	}
+	
 	public static void main(String[] args) throws Throwable {
 		
 		RedisClient redisClient = RedisClient.create();
@@ -17,11 +38,14 @@ public class TrullyCustomCmdMain {
 		StatefulRedisMasterSlaveConnection<String, String> connection = MasterSlave.connect(redisClient, new Utf8StringCodec(),RedisURI.create("redis://localhost"));
 		connection.setReadFrom(ReadFrom.MASTER_PREFERRED);
 		
-		RedisCommandFactory cmdFactory = new RedisCommandFactory(connection);
-		DateCommands dateCmd = cmdFactory.getCommands(DateCommands.class);
-
-		Object aResult = dateCmd.dpDate("s");
-		System.out.println("Real Custom Command[dp.DATE]: " + aResult);
+		RedisAsyncCommands<String, String> ayncCmds = connection.async();
+		RedisCodec<String, String> codec = StringCodec.UTF8;
+		
+		RedisFuture<String> result = ayncCmds.dispatch(DP.DATE, 
+														new StatusOutput<>(codec),
+														new CommandArgs<>(codec).add(""));
+		 
+		System.out.println("Real Custom Command[dp.DATE]: " + result.get());
 		
 		connection.close();
 		redisClient.shutdown();
