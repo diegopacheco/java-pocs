16be80636b35ec1a69fb6372ca9180618067a925
Diego Pacheco <diego.pacheco.it@gmail.com>
2018-02-19 20:22:02 -0300

    Added Dispatch

--- pocs/lettuce-codecs/src/main/java/com/github/diegopacheco/lettuce/codecs/main/MainLettuceCodesWirteAppend.java
+++ pocs/lettuce-codecs/src/main/java/com/github/diegopacheco/lettuce/codecs/main/MainLettuceCodesWirteAppend.java
@@ -7,17 +7,33 @@ import static com.github.diegopacheco.lettuce.codecs.CodecUtils.newEncodingMetho
 import static com.github.diegopacheco.lettuce.codecs.CodecUtils.printByteArray;
 
 import java.nio.ByteBuffer;
-import java.util.List;
 
 import com.github.diegopacheco.lettuce.codecs.CodecUtils;
 import com.lambdaworks.redis.RedisAsyncConnection;
 import com.lambdaworks.redis.RedisClient;
 import com.lambdaworks.redis.RedisFuture;
-import com.lambdaworks.redis.ScriptOutputType;
 import com.lambdaworks.redis.codec.ByteArrayCodec;
 import com.lambdaworks.redis.codec.RedisCodec;
+import com.lambdaworks.redis.output.StatusOutput;
+import com.lambdaworks.redis.protocol.CommandArgs;
+import com.lambdaworks.redis.protocol.LettuceCharsets;
+import com.lambdaworks.redis.protocol.ProtocolKeyword;
 
 public class MainLettuceCodesWirteAppend {
+	
+	static enum HP implements ProtocolKeyword {
+			APPEND;
+	    public final byte[] bytes;
+		
+	    private HP() {
+	        bytes = "HP.HAPPEND".getBytes(LettuceCharsets.UTF8);
+	    }
+	    @Override
+	    public byte[] getBytes() {
+	        return bytes;
+	    }
+	};
+	
 	public static void main(String[] args) throws Throwable {
 		
 		RedisCodec<byte[], byte[]> codec = new ByteArrayCodec();
@@ -31,10 +47,13 @@ public class MainLettuceCodesWirteAppend {
 		RedisFuture<Boolean> cmd2 = connection.hset(newEncodingMethod("myhash2"), newEncodingMethod("age"),newEncodingMethod(new String("3")));
 		System.out.println("string [myhash2] hset " + cmd2.get());
 		
-		//RedisFuture<List<String>> cmdAppend = connection.eval("return redis.call('HP.HAPPEND', KEYS[1],'age','" + newEncodingMethod("3") + "')", ScriptOutputType.MULTI, encodeStr(new String("myhash1")));
-		//System.out.println("string [myhash1] HP.APPEND " + cmdAppend.get());
+		RedisFuture<String> cmdAppend = connection.dispatch(HP.APPEND,new StatusOutput<>(codec),new CommandArgs<>(codec).add("myhash1").add("age").add(3L));
+		System.out.println("string [myhash1] HP.APPEND " + cmdAppend.get());
 		
-		RedisFuture<List<String>> cmdAppend2 = connection.eval("return redis.call('HP.HAPPEND', KEYS[1],'age','3')", ScriptOutputType.MULTI, encodeStr(new String("myhash2")));
+		//RedisFuture<List<String>> cmdAppend2 = connection.eval("return redis.call('HP.HAPPEND', KEYS[1],'age','3')", ScriptOutputType.MULTI, encodeStr(new String("myhash2")));
+		//System.out.println("string [myhash2] HP.APPEND " + cmdAppend2.get());
+		
+		RedisFuture<String> cmdAppend2 = connection.dispatch(HP.APPEND,new StatusOutput<>(codec),new CommandArgs<>(codec).add("myhash2").add("age").add(3L));
 		System.out.println("string [myhash2] HP.APPEND " + cmdAppend2.get());
 		
 		byte[] result = connection.hget(encodeStr("myhash1"), encodeStr("age")).get();
