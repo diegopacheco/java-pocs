a922fcdae578171b58bbc80daafe81cf8c2eaae1
Diego Pacheco <diego.pacheco.it@gmail.com>
2018-03-28 16:32:04 -0300

    working with rxJava 2

--- pocs/rxjava-local-continnuations/src/main/java/com/github/diegopacheco/rx/java/local/RxTLMain.java
+++ pocs/rxjava-local-continnuations/src/main/java/com/github/diegopacheco/rx/java/local/RxTLMain.java
@@ -1,7 +1,10 @@
 package com.github.diegopacheco.rx.java.local;
 
-import rx.Observable;
-import rx.plugins.RxJavaHooks;
+import io.reactivex.Observable;
+import io.reactivex.plugins.RxJavaPlugins;
+
+//import rx.Observable;
+//import rx.plugins.RxJavaHooks;
 
 /**
  * RxJavaHooks allow you to create "interceptors" on Rx but was deprecated on 2.0 - only works on 1.0.
@@ -14,6 +17,8 @@ public class RxTLMain {
 	
 	public static void main(String[] args) {
 		
+		// RX 1.X
+		/*
 		RxJavaHooks.setOnObservableCreate(o -> { 
 	    System.out.println("Creating " + o.getClass());
 	    return o; 
@@ -27,6 +32,17 @@ public class RxTLMain {
 		} finally {
 	    RxJavaHooks.reset();
 		}
+		*/
+		
+		// RX 2.X
+		
+		RxJavaPlugins.reset();
+		RxJavaPlugins.setErrorHandler( (Throwable e) ->  System.out.println("Hacked " + e) );
+		
+		Observable.error(new RuntimeException())
+		.map(v -> "" + v)
+		.doOnError( e -> System.out.println("Deu ruim! " + e))
+		.subscribe(System.out::println);
 		
 	}
 }
