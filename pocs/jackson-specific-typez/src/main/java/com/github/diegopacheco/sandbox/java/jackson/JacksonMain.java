b9df62aecb0dba5f5eb70448055239f486e6fa16
Diego Pacheco <diego.pacheco.it@gmail.com>
2018-04-23 18:27:46 -0300

    Jakcson POC

---
+++ pocs/jackson-specific-typez/src/main/java/com/github/diegopacheco/sandbox/java/jackson/JacksonMain.java
@@ -0,0 +1,29 @@
+package com.github.diegopacheco.sandbox.java.jackson;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JacksonMain {
+	public static void main(String[] args) throws Throwable {
+		
+		Map<String,String> family = new ConcurrentHashMap<>();
+		family.put("Gandalfy", "Cat");
+		family.put("Melina",   "Cat");
+		
+		SimplePojo s1 = SimplePojo.getInstance();
+		s1.setAge(33);
+		s1.setName("Diego");
+		s1.setFamily(family);
+		
+		ObjectMapper mapper = new ObjectMapper();
+		String s1Json = mapper.writeValueAsString(s1);
+		System.out.println("Simple Pojo as String : \n " + s1);
+		System.out.println("Simple Pojo as Json   : \n " + s1Json);
+		
+		SimplePojo s2 = mapper.readValue(s1Json,SimplePojo.class);
+		System.out.println("Simple Pojo2 as String : \n " + s2);
+		
+	}
+}
---
+++ pocs/jackson-specific-typez/src/main/java/com/github/diegopacheco/sandbox/java/jackson/SimplePojo.java
@@ -0,0 +1,49 @@
+package com.github.diegopacheco.sandbox.java.jackson;
+
+import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+public class SimplePojo {
+	
+	private static SimplePojo instance;
+	
+	private String name;
+	private Integer age;
+	private Map<String,String> family;
+	
+	private SimplePojo() {}
+	
+	@JsonCreator
+	public static synchronized SimplePojo getInstance() {
+		if(instance==null) {
+			instance = new SimplePojo();
+		}
+		return instance;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public Map<String, String> getFamily() {
+		return family;
+	}
+	public void setFamily(Map<String, String> family) {
+		this.family = family;
+	}
+
+	@Override
+	public String toString() {
+		return "SimplePojo [name=" + name + ", age=" + age + ", family=" + family + "]";
+	}
+	
+}
