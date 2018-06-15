59e796604017ff77289bd9ceb32a02c6eb19dabc
Diego Pacheco <diego.pacheco.it@gmail.com>
2018-06-14 19:42:27 -0700

    ideas for generic forklifter in v3

--- pocs/cass-dual-writer/src/main/java/com/github/diegopacheco/sandbox/java/cass/dual/writer/dao/CassDAO.java
+++ pocs/cass-dual-writer/src/main/java/com/github/diegopacheco/sandbox/java/cass/dual/writer/dao/CassDAO.java
@@ -5,4 +5,8 @@ import java.util.List;
 public interface CassDAO {
 	public List<String> getAllData();
 	public void insertData(String key, String value);
+	
+	// if this guy return the table metadata we can do it.
+	// also need proper object for key, and linkedlist with all values in insertion order
+	// so there are method to get the key and get values to that key.
 }
