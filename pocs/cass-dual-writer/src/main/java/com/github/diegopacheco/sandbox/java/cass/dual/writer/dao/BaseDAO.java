0cd77970db4ba08b0df71b3ccf9f43734c3385ce
Diego Pacheco <diego.pacheco.it@gmail.com>
2018-06-18 14:50:45 -0700

    .

--- pocs/cass-dual-writer/src/main/java/com/github/diegopacheco/sandbox/java/cass/dual/writer/dao/BaseDAO.java
+++ pocs/cass-dual-writer/src/main/java/com/github/diegopacheco/sandbox/java/cass/dual/writer/dao/BaseDAO.java
@@ -82,11 +82,9 @@ public class BaseDAO implements BusinessDAO, CassDAO {
 	@Override
 	public ResultSet getReadResultSet() {
 		Statement stmt = new SimpleStatement("SELECT * FROM cluster_test.test");
-		stmt.setFetchSize(100);
-		
+		//stmt.setFetchSize(100);
 		Session session = getSession(cluster);
 		ResultSet rs = session.execute(stmt);
-		
 		return rs;
 	}
 	
