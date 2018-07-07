1fbf8c7015b7fa0bf05016297af9c1f9b2a2ddea
Diego Pacheco <diego.pacheco.it@gmail.com>
2018-07-06 18:49:07 -0700

    .

--- pocs/gradle-fetch-from-github/src/main/java/com/github/diegopacheco/sandbox/java/sample/MyDateService.java
+++ pocs/gradle-fetch-from-github/src/main/java/com/github/diegopacheco/sandbox/java/sample/MyDateService.java
@@ -2,8 +2,17 @@ package com.github.diegopacheco.sandbox.java.sample;
 
 import java.util.Date;
 
+import com.github.diegopacheco.sandbox.java.arch.annotations.arch.Architecture;
+import com.github.diegopacheco.sandbox.java.arch.annotations.arch.Bound;
+import com.github.diegopacheco.sandbox.java.arch.annotations.arch.Profile;
+import com.github.diegopacheco.sandbox.java.arch.annotations.arch.ServiceNature;
 import com.github.diegopacheco.sandbox.java.arch.annotations.code.Refactoring;
 
+@ServiceNature(
+		bound=Bound.LATENCY,
+		profile=Profile.FAST_PIPELINE,
+		main_architecture=Architecture.REQUEST_DRIVEN
+)
 public class MyDateService {
 
 	@Refactoring(todo="This should not return void",
