2bf7ab5a5132fd0eedfdbfcb787bec607756ed4a
Diego Pacheco <diego.pacheco.it@gmail.com>
2019-02-18 23:39:51 -0300

    .

--- pocs/simple-sandbox-boot-1x/src/main/java/com/github/diegopacheco/spring/boot/x/microservice/simplesandbox/IndexController.java
+++ pocs/simple-sandbox-boot-1x/src/main/java/com/github/diegopacheco/spring/boot/x/microservice/simplesandbox/IndexController.java
@@ -1,9 +1,11 @@
 package com.github.diegopacheco.spring.boot.x.microservice.simplesandbox;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 public class IndexController {
 
 	@Autowired
