f1b7fc315148dd0238c5e6307cb2258bbaced744
Diego Pacheco <diego.pacheco.it@gmail.com>
2019-02-18 22:43:55 -0300

    spring boot 1x microservice

---
+++ pocs/simple-sandbox-boot-1x/src/main/java/com/github/diegopacheco/spring/boot/x/microservice/simplesandbox/SimpleSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.github.diegopacheco.spring.boot.x.microservice.simplesandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSandboxApplication.class, args);
+	}
+
+}
