eb4b7e77e06e7599a6fd4c21fde2b6eb5efe83bb
Diego Pacheco <diego.pacheco.it@gmail.com>
2019-02-19 00:56:04 -0300

    .

--- pocs/simple-sandbox-boot-1x/src/main/java/com/github/diegopacheco/spring/boot/x/microservice/simplesandbox/GitHubFeignApi.java
+++ pocs/simple-sandbox-boot-1x/src/main/java/com/github/diegopacheco/spring/boot/x/microservice/simplesandbox/GitHubFeignApi.java
@@ -1,28 +1,19 @@
 package com.github.diegopacheco.spring.boot.x.microservice.simplesandbox;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name="gitHubfeignApi", url="https://api.github.com", fallback = GitHubFeignApi.GithubClientFallback.class)
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+@FeignClient(name = "gitHubfeignApi", url = "https://api.github.com") 
 public interface GitHubFeignApi {
-	
-  @RequestMapping(path = "/users/{username}/repos", method = RequestMethod.GET)
-  public List<GithubRepo> getUserRepos(@PathVariable(value = "username") String username);
-	
-  @Component
-  public static class GithubClientFallback implements GitHubFeignApi {
-      @Override
-      public List<GithubRepo> getUserRepos(String username){
-      	  GithubRepo gr = new GithubRepo();
-      	  gr.setFull_name("Fallback: Some dummy response!");
-          return Arrays.asList(gr);
-      }
-  }
-  
+
+	@RequestMapping(path = "/users/{username}/repos", method = RequestMethod.GET)
+	@HystrixCommand
+	public List<GithubRepo> getUserRepos(@PathVariable(value = "username") String username);
+
 }
