6a104c4a3f9c9146e0dde96511373aea25905a1d
Diego Pacheco <diego.pacheco.it@gmail.com>
2019-02-28 08:53:05 -0300

    GraphQL java simple

---
+++ pocs/simple-graphql-java/src/main/java/com/github/diegopacheco/sandbox/java/graphql/GraphQlMain.java
@@ -0,0 +1,34 @@
+package com.github.diegopacheco.sandbox.java.graphql;
+
+import graphql.ExecutionResult;
+import graphql.GraphQL;
+import graphql.schema.GraphQLSchema;
+import graphql.schema.StaticDataFetcher;
+import graphql.schema.idl.RuntimeWiring;
+import graphql.schema.idl.SchemaGenerator;
+import graphql.schema.idl.SchemaParser;
+import graphql.schema.idl.TypeDefinitionRegistry;
+import static graphql.schema.idl.RuntimeWiring.newRuntimeWiring;
+
+public class GraphQlMain {
+
+    public static void main(String[] args) {
+        String schema = "type Query{hello: String} schema{query: Query}";
+
+        SchemaParser schemaParser = new SchemaParser();
+        TypeDefinitionRegistry typeDefinitionRegistry = schemaParser.parse(schema);
+
+        RuntimeWiring runtimeWiring = newRuntimeWiring()
+                .type("Query", builder -> builder.dataFetcher("hello", new StaticDataFetcher("world")))
+                .build();
+
+        SchemaGenerator schemaGenerator = new SchemaGenerator();
+        GraphQLSchema graphQLSchema = schemaGenerator.makeExecutableSchema(typeDefinitionRegistry, runtimeWiring);
+
+        GraphQL build = GraphQL.newGraphQL(graphQLSchema).build();
+        ExecutionResult executionResult = build.execute("{hello}");
+
+        System.out.println(executionResult.getData().toString());
+    }
+    
+}
