83f2f89e47d07ce48137cb0dee6ab75a88e471b3
Diego Pacheco <diego.pacheco.it@gmail.com>
2019-03-09 14:00:21 -0300

    GraalVM Gradle fun

---
+++ pocs/graalvm-simple-jar/src/main/java/com/github/diegopacheco/sandbox/java/graalvm/SimpleConsoleApp.java
@@ -0,0 +1,7 @@
+package com.github.diegopacheco.sandbox.java.graalvm;
+
+public class SimpleConsoleApp{
+    public static void main(String[] args) {
+        System.out.println("hello, world from GraalVM!");
+    }
+}
