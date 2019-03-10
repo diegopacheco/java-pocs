521d4b441249d55230069d3fa1e6034ae7df4be5
Diego Pacheco <diego.pacheco.it@gmail.com>
2019-03-10 02:34:02 -0300

    .

--- pocs/graalvm-bench/build.sh
+++ pocs/graalvm-bench/build.sh
@@ -2,4 +2,5 @@
 
 echo "Gradle build. Building Native Image."
 ./gradlew clean build
+mkdir build/graal/
 $GRAALVM_HOME/bin/native-image -jar build/libs/graalvm-bench.jar -H:Name=build/graal/topten-native --enable-all-security-services
