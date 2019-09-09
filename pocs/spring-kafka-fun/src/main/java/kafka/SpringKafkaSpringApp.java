a0fbafa49e331e63b6238bbf26645063ebd4f9fd
Diego Pacheco <diego.pacheco.it@gmail.com>
2019-09-09 13:49:39 -0300

    Spring kafka poc

---
+++ pocs/spring-kafka-fun/src/main/java/kafka/SpringKafkaSpringApp.java
@@ -0,0 +1,32 @@
+package kafka;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@SpringBootApplication
+public class SpringKafkaSpringApp implements CommandLineRunner {
+
+  private final KafkaTemplate<String, String> kafkaTemplate;
+
+  @Autowired
+  public SpringKafkaSpringApp(KafkaTemplate<String, String> kafkaTemplate) {
+    this.kafkaTemplate = kafkaTemplate;
+  }
+
+  @KafkaListener(topics = "myTopic")
+  public void processMessage(String content) {
+    System.out.println(content);
+  }
+
+  @Override
+  public void run(String... args) {
+    System.out.println("Spring app Running... " + kafkaTemplate);
+    kafkaTemplate.send("myTopic", "hey how is it going? ");
+  }
+
+}
