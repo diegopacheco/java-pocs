6ec13e1c5ece1e6abf7f253491f75c62bbfb3516
Diego Pacheco <diego.pacheco.it@gmail.com>
2019-09-24 19:58:35 -0300

    junit 5 samples

--- pocs/junit5-super-fun/src/test/java/tests/TestSample.java
+++ pocs/junit5-super-fun/src/test/java/tests/TestSample.java
@@ -1,8 +1,11 @@
 package tests;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
@@ -26,6 +29,18 @@ public class TestSample {
     public void shouldBeQuickOrBeDead(){
         System.out.println("almost got me");
         assertSame(1, 1);
+    }
+
+    @BeforeAll
+    public static void runMeAllTheTime(){
+        System.out.println("Im annoying I know :D ");
+    }
+
+    @Order(1)
+    @Test
+    public void meFirst(){
+        System.out.println("There we go");
+        assertNotNull("ok");
     }
 
 }
