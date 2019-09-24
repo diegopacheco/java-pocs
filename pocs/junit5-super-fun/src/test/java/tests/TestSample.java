04c705c3b8f01f64a2a83634ae6bed5ba050b16e
Diego Pacheco <diego.pacheco.it@gmail.com>
2019-09-24 19:54:56 -0300

    more anntoation

--- pocs/junit5-super-fun/src/test/java/tests/TestSample.java
+++ pocs/junit5-super-fun/src/test/java/tests/TestSample.java
@@ -1,13 +1,31 @@
 package tests;
 
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 
 public class TestSample {
 
     @RepeatedTest(value = 2)
     public void test2x(){
         System.out.println("ok");
+    }
+
+    @DisplayName("This is a very important test, I need to becareful here!")
+    @Test
+    public void simpleTest(){
+        String myVar = "1";
+        assertSame(myVar, myVar);
+    }
+
+    @Test
+    @Timeout(value = 2)
+    public void shouldBeQuickOrBeDead(){
+        System.out.println("almost got me");
+        assertSame(1, 1);
     }
 
 }
