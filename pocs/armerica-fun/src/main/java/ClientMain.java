92d1af583704cb9105d72ed2cd4e89538e396abf
Diego Pacheco <diego.pacheco.it@gmail.com>
2019-11-27 22:59:11 -0800

    more code with armerica

--- pocs/armerica-fun/src/main/java/ClientMain.java
+++ pocs/armerica-fun/src/main/java/ClientMain.java
@@ -6,6 +6,9 @@ public class ClientMain{
         WebClient webClient = WebClient.of("http://127.0.0.1:8080/");
         AggregatedHttpResponse res1 = webClient.get("/").aggregate().join();
         System.out.println("Result from Server : " + res1.contentUtf8());
+
+        AggregatedHttpResponse res2 = webClient.get("/hello/DiegoPacheco").aggregate().join();
+        System.out.println("Result from Server : " + res2.contentUtf8());
     }
 }
 
--- pocs/armerica-fun/src/main/java/ServerMain.java
+++ pocs/armerica-fun/src/main/java/ServerMain.java
@@ -2,14 +2,26 @@
 import java.util.concurrent.CompletableFuture;
 
 import com.linecorp.armeria.common.HttpResponse;
+import com.linecorp.armeria.common.HttpStatus;
+import com.linecorp.armeria.common.MediaType;
 import com.linecorp.armeria.server.Server;
 import com.linecorp.armeria.server.ServerBuilder;
+import com.linecorp.armeria.server.annotation.Get;
+import com.linecorp.armeria.server.annotation.Param;
  
 public class ServerMain{
     public static void main(String[] args) throws Exception {
         ServerBuilder sb = Server.builder();
         sb.http(8080);
         sb.service("/", (ctx, res) -> HttpResponse.of("Hello, world!"));
+        sb.annotatedService(new Object() {
+            @Get("/hello/{name}")
+            public HttpResponse hello(@Param("name") String name) {
+                return HttpResponse.of(HttpStatus.OK,
+                                       MediaType.PLAIN_TEXT_UTF_8,
+                                       "Hello, %s!", name);
+            }
+        });
         Server server = sb.build();
         CompletableFuture<Void> future = server.start();
         future.join();        
