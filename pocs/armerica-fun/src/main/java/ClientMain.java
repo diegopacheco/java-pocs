c01d1d8d29f6c2db506c59cf4ad4e2908407543a
Diego Pacheco <diego.pacheco.it@gmail.com>
2019-11-27 22:46:03 -0800

    working poc

--- pocs/armerica-fun/src/main/java/ClientMain.java
+++ pocs/armerica-fun/src/main/java/ClientMain.java
@@ -1,21 +1,11 @@
 import com.linecorp.armeria.client.WebClient;
 import com.linecorp.armeria.common.AggregatedHttpResponse;
-import com.linecorp.armeria.common.HttpHeaderNames;
-import com.linecorp.armeria.common.HttpMethod;
-import com.linecorp.armeria.common.RequestHeaders;
 
 public class ClientMain{
     public static void main(String[] args) throws Exception {
-        WebClient webClient = WebClient.of("http://example.com/");
- 
-        AggregatedHttpResponse res1 = webClient.get("/foo/bar.txt").aggregate().join();
-        
-        RequestHeaders getJson = RequestHeaders.of(HttpMethod.GET, "/foo/bar.json",
-                                                HttpHeaderNames.ACCEPT, "application/json");
-        
-        AggregatedHttpResponse res2 = webClient.execute(getJson).aggregate().join();
-        
-        AggregatedHttpResponse res3 = webClient.post("/upload", "{ \"foo\": \"bar\" }")
-                                            .aggregate().join();    
+        WebClient webClient = WebClient.of("http://127.0.0.1:8080/");
+        AggregatedHttpResponse res1 = webClient.get("/").aggregate().join();
+        System.out.println("Result from Server : " + res1.contentUtf8());
     }
 }
+
