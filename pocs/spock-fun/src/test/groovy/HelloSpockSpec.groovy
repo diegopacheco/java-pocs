fde70407582976743c76fe7a9bb73d4bccb92083
Diego Pacheco <diego.pacheco.it@gmail.com>
2020-01-31 14:37:48 -0300

    spok fun BDD

--- pocs/spock-fun/src/test/groovy/HelloSpockSpec.groovy
+++ pocs/spock-fun/src/test/groovy/HelloSpockSpec.groovy
@@ -11,4 +11,33 @@ class HelloSpockSpec extends Specification {
     "Kirk"   | 4
     "Scotty" | 6
   }
+
+  def "minimum of #a and #b is #c"() {
+    expect:
+    Math.min(a, b) == c
+
+    where:
+    a | b || c
+    3 | 7 || 3
+    5 | 4 || 4
+    9 | 9 || 9
+  }
+
+  def "#person.name is a #sex.toLowerCase() person"() {
+    expect:
+    person.getSex() == sex
+
+    where:
+    person                    || sex
+    new Person(name: "Fred")  || "Male"
+    new Person(name: "Wilma") || "Female"
+  }
+
+  static class Person {
+    String name
+    String getSex() {
+      name == "Fred" ? "Male" : "Female"
+    }
+  }  
+
 }
