3ac492bfc43ff9fff144960fcdef04c1fff0b5d7
diegopacheco <diego.pacheco.it@gmail.com>
2020-02-22 23:14:45 -0300

    spring boot 2

---
+++ pocs/spring2-web-netty/src/main/java/com/github/diegopacheco/spring2/netty/web/fun/spring2webnetty/Spring2WebNettyApplication.java
@@ -0,0 +1,13 @@
+package com.github.diegopacheco.spring2.netty.web.fun.spring2webnetty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring2WebNettyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring2WebNettyApplication.class, args);
+	}
+
+}
---
+++ pocs/spring2-web-netty/src/test/java/com/github/diegopacheco/spring2/netty/web/fun/spring2webnetty/Spring2WebNettyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.diegopacheco.spring2.netty.web.fun.spring2webnetty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring2WebNettyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
