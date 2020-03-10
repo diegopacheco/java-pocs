6bf15370f7695a0edbc3f36e0ab4630186792dc2
diegopacheco <diego.pacheco.it@gmail.com>
2020-03-10 11:49:40 -0300

    spring retry fun

--- pocs/spring-retry-fun/src/main/java/com/Application.java
+++ pocs/spring-retry-fun/src/main/java/com/Application.java
@@ -6,7 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.RetryCallback;
+import org.springframework.retry.RetryContext;
 import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.retry.policy.TimeoutRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
 
 @Configuration
 @EnableRetry
@@ -26,6 +30,18 @@ public class Application implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         service().service();
+    
+        RetryTemplate template = new RetryTemplate();
+        TimeoutRetryPolicy policy = new TimeoutRetryPolicy();
+        policy.setTimeout(30000L);
+        template.setRetryPolicy(policy);
+        
+        String result = template.execute(new RetryCallback<String,Exception>(){
+            public String doWithRetry(RetryContext context) {
+                return "Works";
+            }
+        }); 
+        System.out.println("works: " + result);
     }
 
 }
