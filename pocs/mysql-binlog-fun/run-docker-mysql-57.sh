963d7c575be42de49e53a8652a08534bd902dfde
diegopacheco <diego.pacheco.it@gmail.com>
2020-04-20 12:51:46 -0300

    mysql bin log

--- pocs/mysql-binlog-fun/run-docker-mysql-57.sh
+++ pocs/mysql-binlog-fun/run-docker-mysql-57.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-docker run --rm -d -e MYSQL_ROOT_PASSWORD=pass -p 3325:3306 --name mysql_test mysql:5.7
+docker run --rm -d -v /var/lib/mysql:/var/lib/mysql \
+  --user=root \
+  -e MYSQL_ROOT_PASSWORD=pass -p 3325:3306 --name mysql_test mysql:5.7
 echo "connect via client: "
 echo "mysql -uroot -ppass -h127.0.0.1 -P3325"
