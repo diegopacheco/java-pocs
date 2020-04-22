432d003acc517d3664e0bc9ad2c4e13ca61ce61d
diegopacheco <diego.pacheco.it@gmail.com>
2020-04-22 11:24:28 -0300

    working

--- pocs/security-kms-fun/src/main/java/Main.java
+++ pocs/security-kms-fun/src/main/java/Main.java
@@ -1,19 +1,16 @@
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.encryptionsdk.AwsCrypto;
 import com.amazonaws.encryptionsdk.CryptoResult;
+import com.amazonaws.encryptionsdk.MasterKey;
 import com.amazonaws.encryptionsdk.kms.KmsMasterKey;
 import com.amazonaws.encryptionsdk.kms.KmsMasterKeyProvider;
 import com.amazonaws.regions.Regions;
-import com.amazonaws.services.apigateway.model.EndpointConfiguration;
+import com.amazonaws.services.kms.AWSKMSClientBuilder;
 
-import java.util.Collections;
-import java.util.Map;
+import java.util.Base64;
 
 
 public class Main {
-
-  private static BasicAWSCredentials awsCreds = new BasicAWSCredentials("0", "0");
 
   // ./create-key.sh
   private static String keyArn = "arn:aws:kms:us-east-1:000000000000:key/d2baffab-0150-4604-aec1-8714602f551c";
@@ -24,14 +21,18 @@ public class Main {
     final AwsCrypto crypto = new AwsCrypto();
 
     final KmsMasterKeyProvider prov = KmsMasterKeyProvider.builder()
-                                      .withDefaultRegion(Regions.US_EAST_1.getName())
-                                      .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
-                                      .withKeysForEncryption(keyArn).build();
+          .withCustomClientFactory(
+                  (regionName ->
+                          AWSKMSClientBuilder.standard().withEndpointConfiguration(
+                              new AwsClientBuilder.EndpointConfiguration("http://localhost:4566",Regions.US_EAST_1.getName())
+                          ).build()
+                  )
+          ).withKeysForEncryption(keyArn).build();
 
-    final String ciphertext = new String(crypto.encryptData(prov,data.getBytes()).getResult());
-    System.out.println("Ciphertext: " + ciphertext);
+    final CryptoResult<byte[], KmsMasterKey> result = crypto.encryptData(prov,data.getBytes());
+    System.out.println("Ciphertext as Base64: " + Base64.getEncoder().encodeToString(result.getResult()));
 
-    final CryptoResult<byte[], KmsMasterKey> decryptResult = crypto.decryptData(prov,ciphertext.getBytes());
-    System.out.println("Decrypted: " + decryptResult.getResult());
+    final CryptoResult<byte[], KmsMasterKey> decryptResult = crypto.decryptData(prov,result.getResult());
+    System.out.println("Decrypted: " + new String(decryptResult.getResult(),"UTF-8"));
   }
 }
