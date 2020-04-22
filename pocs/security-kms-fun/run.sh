b13cb9e3e8304fd22d17347873c32629286ff499
diegopacheco <diego.pacheco.it@gmail.com>
2020-04-22 14:04:34 -0300

    kms + java

--- pocs/security-kms-fun/run.sh
+++ pocs/security-kms-fun/run.sh
@@ -5,5 +5,6 @@ echo "Creating KMS KEY..."
 export KMS_KEY=$(./create-key.sh | jq ".KeyMetadata.Arn")
 echo "KEY ARN: $KMS_KEY"
 sleep 3
+echo "KMS Key: $(awslocal kms list-keys | grep $KMS_KEY)"
 ./mvnw exec:java \
   -Dexec.mainClass="Main" -Dexec.args="" -DKMS_KEY="$KMS_KEY" -Dexec.classpathScope=runtime
