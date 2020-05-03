f6b6269a5d594fd2c7bc8c664c96698d9bbfae4e
diegopacheco <diego.pacheco.it@gmail.com>
2020-05-02 21:47:04 -0300

    simple stream

--- pocs/simple-streams/src/main/java/Main.java
+++ pocs/simple-streams/src/main/java/Main.java
@@ -1,7 +1,6 @@
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
 
 public class Main {
     public static void main(String args[]) {
