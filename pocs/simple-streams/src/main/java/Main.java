333456bc47abbfc58acae1b03fe1c1792cf9d179
diegopacheco <diego.pacheco.it@gmail.com>
2020-05-02 21:46:21 -0300

    simple stream

--- pocs/simple-streams/src/main/java/Main.java
+++ pocs/simple-streams/src/main/java/Main.java
@@ -1,12 +1,16 @@
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 public class Main {
     public static void main(String args[]) {
-      
-      List<String> people = Arrays.asList(new String[] { "Adam", "Allan","Anna","John","Paul","Peter" })
+
+      List<String> rawValues = Arrays.asList(new String[] { "Adam", "Allan","Anna","John","Paul","Peter" });
+      List<String> people =  rawValues
                             .stream()
+                            .distinct()
+                            .sorted()
                             .filter( name -> name.startsWith("A"))
                             .map(String::toUpperCase)
                             .limit(2)
