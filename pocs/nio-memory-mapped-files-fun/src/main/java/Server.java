33b3a9ccb1fb8b1157c9901f45cf178fd5ee4e5d
diegopacheco <diego.pacheco.it@gmail.com>
2020-05-14 16:23:28 -0300

    NIO memory mapped files with Java

---
+++ pocs/nio-memory-mapped-files-fun/src/main/java/Server.java
@@ -0,0 +1,28 @@
+import java.io.File;
+import java.nio.CharBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.StandardOpenOption;
+
+public class Server {
+
+    public static void main( String[] args ) throws Throwable {
+
+        File base = new File(".").getAbsoluteFile();
+        File f = new File( base + "/shared-memory-file" );
+
+        FileChannel channel = FileChannel.open( f.toPath(), StandardOpenOption.READ, StandardOpenOption.WRITE, StandardOpenOption.CREATE );
+
+        MappedByteBuffer b = channel.map( FileChannel.MapMode.READ_WRITE, 0, 4096 );
+        CharBuffer charBuf = b.asCharBuffer();
+
+        char[] string = "Hello client\0".toCharArray();
+        charBuf.put( string );
+
+        System.out.println( "Waiting for client." );
+        while( charBuf.get( 0 ) != '\0' );
+        System.out.println( "Finished waiting." );
+
+        System.out.println("FIN.");
+    }
+}
