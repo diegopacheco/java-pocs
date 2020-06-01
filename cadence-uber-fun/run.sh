5643ae7aeb3d7f132ae19ef400188354c10b563f
diegopacheco <diego.pacheco.it@gmail.com>
2020-05-31 23:08:27 -0300

    uver cadence

--- cadence-uber-fun/run.sh
+++ cadence-uber-fun/run.sh
@@ -1,5 +1,5 @@
 
 #!/bin/bash
 
-#./mvnw exec:java -Dexec.mainClass="Main" -Dexec.classpathScope=runtime
-docker run --network=host --rm ubercadence/cli:master --do test-domain workflow start --tasklist HelloWorldTaskList --workflow_type HelloWorld::sayHello --execution_timeout 3600 --input \"World\"
+./mvnw exec:java -Dexec.mainClass="Main" -Dexec.classpathScope=runtime
+#docker run --network=host --rm ubercadence/cli:master --do test-domain workflow start --tasklist HelloWorldTaskList --workflow_type HelloWorld::sayHello --execution_timeout 3600 --input \"World\"
