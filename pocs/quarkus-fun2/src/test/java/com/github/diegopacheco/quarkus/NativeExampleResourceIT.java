8f1e78ea05061c60f78c41a3577c566923d48a65
diegopacheco <diego.pacheco.it@gmail.com>
2020-06-01 23:45:11 -0300

    quarkus fun

---
+++ pocs/quarkus-fun2/src/test/java/com/github/diegopacheco/quarkus/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package com.github.diegopacheco.quarkus;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
