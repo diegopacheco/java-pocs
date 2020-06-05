d3b9a44eef4898590742ab7e595a1c58d346c128
diegopacheco <diego.pacheco.it@gmail.com>
2020-06-05 11:57:47 -0300

    RSA

--- pocs/security-generate-key-java/src/main/java/Main.java
+++ pocs/security-generate-key-java/src/main/java/Main.java
@@ -1,17 +1,28 @@
 import javax.crypto.SecretKey;
+import java.security.KeyPair;
 
 public class Main{
   public static void main(String args[]){
     SecretKey keyGen    = SecurityService.generateKey();
     SecretKey keyImport = SecurityService.importKey(new byte[]{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0});
+    KeyPair keyRSA      = SecurityService.generateAsymmetricKey();
     workWithKey(keyGen);
     workWithKey(keyImport);
+    workWithKeyRSA(keyRSA);
   }
 
   private static void workWithKey(SecretKey key) {
     System.out.println("****** Using key: " + key + " @ " + key.getAlgorithm());
     String cipher = SecurityService.enc("This is a secret msg by Diego.",key);
     String text   = SecurityService.dec(cipher,key);
+    System.out.println("Cipher: " + cipher);
+    System.out.println("Text  : " + text);
+  }
+
+  private static void workWithKeyRSA(KeyPair key) {
+    System.out.println("****** Using key: " + key + " @ " + key.getPublic());
+    String cipher = SecurityService.encRSA("This is a secret msg by Diego.",key);
+    String text   = SecurityService.decRSA(cipher,key);
     System.out.println("Cipher: " + cipher);
     System.out.println("Text  : " + text);
   }
--- pocs/security-generate-key-java/src/main/java/SecurityService.java
+++ pocs/security-generate-key-java/src/main/java/SecurityService.java
@@ -3,6 +3,8 @@ import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
 import java.security.SecureRandom;
 import java.util.Base64;
 
@@ -10,6 +12,17 @@ public class SecurityService {
 
     public static SecretKey importKey(byte[] data){
         return new SecretKeySpec(data, "AES");
+    }
+
+    public static KeyPair generateAsymmetricKey(){
+        try{
+            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+            keyGen.initialize(4096);
+            KeyPair pair = keyGen.generateKeyPair();
+            return pair;
+        }catch(Exception e){
+            throw new RuntimeException(e);
+        }
     }
 
     public static SecretKey generateKey(){
@@ -46,6 +59,27 @@ public class SecurityService {
             IvParameterSpec ivspec = new IvParameterSpec(iv);
             Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
             cipher.init(Cipher.DECRYPT_MODE, key,ivspec);
+            return new String(cipher.doFinal(Base64.getDecoder().decode(text)));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String encRSA(String data, KeyPair key){
+        try{
+            Cipher cipher = Cipher.getInstance("RSA");
+            cipher.init(Cipher.ENCRYPT_MODE, key.getPrivate());
+            byte[] encData = cipher.doFinal(data.getBytes("UTF-8"));;
+            return Base64.getEncoder().encodeToString(encData);
+        }catch(Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String decRSA(String text, KeyPair key) {
+        try {
+            Cipher cipher = Cipher.getInstance("RSA");
+            cipher.init(Cipher.DECRYPT_MODE, key.getPublic());
             return new String(cipher.doFinal(Base64.getDecoder().decode(text)));
         } catch (Exception e) {
             throw new RuntimeException(e);
