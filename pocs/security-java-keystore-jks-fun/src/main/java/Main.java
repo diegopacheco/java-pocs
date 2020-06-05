db9974b81a99e3c9ee7a6c60566cc70546dcb8f2
diegopacheco <diego.pacheco.it@gmail.com>
2020-06-05 14:04:36 -0300

    jks

--- pocs/security-java-keystore-jks-fun/src/main/java/Main.java
+++ pocs/security-java-keystore-jks-fun/src/main/java/Main.java
@@ -1,6 +1,63 @@
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.security.Key;
+import java.security.KeyStore;
+import java.util.Base64;
 
+/**
+ * Run with: -Dkeystore.type=pkcs12
+ */
 public class Main{
+
+  private static final char[] pwdArray = "password".toCharArray();
+
   public static void main(String args[]){
-      
+    try {
+      writeKeyStore();
+      KeyStore ks = readKeyStore();
+      ks = storeSymmetricKey(ks);
+      Key dbKey = ks.getKey("db-encryption-secret", pwdArray);
+      System.out.println("DB Key from JKS: " + Base64.getEncoder().encodeToString(dbKey.getEncoded()) + " @ " + dbKey.getAlgorithm() );
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+
   }
+
+  public static void writeKeyStore(){
+    try{
+      KeyStore ks = KeyStore.getInstance("pkcs12");
+      ks.load(null, pwdArray);
+      try (FileOutputStream fos = new FileOutputStream("ks.jks")) {
+        ks.store(fos,pwdArray);
+      }
+    }catch(Exception e){
+      throw new RuntimeException(e);
+    }
+  }
+
+  public static KeyStore readKeyStore(){
+    try {
+      KeyStore ks = KeyStore.getInstance("JKS");
+      ks.load(new FileInputStream("ks.jks"), pwdArray);
+      return ks;
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public static KeyStore storeSymmetricKey(KeyStore ks){
+    try {
+      SecretKey key = KeyGenerator.getInstance("AES").generateKey();
+      KeyStore.SecretKeyEntry secret = new KeyStore.SecretKeyEntry(key);
+      KeyStore.ProtectionParameter password = new KeyStore.PasswordProtection(pwdArray);
+      ks.setEntry("db-encryption-secret", secret, password);
+      return ks;
+    }catch(Exception e){
+      throw new RuntimeException(e);
+    }
+  }
+
 }
