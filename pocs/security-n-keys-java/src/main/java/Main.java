9a7998b1fd2bf1c8a34049a2c5ab4acaf0809ebf
diegopacheco <diego.pacheco.it@gmail.com>
2020-06-11 11:55:01 -0300

    added bench

--- pocs/security-n-keys-java/src/main/java/Main.java
+++ pocs/security-n-keys-java/src/main/java/Main.java
@@ -3,17 +3,34 @@ import java.util.Base64;
 
 public class Main{
   public static void main(String args[]) throws Exception {
-    Key rootKey = CryptoService.generateKey(256);
-    System.out.println("Root Key: " + Base64.getEncoder().encodeToString(rootKey.getEncoded()));
-
-    String plainText = "This is a message by Diego Pacheco!";
-    System.out.println("Plain Text: " + plainText);
-
-    CryptoService cs = new CryptoService();
-    Envelope e = cs.enc(rootKey,plainText.getBytes("UTF-8"));
-    System.out.println("Envelope: " + e);
-
-    byte[] result = cs.dec(rootKey,e);
-    System.out.println("Decrypted Data from Envelope: " + new String(result,"UTF-8"));
+    bench("this is a secret"); //16
+    bench("this is a secretthis is a secret"); // 32
+    bench("this is a secretthis is a secretthis is a secretthis is a secret"); // 64
+    bench("this is a secretthis is a secretthis is a secretthis is a secretthis is a secretthis is a secretthis is a secretthis is a secret"); // 128
   }
+
+  private static void bench(String plainText){
+    try{
+      System.out.println("******** Plain Text Size: " + plainText.length());
+      Key rootKey = CryptoService.generateKey(256);
+      System.out.println("Root Key: " + Base64.getEncoder().encodeToString(rootKey.getEncoded()));
+      System.out.println("Plain Text: " + plainText);
+
+      CryptoService cs = new CryptoService();
+      Long start = System.currentTimeMillis();
+      Envelope e = cs.enc(rootKey,plainText.getBytes("UTF-8"));
+      Long end   = System.currentTimeMillis();
+      System.out.println("Envelope: " + e);
+      System.out.println("Envelope Encryption time: " + (end-start) + " ms");
+
+      start = System.currentTimeMillis();
+      byte[] result = cs.dec(rootKey,e);
+      end   = System.currentTimeMillis();
+      System.out.println("Decrypted Data from Envelope: " + new String(result,"UTF-8"));
+      System.out.println("Envelope Decryption time: " + (end-start) + " ms");
+    }catch(Exception e){
+      throw new RuntimeException(e);
+    }
+  }
+
 }
