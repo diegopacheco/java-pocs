876003d1938bd4780595e995ecf5a8e1283a1ae8
diegopacheco <diego.pacheco.it@gmail.com>
2020-06-12 10:57:52 -0300

    Added gradle wrapper jar

--- pocs/unirest-fun/src/main/java/com/github/diegopacheco/sandbox/java/unirest/sample/Main.java
+++ pocs/unirest-fun/src/main/java/com/github/diegopacheco/sandbox/java/unirest/sample/Main.java
@@ -1,11 +1,10 @@
 package com.github.diegopacheco.sandbox.java.unirest.sample;
 
-import com.mashape.unirest.http.Unirest;
-import com.mashape.unirest.http.exceptions.UnirestException;
+import kong.unirest.Unirest;
+import kong.unirest.UnirestException;
 
 public class Main {
 	public static void main(String[] args) throws UnirestException {
-		
 		System.out.println(
 		    Unirest.post("http://httpbin.org/post")
 		   .queryString("name", "Mark")
