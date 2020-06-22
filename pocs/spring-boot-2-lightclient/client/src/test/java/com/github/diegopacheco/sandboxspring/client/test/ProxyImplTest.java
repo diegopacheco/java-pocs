e04d31f0ccebef9b8f9ce7fb3fa69efd598da3aa
diegopacheco <diego.pacheco.it@gmail.com>
2020-06-22 20:35:48 -0300

    created module pom

--- pocs/spring-boot-2-lightclient/client/src/test/java/com/github/diegopacheco/sandboxspring/client/test/ProxyImplTest.java
+++ pocs/spring-boot-2-lightclient/client/src/test/java/com/github/diegopacheco/sandboxspring/client/test/ProxyImplTest.java
@@ -1,11 +1,24 @@
 package com.github.diegopacheco.sandboxspring.client.test;
 
+import com.github.diegopacheco.sandboxspring.Application;
 import com.github.diegopacheco.sandboxspring.client.Client;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ProxyImplTest {
+
+    @BeforeAll
+    public static void setup(){
+        Application.main(new String[]{});
+        // warmup
+        Client client = Client.getInstance();
+        for(int i=0;i<=10;i++){
+            client.mul(2D,3D+i);
+            client.sum(3D,7D+i);
+        }
+    }
 
     @Test
     public void testMul(){
