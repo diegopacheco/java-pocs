c8392e887a7662f38107711c76f713d573163299
diegopacheco <diego.pacheco.it@gmail.com>
2020-06-24 12:15:39 -0300

    .

--- pocs/debug-playground/src/main/java/Main.java
+++ pocs/debug-playground/src/main/java/Main.java
@@ -8,9 +8,9 @@ public class Main{
     p.setEmail("diego.pacheco.it@gmail.com");
 
     Pojo p2 = new Pojo();
-    p.setName("Melina");
-    p.setEmail("mel@mel");
-    p.setAge(6);
+    p2.setName("Melina");
+    p2.setEmail("mel@mel");
+    p2.setAge(6);
 
     boolean result = p.equals(p2);
     if (result){
@@ -18,6 +18,8 @@ public class Main{
     }else{
       System.out.println(p.getName() + " is NOT the same as " + p2.getName());
     }
+
+    System.out.println( String.join(";", p.getEmail(), p2.getEmail()) );
 
     if (result){
       throw new RuntimeException(new IOException("Error!"));
