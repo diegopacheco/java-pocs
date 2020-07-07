f980efc4b709f2ba670ace57c045d8a4ded69fe0
diegopacheco <diego.pacheco.it@gmail.com>
2020-07-07 19:23:36 -0300

    immutable values

---
+++ pocs/immutables-fun/src/main/java/Main.java
@@ -0,0 +1,18 @@
+import java.util.List;
+
+public class Main{
+  public static void main(String args[]){
+
+    FoobarValue value = ImmutableFoobarValue.builder()
+            .foo(2)
+            .bar("Bar")
+            .addBuz(1, 3, 4)
+            .build(); // FoobarValue{foo=2, bar=Bar, buz=[1, 3, 4], crux={}}
+
+    int foo = value.foo(); // 2
+    System.out.println(foo);
+
+    List<Integer> buz = value.buz(); // ImmutableList.of(1, 3, 4)
+    System.out.println(buz);
+  }
+}
