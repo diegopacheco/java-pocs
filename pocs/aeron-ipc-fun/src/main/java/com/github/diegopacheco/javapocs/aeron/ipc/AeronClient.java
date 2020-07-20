dedde1aed0dce2df13b65c3f2ef68be761cb0ef0
diegopacheco <diego.pacheco.it@gmail.com>
2020-07-20 17:17:07 -0300

    working

--- pocs/aeron-ipc-fun/src/main/java/com/github/diegopacheco/javapocs/aeron/ipc/AeronClient.java
+++ pocs/aeron-ipc-fun/src/main/java/com/github/diegopacheco/javapocs/aeron/ipc/AeronClient.java
@@ -1,7 +1,27 @@
 package com.github.diegopacheco.javapocs.aeron.ipc;
 
-public class AeronClient {
-    public static void main(String[] args) {
+import io.aeron.Aeron;
+import io.aeron.Publication;
+import org.agrona.BufferUtil;
+import org.agrona.concurrent.UnsafeBuffer;
 
+public class AeronClient {
+
+    private static final UnsafeBuffer BUFFER =
+            new UnsafeBuffer(BufferUtil.allocateDirectAligned(256, 64));
+
+    public static void main(String[] args) throws Throwable{
+        String channel = "aeron:ipc";
+        final Aeron.Context ctx = new Aeron.Context();
+        Aeron aeron = Aeron.connect(ctx);
+
+        final Publication publication = aeron.addPublication(channel, 1);
+        sentMessage(aeron,publication,"Hello");
+    }
+
+    private static void sentMessage(Aeron aeron,Publication publication,String message){
+        BUFFER.putBytes(0, message.getBytes());
+        final long resultingPosition = publication.offer(BUFFER, 0, message.getBytes().length);
+        System.out.println("Publish: " + resultingPosition);
     }
 }
--- pocs/aeron-ipc-fun/src/main/java/com/github/diegopacheco/javapocs/aeron/ipc/AeronServer.java
+++ pocs/aeron-ipc-fun/src/main/java/com/github/diegopacheco/javapocs/aeron/ipc/AeronServer.java
@@ -2,35 +2,20 @@ package com.github.diegopacheco.javapocs.aeron.ipc;
 
 import io.aeron.Aeron;
 import io.aeron.FragmentAssembler;
-import io.aeron.Publication;
 import io.aeron.Subscription;
 import io.aeron.driver.MediaDriver;
 import io.aeron.logbuffer.FragmentHandler;
-import org.agrona.BufferUtil;
 import org.agrona.concurrent.BackoffIdleStrategy;
 import org.agrona.concurrent.IdleStrategy;
-import org.agrona.concurrent.UnsafeBuffer;
-
 import java.util.concurrent.TimeUnit;
 
 public class AeronServer {
 
-    private static final UnsafeBuffer BUFFER =
-            new UnsafeBuffer(BufferUtil.allocateDirectAligned(256, 64));
-
-    public static void main(String[] args) throws Throwable{
-
+    public static void main(String[] args) {
         String channel = "aeron:ipc";
-
-        final MediaDriver driver = MediaDriver.launchEmbedded();
+        final MediaDriver driver = MediaDriver.launch();
         final Aeron.Context ctx = new Aeron.Context();
         Aeron aeron = Aeron.connect(ctx.aeronDirectoryName(driver.aeronDirectoryName()));
-
-        final Publication publication = aeron.addPublication(channel, 1);
-        final String message = "Hello World!";
-        BUFFER.putBytes(0, message.getBytes());
-        final long resultingPosition = publication.offer(BUFFER, 0, message.getBytes().length);
-        System.out.println("Publish: " + resultingPosition);
 
         final Subscription subscription = aeron.addSubscription(channel, 1);
         final FragmentHandler fragmentHandler = (buffer, offset, length, header) -> {
@@ -45,9 +30,11 @@ public class AeronServer {
         final IdleStrategy idleStrategy = new BackoffIdleStrategy(
                 100, 10, TimeUnit.MICROSECONDS.toNanos(1), TimeUnit.MICROSECONDS.toNanos(100));
 
+        System.out.println("Waiting for messages... ");
         while (true) {
             final int fragmentsRead = subscription.poll(fragmentHandler, 10);
             idleStrategy.idle(fragmentsRead);
         }
     }
+
 }
