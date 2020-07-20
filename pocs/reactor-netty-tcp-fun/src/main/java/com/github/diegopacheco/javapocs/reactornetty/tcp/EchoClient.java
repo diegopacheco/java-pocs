7444859748da3fbec842c23bb5467bd0f673e42d
diegopacheco <diego.pacheco.it@gmail.com>
2020-07-20 15:18:27 -0300

    reactor

---
+++ pocs/reactor-netty-tcp-fun/src/main/java/com/github/diegopacheco/javapocs/reactornetty/tcp/EchoClient.java
@@ -0,0 +1,45 @@
+package com.github.diegopacheco.javapocs.reactornetty.tcp;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.netty.ByteBufFlux;
+import reactor.netty.Connection;
+import reactor.netty.tcp.TcpClient;
+
+import java.util.UUID;
+
+public final class EchoClient {
+
+    static final boolean SECURE = System.getProperty("secure") != null;
+    static final int PORT = Integer.parseInt(System.getProperty("port", SECURE ? "8443" : "8080"));
+    static final boolean WIRETAP = System.getProperty("wiretap") != null;
+
+    public static void main(String[] args) {
+        TcpClient client =
+                TcpClient.create()
+                        .port(PORT)
+                        .wiretap(WIRETAP);
+
+        if (SECURE) {
+            client = client.secure(
+                    spec -> spec.sslContext(SslContextBuilder.forClient().trustManager(InsecureTrustManagerFactory.INSTANCE)));
+        }
+
+        sendMessage(client);
+    }
+
+    private static void sendMessage(TcpClient client){
+        try{
+            Connection connection = client.handle((in, out) -> {
+                String message = "echo_"+ UUID.randomUUID().toString();
+                System.out.println("Sending message... " + message);
+                return out.send(Flux.concat(ByteBufFlux.fromString(Mono.just(message)),in.receive().retain()));
+            }).connectNow();
+            connection.onDispose().block();
+        }catch(Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+
+}
---
+++ pocs/reactor-netty-tcp-fun/src/main/java/com/github/diegopacheco/javapocs/reactornetty/tcp/EchoServer.java
@@ -0,0 +1,39 @@
+package com.github.diegopacheco.javapocs.reactornetty.tcp;
+
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.SelfSignedCertificate;
+import reactor.netty.ByteBufFlux;
+import reactor.netty.tcp.TcpServer;
+
+public final class EchoServer {
+
+    static final boolean SECURE = System.getProperty("secure") != null;
+    static final int PORT = Integer.parseInt(System.getProperty("port", SECURE ? "8443" : "8080"));
+    static final boolean WIRETAP = System.getProperty("wiretap") != null;
+
+    public static void main(String[] args) throws Exception {
+        TcpServer server =
+                TcpServer.create()
+                        .port(PORT)
+                        .wiretap(WIRETAP)
+                        .handle(
+                                (in, out) -> {
+                                    System.out.println("got message: ");
+                                    ByteBufFlux result = in.receive().retain();
+                                    result.asString().subscribe( data -> System.out.println("recived: " + data) );
+                                    return out.send(result);
+                                }
+                        );
+
+        if (SECURE) {
+            SelfSignedCertificate ssc = new SelfSignedCertificate();
+            server = server.secure(
+                    spec -> spec.sslContext(SslContextBuilder.forServer(ssc.certificate(), ssc.privateKey())));
+        }
+
+        System.out.println("Running EchoServer... ");
+        server.bindNow()
+                .onDispose()
+                .block();
+    }
+}
