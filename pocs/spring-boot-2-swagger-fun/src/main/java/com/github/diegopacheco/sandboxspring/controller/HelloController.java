c1f20b8d682e7d8bfc1dab94a34fa923d4e892d6
diegopacheco <diego.pacheco.it@gmail.com>
2020-07-24 20:01:59 -0300

    improve text for spring-fox boot2 + swagger

--- pocs/spring-boot-2-swagger-fun/src/main/java/com/github/diegopacheco/sandboxspring/controller/HelloController.java
+++ pocs/spring-boot-2-swagger-fun/src/main/java/com/github/diegopacheco/sandboxspring/controller/HelloController.java
@@ -1,14 +1,16 @@
 package com.github.diegopacheco.sandboxspring.controller;
 
 import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
 
-	@ApiOperation(value = "Greetings Bitches!!!",notes = "It is what it is!")
+	@ApiOperation(value = "Greetings",notes = "It is what it is!")
 	@RequestMapping("/")
+	@GetMapping
 	public String index() {
 		return "Greetings from Spring Boot!";
 	}
