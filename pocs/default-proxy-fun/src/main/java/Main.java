14ac0f0c758a702a2ea148b28c3f73a85bff72ba
diegopacheco <diego.pacheco.it@gmail.com>
2020-08-04 13:34:08 -0300

    std proxy

--- pocs/default-proxy-fun/src/main/java/Main.java
+++ pocs/default-proxy-fun/src/main/java/Main.java
@@ -3,15 +3,27 @@ import com.github.diegopacheco.javapocs.proxy.DynamicInvocationHandler;
 import java.lang.reflect.Proxy;
 import java.util.Map;
 
-public class Main{
-  public static void main(String args[]){
+public class Main {
+    public static void main(String args[]) {
 
-    Map proxyInstance = (Map) Proxy.newProxyInstance(
-            DynamicInvocationHandler.class.getClassLoader(),
-            new Class[] { Map.class },
-            new DynamicInvocationHandler());
+        Map proxyInstance = (Map) Proxy.newProxyInstance(
+                DynamicInvocationHandler.class.getClassLoader(),
+                new Class[]{Map.class},
+                new DynamicInvocationHandler());
 
-    System.out.println("Map.get(1) == " + proxyInstance.get("1"));
+        System.out.println("Map.get(1) == " + proxyInstance.get("1"));
+    
+        Map proxyInstanceLambda = (Map) Proxy.newProxyInstance(
+                DynamicInvocationHandler.class.getClassLoader(),
+                new Class[]{Map.class},
+                (proxy, method, methodArgs) -> {
+                    if (method.getName().equals("get")) {
+                        return 43;
+                    } else {
+                        throw new UnsupportedOperationException("Unsupported method: " + method.getName());
+                    }
+                });
+        System.out.println("Map.get(1) == " + proxyInstanceLambda.get("1"));
 
-  }
+    }
 }
