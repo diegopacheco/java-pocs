91fb4104cc8d054b3d8e2aa8224a5ea92832eabe
diegopacheco <diego.pacheco.it@gmail.com>
2020-08-03 23:05:35 -0300

    clean tests

--- pocs/pitest-mutation-testing-mvn-fun/src/main/java/com/github/diegopacheco/javapocs/pitest/service/CalculatorService.java
+++ pocs/pitest-mutation-testing-mvn-fun/src/main/java/com/github/diegopacheco/javapocs/pitest/service/CalculatorService.java
@@ -1,32 +1,20 @@
 package com.github.diegopacheco.javapocs.pitest.service;
 
-import java.util.Objects;
-
 public class CalculatorService {
 
-    public double sum(double arg1,double arg2){
-        Objects.requireNonNull(arg1);
-        Objects.requireNonNull(arg2);
+    public double sum(Double arg1,Double arg2){
         return arg1 + arg2;
     }
 
-    public double mul(double arg1, double arg2){
-        Objects.requireNonNull(arg1);
-        Objects.requireNonNull(arg2);
+    public double mul(Double arg1, Double arg2){
         return arg1 * arg2;
     }
 
-    public double divide(double arg1,double arg2){
-        Objects.requireNonNull(arg1);
-        Objects.requireNonNull(arg2);
-        if (0==arg1 || 0==arg2)
-            throw new IllegalArgumentException("Args cannot be ZERO.");
+    public double divide(Double arg1,Double arg2){
         return arg1 / arg2;
     }
 
-    public double sub(double arg1, double arg2){
-        Objects.requireNonNull(arg1);
-        Objects.requireNonNull(arg2);
+    public double sub(Double arg1, Double arg2){
         return arg1 - arg2;
     }
 
--- pocs/pitest-mutation-testing-mvn-fun/src/test/java/com/github/diegopacheco/javapocs/pitest/service/CalculatorServiceTest.java
+++ pocs/pitest-mutation-testing-mvn-fun/src/test/java/com/github/diegopacheco/javapocs/pitest/service/CalculatorServiceTest.java
@@ -2,40 +2,31 @@ package com.github.diegopacheco.javapocs.pitest.service;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CalculatorServiceTest {
 
     @Test
     public void testSum(){
         CalculatorService calc = new CalculatorService();
-        assertEquals(6,calc.sum(3,3));
+        assertEquals(6,calc.sum(3D,3D));
     }
 
     @Test
     public void testMul(){
         CalculatorService calc = new CalculatorService();
-        assertEquals(6,calc.mul(2,3));
+        assertEquals(6,calc.mul(2d,3D));
     }
 
     @Test
     public void testDivide(){
         CalculatorService calc = new CalculatorService();
-        assertEquals(2,calc.divide(12,6));
+        assertEquals(2,calc.divide(12D,6D));
     }
 
     @Test
     public void testSub(){
         CalculatorService calc = new CalculatorService();
-        assertEquals(6,calc.sub(12,6));
-    }
-
-    @Test
-    public void testDivZero(){
-        CalculatorService calc = new CalculatorService();
-        double a = 0D;
-        double b = 1D;
-        assertThrows(IllegalArgumentException.class,() -> calc.divide(a,b));
+        assertEquals(6,calc.sub(12D,6D));
     }
 
 }
