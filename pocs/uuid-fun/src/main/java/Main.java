eb33392021b410621eff5f7afa3f207ce139f9d8
diegopacheco <diego.pacheco.it@gmail.com>
2020-08-13 17:48:08 -0300

    added sample of time uuid

--- pocs/uuid-fun/src/main/java/Main.java
+++ pocs/uuid-fun/src/main/java/Main.java
@@ -2,7 +2,9 @@ import com.github.diegopacheco.javapocs.uuid.UUIDUtils;
 
 public class Main{
   public static void main(String args[]){
-    System.out.println(UUIDUtils.uuidMethod1());
-    System.out.println(UUIDUtils.uuidMethod2());
+    System.out.println("UUID method 1 " + UUIDUtils.uuidMethod1());
+    System.out.println("UUID method 2 " + UUIDUtils.uuidMethod2());
+    System.out.println("UUID TS       " + UUIDUtils.generateTimeBasedUUID().timestamp());
+    System.out.println("UUID TS       " + UUIDUtils.getDatefromUUID(UUIDUtils.generateTimeBasedUUID()));
   }
 }
--- pocs/uuid-fun/src/main/java/com/github/diegopacheco/javapocs/uuid/UUIDUtils.java
+++ pocs/uuid-fun/src/main/java/com/github/diegopacheco/javapocs/uuid/UUIDUtils.java
@@ -1,9 +1,16 @@
 package com.github.diegopacheco.javapocs.uuid;
 
+import com.github.f4b6a3.uuid.UuidCreator;
+
 import java.security.SecureRandom;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.UUID;
 
 public class UUIDUtils {
+
+    private static final long KCLOCK_OFFSET = 0x01b21dd213814000L;
+    private static final long KCLOCK_MULTIPLIER_L = 10000L;
 
     public static String uuidMethod1() {
         return UUID.randomUUID().toString();
@@ -11,6 +18,24 @@ public class UUIDUtils {
 
     public static String uuidMethod2() {
         return UUID.nameUUIDFromBytes(new SecureRandom().generateSeed(16)).toString();
+    }
+
+    public static UUID generateTimeBasedUUID(){
+        return UuidCreator.getTimeBased();
+    }
+
+    public static String getDatefromUUID(UUID uuid){
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        return sdf.format(new Date(getUnixTimestampInMillisFromUUIDTimestamp(uuid.timestamp())));
+    }
+
+    private static long uuidTimestamp(String strTS){
+        long ts = UUID.fromString(strTS).timestamp();
+        return getUnixTimestampInMillisFromUUIDTimestamp(ts);
+    }
+
+    private static long getUnixTimestampInMillisFromUUIDTimestamp( long uuidTimestamp ) {
+        return ( uuidTimestamp - KCLOCK_OFFSET ) / KCLOCK_MULTIPLIER_L;
     }
 
 }
