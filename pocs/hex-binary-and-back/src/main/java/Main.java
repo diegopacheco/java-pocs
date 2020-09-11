076e3d847edde40e62de0fc212e6a6ee6287393e
diegopacheco <diego.pacheco.it@gmail.com>
2020-09-11 19:17:20 -0300

    convert formats

--- pocs/hex-binary-and-back/src/main/java/Main.java
+++ pocs/hex-binary-and-back/src/main/java/Main.java
@@ -1,47 +1,47 @@
 import java.math.BigInteger;
 import java.util.Base64;
 
-public class Main{
-  public static void main(String args[]){
-    System.out.println("HEX      =  1234abcd12ab34cd56ef1234567890ab");
-    System.out.println("BIN      = " + hexToBinary("1234abcd12ab34cd56ef1234567890ab"));
-    System.out.println("DEC      = " + binToDec(hexToBinary("1234abcd12ab34cd56ef1234567890ab")));
-    System.out.println("BASE64   = " + binToBase64(hexToBinary("1234abcd12ab34cd56ef1234567890ab")));
-    System.out.println("HEX BACK = " + binToHex( base64ToBin( binToBase64(hexToBinary("1234abcd12ab34cd56ef1234567890ab")))));
-  }
-
-  private static String hexToBinary(String hex) {
-    return new BigInteger(hex, 16).toString(2);
-  }
-
-  private static int binToDec(String binaryString){
-    double j=0;
-    for(int i=0;i<binaryString.length();i++){
-      if(binaryString.charAt(i)== '1'){
-        j=j+ Math.pow(2,binaryString.length()-1-i);
-      }
+public class Main {
+    public static void main(String args[]) {
+        System.out.println("HEX      = 1234abcd12ab34cd56ef1234567890ab");
+        System.out.println("BIN      = " + hexToBinary("1234abcd12ab34cd56ef1234567890ab"));
+        System.out.println("DEC      = " + binToDec(hexToBinary("1234abcd12ab34cd56ef1234567890ab")));
+        System.out.println("BASE64   = " + binToBase64(hexToBinary("1234abcd12ab34cd56ef1234567890ab")));
+        System.out.println("HEX BACK = " + decToHex(binToDec(base64ToBin(binToBase64(hexToBinary("1234abcd12ab34cd56ef1234567890ab"))))));
     }
-    return (int) j;
-  }
 
-  private static String binToBase64(String binaryString){
-    try{
-      return new String(Base64.getEncoder().encode(binaryString.getBytes("UTF-8")),"UTF-8");
-    }catch(Exception e){
-      throw new RuntimeException(e);
+    private static String hexToBinary(String hex) {
+        return new BigInteger(hex, 16).toString(2);
     }
-  }
 
-  private static String base64ToBin(String binaryString){
-    try{
-      return new String(Base64.getDecoder().decode(binaryString.getBytes("UTF-8")),"UTF-8");
-    }catch(Exception e){
-      throw new RuntimeException(e);
+    private static int binToDec(String binaryString) {
+        double j = 0;
+        for (int i = 0; i < binaryString.length(); i++) {
+            if (binaryString.charAt(i) == '1') {
+                j = j + Math.pow(2, binaryString.length() - 1 - i);
+            }
+        }
+        return (int) j;
     }
-  }
 
-  private static String binToHex(String binaryStr){
-    return Integer.toString(Integer.parseInt(binaryStr,2),16);
-  }
+    private static String binToBase64(String binaryString) {
+        try {
+            return new String(Base64.getEncoder().encode(binaryString.getBytes("UTF-8")), "UTF-8");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static String base64ToBin(String binaryString) {
+        try {
+            return new String(Base64.getDecoder().decode(binaryString.getBytes("UTF-8")), "UTF-8");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static String decToHex(int dec) {
+        return Integer.toHexString(dec);
+    }
 
 }
