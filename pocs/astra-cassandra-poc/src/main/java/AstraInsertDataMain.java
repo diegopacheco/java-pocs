d935ad9d160373114e833e8afe3e49ed2bf83133
diegopacheco <diego.pacheco.it@gmail.com>
2020-11-10 19:20:19 -0300

    select and insert sample

--- pocs/astra-cassandra-poc/src/main/java/AstraInsertDataMain.java
+++ pocs/astra-cassandra-poc/src/main/java/AstraInsertDataMain.java
@@ -1,11 +1,8 @@
 import com.datastax.oss.driver.api.core.CqlSession;
 import com.datastax.oss.driver.api.core.cql.PreparedStatement;
 import com.datastax.oss.driver.api.core.cql.ResultSet;
-import com.datastax.oss.driver.api.core.cql.Row;
 import com.datastax.oss.driver.api.querybuilder.QueryBuilder;
-
 import java.nio.file.Paths;
-import java.util.Iterator;
 
 public class AstraInsertDataMain {
     public static void main(String[] args) {
