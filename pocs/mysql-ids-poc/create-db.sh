c5c056bf7ac51f105cae523e9513a8f39e88a4a5
diegopacheco <diego.pacheco.it@gmail.com>
2020-11-11 14:37:34 -0300

    diff size

--- pocs/mysql-ids-poc/create-db.sh
+++ pocs/mysql-ids-poc/create-db.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 echo "create database test_db;" | mysql -uroot -ppass -h127.0.0.1 -P3325
+
 echo "use test_db; CREATE TABLE IF NOT EXISTS Test(
+  k VARCHAR(100),
+  v VARCHAR(100),
+  PRIMARY KEY (k)
+) ENGINE=INNODB;" | mysql -uroot -ppass -h127.0.0.1 -P3325
+
+echo "use test_db; CREATE TABLE IF NOT EXISTS Test2(
   k VARCHAR(100),
   v VARCHAR(100),
   PRIMARY KEY (k)
--- pocs/mysql-ids-poc/src/main/java/Main.java
+++ pocs/mysql-ids-poc/src/main/java/Main.java
@@ -1,26 +1,56 @@
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.function.Consumer;
 
 public class Main {
     public static void main(String args[]) {
-        // 1M records
-        benchIt( (Void) -> insertBatch(1000000) );
+        // 1.5M records, took 3 minutes to insert them.
+        /*
+        benchIt((Void) -> {
+            insertBatch(1000000, "Test");
+            insertBatch(500000, "Test2");
+        });
+        */
+        benchIt((Void) -> {
+                    diff();
+                });
         System.out.println("FIN.");
     }
 
-    public static void insertBatch(int total) {
-        final int BATCH_SIZE=10000;
+    private static int diff() {
         try {
-            try(Connection cnn = HikariDS.getConnection();){
-                for(int i=1; i<=total;i+=BATCH_SIZE){
+            try (Connection cnn = HikariDS.getConnection();) {
+                Statement stmt = cnn.createStatement();
+                ResultSet rs = stmt.executeQuery("SELECT Test.k\n" +
+                        "FROM Test\n" +
+                        "    LEFT JOIN Test2 ON (Test.k = Test2.k)\n" +
+                        "WHERE Test2.k IS NULL");
+                int i = 0;
+                while(rs.next()) {
+                    i++;
+                }
+                System.out.println("Diff size: " + i);
+                return i;
+            }
+        } catch (Exception e) {
+            System.out.println("Could not get the diff. Exception: " + e);
+            return -1;
+        }
+    }
+
+    public static void insertBatch(int total, String table) {
+        final int BATCH_SIZE = 10000;
+        try {
+            try (Connection cnn = HikariDS.getConnection();) {
+                for (int i = 1; i <= total; i += BATCH_SIZE) {
                     Statement stmt = cnn.createStatement();
                     cnn.setAutoCommit(false);
-                    for(int j=1;j<=BATCH_SIZE;j++){
-                        stmt.addBatch("REPLACE into Test values ('k" + i+j + "','v" + i+j +"')");
+                    for (int j = 1; j <= BATCH_SIZE; j++) {
+                        stmt.addBatch("REPLACE into " + table + " values ('k" + i + j + "','v" + i + j + "')");
                     }
                     int[] result = stmt.executeBatch();
-                    System.out.println("The number of rows inserted: "+ result.length);
+                    System.out.println("The number of rows inserted: " + result.length);
                     cnn.commit();
                 }
             }
@@ -29,11 +59,11 @@ public class Main {
         }
     }
 
-    private static void benchIt(Consumer<Void> func){
+    private static void benchIt(Consumer<Void> func) {
         long init = System.currentTimeMillis();
         func.accept(null);
         long end = System.currentTimeMillis();
-        System.out.println("Executed in: " + (end-init) + " ms.");
+        System.out.println("Executed in: " + (end - init) + " ms.");
     }
 
 }
