03b3cc4dd7999d5c7a78ea5d9250a5f4acedacec
diegopacheco <diego.pacheco.it@gmail.com>
2020-11-11 20:56:31 -0300

    Cass ids pocs

--- pocs/cass-diff-ids-poc/src/main/java/Main.java
+++ pocs/cass-diff-ids-poc/src/main/java/Main.java
@@ -1,25 +1,52 @@
+import com.datastax.oss.driver.api.core.CqlSession;
+import com.datastax.oss.driver.api.core.cql.ResultSet;
+import com.datastax.oss.driver.api.querybuilder.insert.Insert;
+import com.datastax.oss.driver.api.querybuilder.select.Select;
+
 import java.util.function.Consumer;
 
-public class Main{
+import static com.datastax.oss.driver.api.querybuilder.QueryBuilder.*;
 
-  public static void main(String args[]){
-    bench( (Void)-> { insertData(); });
-    bench( (Void)-> { count(); });
-  }
+public class Main {
 
-  private static void insertData(){
+    public static void main(String args[]) {
+        bench((Void) -> {
+            insertData("Test");
+            insertData("Test2");
+        },"Data Insert");
+        bench((Void) -> {
+            count();
+        },"Diff IDS");
+    }
 
-  }
+    private static void insertData(String table) {
+        try (CqlSession session = CqlSession.builder().build()) {
+            session.execute("USE CLUSTER_TEST");
 
-  private static void count(){
+            Insert insert = insertInto(table)
+                    .value("key", literal("k"))
+                    .value("value", literal("V")).ifNotExists();
+            ResultSet rs = session.execute(insert.build());
+            System.out.println(rs.wasApplied());
+        }
+    }
 
-  }
+    private static void count() {
+        try (CqlSession session = CqlSession.builder().build()) {
+            session.execute("USE CLUSTER_TEST");
 
-  private static void bench(Consumer<Void> func){
-    long init = System.currentTimeMillis();
-    func.accept(null);
-    long end = System.currentTimeMillis();
-    System.out.println("Executed in > " + (end-init) + " ms.");
-  }
+            Select query = selectFrom("CLUSTER_TEST", "Test2").
+                    column("key");
+            ResultSet rs = session.execute(query.build());
+            System.out.println("Total IDS: " + rs.all().size());
+        }
+    }
+
+    private static void bench(Consumer<Void> func,String msg) {
+        long init = System.currentTimeMillis();
+        func.accept(null);
+        long end = System.currentTimeMillis();
+        System.out.println(msg + " .Executed in > " + (end - init) + " ms.");
+    }
 
 }
