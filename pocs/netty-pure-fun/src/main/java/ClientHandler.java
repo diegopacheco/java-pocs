0a506e926e7f37fb3e42c5a41bcd02af0e3b27d4
diegopacheco <diego.pacheco.it@gmail.com>
2020-11-17 10:34:13 -0300

    netty fun

---
+++ pocs/netty-pure-fun/src/main/java/ClientHandler.java
@@ -0,0 +1,22 @@
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class ClientHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        RequestData msg = new RequestData();
+        msg.setIntValue(123);
+        msg.setStringValue(
+                "all work and no play makes jack a dull boy");
+        ChannelFuture future = ctx.writeAndFlush(msg);
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println((ResponseData)msg);
+        ctx.close();
+    }
+
+}
---
+++ pocs/netty-pure-fun/src/main/java/ResponseData.java
@@ -0,0 +1,17 @@
+public class ResponseData {
+
+    private int intValue;
+
+    int getIntValue() {
+        return intValue;
+    }
+    void setIntValue(int intValue) {
+        this.intValue = intValue;
+    }
+
+    @Override
+    public String toString() {
+        return "ResponseData{" + "intValue=" + intValue + '}';
+    }
+
+}
