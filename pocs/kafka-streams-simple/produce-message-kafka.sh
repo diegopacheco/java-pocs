71cd34d708670ff4c3eb5346c0b31f5627e4aae8
diegopacheco <diego.pacheco.it@gmail.com>
2020-11-22 11:50:16 -0300

    Kafka streams simple

---
+++ pocs/kafka-streams-simple/produce-message-kafka.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+HERE=$(pwd)
+KAFKA_HOME=/home/diego/bin/kafka_2.13-2.6.0/
+cd $KAFKA_HOME 
+echo "diego likes kafka $(date) " | bin/kafka-console-producer.sh --topic inputTopic --bootstrap-server localhost:9092
+cd $HERE 
---
+++ pocs/kafka-streams-simple/src/main/java/Main.java
@@ -0,0 +1,44 @@
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KStreamBuilder;
+import org.apache.kafka.streams.kstream.KTable;
+
+import java.util.Arrays;
+import java.util.Properties;
+import java.util.regex.Pattern;
+
+public class Main{
+  public static void main(String args[]) throws Exception {
+
+    String bootstrapServers = "localhost:9092";
+    String inputTopic = "inputTopic";
+
+    Properties streamsConfiguration = new Properties();
+    streamsConfiguration.put(StreamsConfig.APPLICATION_ID_CONFIG, "wordcount-live-test");
+    streamsConfiguration.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+    streamsConfiguration.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+    streamsConfiguration.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+    streamsConfiguration.put(StreamsConfig.STATE_DIR_CONFIG,"/tmp/kafka-streams/");
+
+    KStreamBuilder builder = new KStreamBuilder();
+    KStream<String, String> textLines = builder.stream(inputTopic);
+    Pattern pattern = Pattern.compile("\\W+", Pattern.UNICODE_CHARACTER_CLASS);
+    KTable<String, Long> wordCounts = textLines
+            .flatMapValues(value -> Arrays.asList(pattern.split(value.toLowerCase())))
+            .groupBy((key, word) -> word)
+            .count();
+
+    wordCounts.foreach((w, c) -> System.out.println("word: " + w + " -> " + c));
+    String outputTopic = "outputTopic";
+    Serde<String> stringSerde = Serdes.String();
+    Serde<Long> longSerde = Serdes.Long();
+    wordCounts.to(stringSerde, longSerde, outputTopic);
+
+    KafkaStreams streams = new KafkaStreams(builder, streamsConfiguration);
+    streams.start();
+    //streams.close();
+  }
+}
