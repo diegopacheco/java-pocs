f5d0a403f11c9ba9f791e083f223626d0de5b3a3
diegopacheco <diego.pacheco.it@gmail.com>
2020-11-23 20:44:56 -0300

    code clean up

--- pocs/kafka-streams-simple/src/main/java/Main.java
+++ pocs/kafka-streams-simple/src/main/java/Main.java
@@ -1,4 +1,3 @@
-import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
