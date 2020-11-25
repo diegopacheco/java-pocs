16d3449ccd1dc00a9ae451fc29c3f62c80be700f
diegopacheco <diego.pacheco.it@gmail.com>
2020-11-25 15:32:03 -0300

    larray

--- pocs/larray-fun/src/main/java/Main.java
+++ pocs/larray-fun/src/main/java/Main.java
@@ -1,6 +1,15 @@
 
+import xerial.larray.japi.LArrayJ;
+import xerial.larray.*;
+
 public class Main{
   public static void main(String args[]){
-    System.out.println("It works!");
+
+    LIntArray l = LArrayJ.newLIntArray(10000L);
+    l.update(0L, 20); // Set l[0L] = 20
+    int e0 = l.apply(0L);  //  Get l[0L]
+    // release
+    l.free();
+    System.out.println(e0);
   }
 }
