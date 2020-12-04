c568a1f6beec909dfce011162c2fa8427723e556
diegopacheco <diego.pacheco.it@gmail.com>
2020-12-04 18:27:05 -0300

    build with fb/buck

---
+++ pocs/hello-buck-java/com/facebook/buck/demo/HelloBuckJava.java
@@ -0,0 +1,8 @@
+package com.facebook.buck.demo;
+
+public class HelloBuckJava {
+  public static void main(String[] args) {
+    System.out.println("Hello " + HelloString.getHelloString());
+  }
+}
+
---
+++ pocs/hello-buck-java/com/facebook/buck/demo/HelloString.java
@@ -0,0 +1,8 @@
+package com.facebook.buck.demo;
+
+public class HelloString {
+  public static String getHelloString() {
+    return "Buck\n";
+  }
+}
+
