bb28e34fedd039a4260ada8e4ba45ed8d71ec4d0
diegopacheco <diego.pacheco.it@gmail.com>
2020-12-08 13:47:27 -0300

    bazel mono repo with java - 100% working

--- pocs/bazel-monorepo-java/project-c/src/test/java/com/github/diegopacheco/projectc/TestApp.java
+++ pocs/bazel-monorepo-java/project-c/src/test/java/com/github/diegopacheco/projectc/TestApp.java
@@ -2,6 +2,7 @@ package com.github.diegopacheco.projectc;
 
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
+import com.github.diegopacheco.projectc.App;
 
 /**
  * Tests for correct dependency retrieval with maven rules.
