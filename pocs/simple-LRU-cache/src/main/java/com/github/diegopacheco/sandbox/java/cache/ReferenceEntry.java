2bed4af049ad0d7ca49457d945cc72be201f8691
diegopacheco <diego.pacheco.it@gmail.com>
2021-02-15 14:37:23 -0300

    simple and dummy LRU impl

---
+++ pocs/simple-LRU-cache/src/main/java/com/github/diegopacheco/sandbox/java/cache/ReferenceEntry.java
@@ -0,0 +1,79 @@
+package com.github.diegopacheco.sandbox.java.cache;
+
+import java.math.BigInteger;
+import java.util.Comparator;
+import java.util.Objects;
+
+/*
+*  ReferenceEntry is a container to keep track of data between hashmap and the heap.
+*
+*  @author Diego Pacheco
+* */
+public class ReferenceEntry<T> {
+
+    public static class ReferenceEntryMinComparator implements Comparator<ReferenceEntry> {
+        @Override
+        public int compare (ReferenceEntry x, ReferenceEntry y) {
+            return Long.compare(x.lastTimestampAccess,y.lastTimestampAccess);
+        }
+    }
+
+    private String key;
+    private T value;
+    private long lastTimestampAccess;
+
+    public ReferenceEntry(String key,long lastTimestampAccess) {
+        this.key = key;
+        this.lastTimestampAccess = lastTimestampAccess;
+    }
+    public ReferenceEntry(String key,T value, long lastTimestampAccess) {
+        this.key = key;
+        this.value = value;
+        this.lastTimestampAccess = lastTimestampAccess;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public long getLastTimestampAccess() {
+        return lastTimestampAccess;
+    }
+    public void setLastTimestampAccess(long lastTimestampAccess) {
+        this.lastTimestampAccess = lastTimestampAccess;
+    }
+
+    public static<R> ReferenceEntry<R> from(String key,R value){
+        return new ReferenceEntry<R>(key,value,System.currentTimeMillis());
+    }
+
+    public T getValue() {
+        this.lastTimestampAccess=System.currentTimeMillis();
+        return value;
+    }
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ReferenceEntry<?> that = (ReferenceEntry<?>) o;
+        return Objects.equals(key, that.key);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(key);
+    }
+
+    @Override
+    public String toString() {
+        return "ReferenceEntry{" +
+                "key='" + key + '\'' +
+                "value='" + value + '\'' +
+                ", lastTimestampAccess=" + lastTimestampAccess +
+                '}';
+    }
+}
---
+++ pocs/simple-LRU-cache/src/main/java/com/github/diegopacheco/sandbox/java/cache/SimpleLRUCache.java
@@ -0,0 +1,57 @@
+package com.github.diegopacheco.sandbox.java.cache;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.PriorityQueue;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * SimpleLRUCache is a simple LRU cache impl with a ConcurrentHashMap and PriorityQueue as heap.
+ * ReferenceEntry is used as container to track map and heap together.
+ * Evictions are done in SET operations. Heap is refreshed in GET/SET Operations.
+ *
+ * @param <T> is any type.
+ *
+ * @author Diego Pacheco
+ */
+public class SimpleLRUCache<T> implements LRUCache<T>{
+
+    private static final Integer THRESHOLD = 2;
+    private Map<String,ReferenceEntry<T>>  cache = new ConcurrentHashMap<>();
+    private PriorityQueue<ReferenceEntry<T>> lru = new PriorityQueue<>(new ReferenceEntry.ReferenceEntryMinComparator());
+
+    public Optional<T> get(String key){
+        ReferenceEntry<T> ref = cache.get(key);
+        if (null==ref){
+            return Optional.empty();
+        }
+        updateHeap(ref);
+        return Optional.of( ref.getValue() );
+    }
+
+    @Override
+    public void set(String key,T value){
+        evictIfRequired();
+        ReferenceEntry<T> ref = ReferenceEntry.from(key,value);
+        cache.put(key,ref);
+        updateHeap(ref);
+    }
+
+    private void updateHeap(ReferenceEntry<T> ref){
+        lru.remove(ref);
+        lru.add(ref);
+    }
+
+    private void evictIfRequired(){
+        if (size()>=THRESHOLD){
+            ReferenceEntry<T> ref = lru.poll();
+            cache.remove(ref.getKey());
+        }
+    }
+
+    @Override
+    public Integer size() {
+        return cache.size();
+    }
+
+}
