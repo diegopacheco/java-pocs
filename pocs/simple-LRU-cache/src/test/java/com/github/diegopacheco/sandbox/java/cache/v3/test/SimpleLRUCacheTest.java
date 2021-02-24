be6f1350dd64b505a056b56822c628d4edf6e376
diegopacheco <diego.pacheco.it@gmail.com>
2021-02-24 01:08:40 -0300

    added test and fix main on build file

--- pocs/simple-LRU-cache/src/test/java/com/github/diegopacheco/sandbox/java/cache/v3/test/SimpleLRUCacheTest.java
+++ pocs/simple-LRU-cache/src/test/java/com/github/diegopacheco/sandbox/java/cache/v3/test/SimpleLRUCacheTest.java
@@ -1,4 +1,25 @@
 package com.github.diegopacheco.sandbox.java.cache.v3.test;
 
+import com.github.diegopacheco.sandbox.java.cache.v3.LRUCache;
+import com.github.diegopacheco.sandbox.java.cache.v3.SimpleLRUCache;
+import org.junit.jupiter.api.Test;
+import java.util.Optional;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class SimpleLRUCacheTest {
+
+    @Test
+    public void testLRU(){
+        LRUCache<String> cache = new SimpleLRUCache<>();
+        cache.put("g1","Sc2");
+        cache.put("g2","D3");
+        cache.put("g3","D2");
+        assertNotNull(cache);
+
+        assertEquals("D3",cache.get("g2").get());
+        assertEquals("D2",cache.get("g3").get());
+        assertEquals(Optional.empty(),cache.get("g1"));
+        assertEquals(2,cache.size());
+    }
+
 }
