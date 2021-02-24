72087470bd4f937c5583051b985ec36c4514f36b
diegopacheco <diego.pacheco.it@gmail.com>
2021-02-24 17:11:47 -0300

    .

--- pocs/mina-tcp-server-fun/src/test/java/com/github/diegopacheco/java/sandbox/pocs/mina/tcp/test/TCPServerTest/ClientSessionHandler.java
+++ pocs/mina-tcp-server-fun/src/test/java/com/github/diegopacheco/java/sandbox/pocs/mina/tcp/test/TCPServerTest/ClientSessionHandler.java
@@ -3,28 +3,31 @@ package com.github.diegopacheco.java.sandbox.pocs.mina.tcp.test.TCPServerTest;
 import org.apache.mina.core.service.IoHandlerAdapter;
 import org.apache.mina.core.session.IoSession;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class ClientSessionHandler extends IoHandlerAdapter {
 
     private IoSession serverWritingSession;
-    private StringBuffer result;
+    private Map<String,StringBuffer> sessionBuffers = new ConcurrentHashMap<>();
 
     public String askTime(){
         serverWritingSession.write("</time>");
-        while(null==result){
+        while(null==sessionBuffers.get(serverWritingSession.getId()+"")){
             SleepWell.sleepSilently(100);
         }
-        String temp = result.toString();
-        result = null;
+        String temp = sessionBuffers.get(serverWritingSession.getId()+"").toString();
+        sessionBuffers.put(serverWritingSession.getId()+"",null);
         return temp;
     }
 
     @Override
     public void messageReceived(IoSession session, Object message ) throws Exception {
         System.out.println("CLIENT got : "+ message);
-        if (null==result){
-            result = new StringBuffer();
+        if (null==sessionBuffers.get(session.getId())){
+            sessionBuffers.put(session.getId()+"",new StringBuffer());
         }
-        result.append((String)message);
+        sessionBuffers.get(session.getId()+"").append((String)message);
     }
 
     public void setServerWritingSession(IoSession serverWritingSession) {
