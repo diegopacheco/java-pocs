3024edbc1373b1f4cc887e3197724753d1a60497
diegopacheco <diego.pacheco.it@gmail.com>
2021-03-23 21:22:25 -0300

    improve code

--- reflections-playground/src/main/java/com/github/diegopacheco/javapocs/reclections/logger/SimpleLogger.java
+++ reflections-playground/src/main/java/com/github/diegopacheco/javapocs/reclections/logger/SimpleLogger.java
@@ -21,17 +21,7 @@ public class SimpleLogger implements Logger{
                 callCustomProcessor(obj,customPostProcessor.get());
                 return;
             }else{
-                if (!Optional.empty().equals(cachePostProc.get(obj.getClass()))){
-                    Method[] methods = obj.getClass().getDeclaredMethods();
-                    for(Method m: methods){
-                        if(null!=m.getDeclaredAnnotation(PostProcessToString.class)){
-                            cachePostProc.put(obj.getClass(),Optional.of(m));
-                            callCustomProcessor(obj,m);
-                            return;
-                        }
-                    }
-                    cachePostProc.put(obj.getClass(),Optional.empty());
-                }
+                if (processReflectionLevel2(obj)) return;
             }
 
             if (null!=cacheUpper.get(obj.getClass()) && cacheUpper.get(obj.getClass()).isPresent()){
@@ -39,18 +29,38 @@ public class SimpleLogger implements Logger{
                 return;
             }
             if (!Optional.empty().equals(cacheUpper.get(obj.getClass()))){
-                UpperCasePostToString annotation = obj.getClass().getAnnotation(UpperCasePostToString.class);
-                if (null!=annotation){
-                    cacheUpper.put(obj.getClass(),Optional.of(true));
-                    System.out.println(obj.toString().toUpperCase());
-                    return;
-                }
-                cacheUpper.put(obj.getClass(),Optional.empty());
+                if (processReflectionLevel1(obj)) return;
             }
             System.out.println(obj);
         }catch(Exception e){
             throw new RuntimeException(e);
         }
+    }
+
+    private <T> boolean processReflectionLevel1(T obj) {
+        UpperCasePostToString annotation = obj.getClass().getAnnotation(UpperCasePostToString.class);
+        if (null!=annotation){
+            cacheUpper.put(obj.getClass(),Optional.of(true));
+            System.out.println(obj.toString().toUpperCase());
+            return true;
+        }
+        cacheUpper.put(obj.getClass(),Optional.empty());
+        return false;
+    }
+
+    private <T> boolean processReflectionLevel2(T obj) {
+        if (!Optional.empty().equals(cachePostProc.get(obj.getClass()))){
+            Method[] methods = obj.getClass().getDeclaredMethods();
+            for(Method m: methods){
+                if(null!=m.getDeclaredAnnotation(PostProcessToString.class)){
+                    cachePostProc.put(obj.getClass(),Optional.of(m));
+                    callCustomProcessor(obj,m);
+                    return true;
+                }
+            }
+            cachePostProc.put(obj.getClass(),Optional.empty());
+        }
+        return false;
     }
 
     private void callCustomProcessor(Object obj,Method customPostProcessor){
