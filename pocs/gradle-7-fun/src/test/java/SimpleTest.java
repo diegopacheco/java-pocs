5813a0a92f6b22ba225bf466bd4a6062a43a5c9e
diegopacheco <diego.pacheco.it@gmail.com>
2021-04-12 02:21:36 -0300

    gradle 7

---
+++ pocs/gradle-7-fun/src/test/java/SimpleTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SimpleTest {
+
+    @Test
+    public void testIt(){
+        assertEquals(2,1+1);
+    }
+
+}
