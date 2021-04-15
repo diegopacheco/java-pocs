2c7ea1e363d0ca606758c2c73c7cee34053cc5dc
diegopacheco <diego.pacheco.it@gmail.com>
2021-04-15 20:01:42 -0300

    array sum

---
+++ pocs/sun-array-fun/src/main/java/Main.java
@@ -0,0 +1,50 @@
+public class Main {
+
+    public static int sumArrayRange(int[] dataArray, int limit) {
+        int sum = 0;
+        int i = 0;
+        while (i != limit) {
+            if (dataArray[i]!=limit){
+                sum += dataArray[i];
+            }else{
+                return sum;
+            }
+            i++;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int[] anArray = {1, 2, 3, 4, 5, 6, 7};
+        System.out.println("Should be: 1");
+        System.out.println("Got      : " + sumArrayRange(anArray, 2));
+
+        anArray = new int[]{1, 2, 3, 4, 5, 6, 7};
+        System.out.println("Should be: 10");
+        System.out.println("Got      : " + sumArrayRange(anArray, 5));
+
+        anArray = new int[]{1, 2, 3, 4, 5, 6, 7};
+        System.out.println("Should be: 3");
+        System.out.println("Got      : " + sumArrayRange(anArray, 3));
+
+        anArray = new int[]{45, 43, 5, 6, 7, 23, 9, 6, 55, 4, 2, 32, 12};
+        System.out.println("Should be: 237");
+        System.out.println("Got      : " + sumArrayRange(anArray, 12));
+
+        anArray = new int[]{45, 43, 5, 6, 7, 23, 9, 6, 55, 4, 2, 32, 12};
+        System.out.println("Should be: 99");
+        System.out.println("Got      : " + sumArrayRange(anArray, 7));
+
+        anArray = new int[]{45, 43, 5, 6, 7, 23, 9, 6, 55, 4, 2, 32, 12};
+        System.out.println("Should be: 88");
+        System.out.println("Got      : " + sumArrayRange(anArray, 5));
+
+        anArray = new int[]{45, 43, 5, 6, 7, 23, 9, 6, 55, 4, 2, 32, 12};
+        System.out.println("Should be: 93");
+        System.out.println("Got      : " + sumArrayRange(anArray, 6));
+
+        anArray = new int[]{1, 2, 3, 4, 5, 6, 7};
+        System.out.println("Should be: 0");
+        System.out.println("Got      : " + sumArrayRange(anArray, 1));
+    }
+}
