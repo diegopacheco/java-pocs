556729896a180e52a3dba9db92a08d534ff685c3
diegopacheco <diego.pacheco.it@gmail.com>
2021-04-15 14:06:52 -0300

    BOM poc

---
+++ pocs/maven-bom-fun/bom-user/src/main/java/Main.java
@@ -0,0 +1,11 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main{
+
+  private static final Logger logger = LogManager.getLogger(Main.class);
+
+  public static void main(String args[]){
+    logger.info("It works!");
+  }
+}
