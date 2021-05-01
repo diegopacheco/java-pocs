21945a5a8594531c65c80b70bb751aed7fce2128
diegopacheco <diego.pacheco.it@gmail.com>
2021-05-01 18:28:16 -0300

    .

--- pocs/spring-restart-app/src/main/java/com/github/diegopacheco/sandboxspring/controller/RestartController.java
+++ pocs/spring-restart-app/src/main/java/com/github/diegopacheco/sandboxspring/controller/RestartController.java
@@ -2,7 +2,6 @@ package com.github.diegopacheco.sandboxspring.controller;
 
 import com.github.diegopacheco.sandboxspring.Application;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
