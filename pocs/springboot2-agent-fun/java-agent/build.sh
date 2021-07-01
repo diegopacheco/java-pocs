28647802a5cdc19756430880945aeb143ade7750
diegopacheco <diego.pacheco.it@gmail.com>
2021-07-01 02:09:01 -0300

    agent spring boot fun

---
+++ pocs/springboot2-agent-fun/java-agent/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash 
+
+./mvnw clean install assembly:single 
--- pocs/springboot2-agent-fun/java-agent/src/main/java/com/github/diegopacheco/javapocs/agent/Agent.java
+++ pocs/springboot2-agent-fun/java-agent/src/main/java/com/github/diegopacheco/javapocs/agent/Agent.java
@@ -1,2 +1,18 @@
-package com.github.diegopacheco.javapocs.agent;public class Agent {
+package com.github.diegopacheco.javapocs.agent;
+
+import java.lang.instrument.Instrumentation;
+import java.util.UUID;
+
+public class Agent {
+
+    public static void premain(String agentArgs, Instrumentation inst) {
+        String id = UUID.randomUUID().toString();
+        System.out.println("[premain] Agent ID["+id+"] running before main app... ");
+    }
+
+    public static void agentmain(String agentArgs, Instrumentation inst) {
+        String id = UUID.randomUUID().toString();
+        System.out.println("[agentmain] Agent ID["+id+"] running before main app... ");
+    }
+
 }
