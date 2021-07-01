164d3f4c7156b6939d96b76fb6ad8bd67c09a978
diegopacheco <diego.pacheco.it@gmail.com>
2021-07-01 02:24:58 -0300

    agents fun

--- pocs/springboot2-agent-fun/spring-boot-app/run.sh
+++ pocs/springboot2-agent-fun/spring-boot-app/run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 current_dir=$(pwd)
-export MAVEN_OPTS="-javaagent:$current_dir/../java-agent/target/java-agent-1.0-SNAPSHOT-jar-with-dependencies.jar -javaagent:$current_dir/../java-agent/target/java-agent-1.0-SNAPSHOT-jar-with-dependencies.jar"
-./mvnw spring-boot:run
+
+# option 1 - Before Maven
+#export MAVEN_OPTS="-javaagent:$current_dir/../java-agent/target/java-agent-1.0-SNAPSHOT-jar-with-dependencies.jar -javaagent:$current_dir/../java-agent/target/java-agent-1.0-SNAPSHOT-jar-with-dependencies.jar"
+#./mvnw spring-boot:run
+
+# option 2 - Before Spring Boot App
+./mvnw spring-boot:run -Dspring-boot.run.agents="../java-agent/target/java-agent-1.0-SNAPSHOT-jar-with-dependencies.jar"
