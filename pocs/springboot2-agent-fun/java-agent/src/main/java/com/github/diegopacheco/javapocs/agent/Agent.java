dff5573dbbce0293568c787db3d71a856d9ff05d
diegopacheco <diego.pacheco.it@gmail.com>
2021-07-01 03:09:23 -0300

    added background thread to proof agent is running

--- pocs/springboot2-agent-fun/java-agent/src/main/java/com/github/diegopacheco/javapocs/agent/Agent.java
+++ pocs/springboot2-agent-fun/java-agent/src/main/java/com/github/diegopacheco/javapocs/agent/Agent.java
@@ -6,8 +6,19 @@ import java.util.UUID;
 public class Agent {
 
     public static void premain(String agentArgs, Instrumentation inst) {
-        String id = UUID.randomUUID().toString();
+        final String id = UUID.randomUUID().toString();
         System.out.println("[premain] Agent ID["+id+"] running before main app... ");
+        new Thread(){
+            @Override
+            public void run() {
+                while(true){
+                    try{
+                        System.out.println("Agent ID["+id+"] running... ");
+                        Thread.sleep(3000L);
+                    }catch(Exception e){}
+                }
+            }
+        }.start();
     }
 
     public static void agentmain(String agentArgs, Instrumentation inst) {
