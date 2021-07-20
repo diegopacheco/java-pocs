27cbddefc8b98c14f155e128c8161c5bfa9f391b
diegopacheco <diego.pacheco.it@gmail.com>
2021-07-20 02:13:14 -0300

    tinylog fat jar

---
+++ pocs/tinylog-fatjar-shade-fun/run.sh
@@ -0,0 +1,4 @@
+
+#!/bin/bash
+
+java -jar target/tinylog-fatjar-shade-fun-1.0-SNAPSHOT.jar 
---
+++ pocs/tinylog-fatjar-shade-fun/src/main/java/Main.java
@@ -0,0 +1,26 @@
+import org.pmw.tinylog.Logger;
+
+import java.util.UUID;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+    public static void main(String args[]) {
+
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                Logger.info("App is going down! Bye. ");
+            }
+        });
+
+        Logger.info("Hello World from TinyLog 2");
+
+        ScheduledExecutorService ses = Executors.newScheduledThreadPool(1);
+        ses.scheduleAtFixedRate( () -> {
+            Logger.info(UUID.randomUUID().toString() + " - " + System.nanoTime());
+        }, 0, 5, TimeUnit.SECONDS);
+
+    }
+}
