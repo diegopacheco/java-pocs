d3400458f4a91cf4e5052dda8e0da3f5d60131be
diegopacheco <diego.pacheco.it@gmail.com>
2021-07-26 12:46:33 -0300

    Flyweight

---
+++ pocs/design-patterns/src/main/java/com/github/diegopacheco/design/patterns/structural/flyweight/Tree.java
@@ -0,0 +1,19 @@
+package com.github.diegopacheco.design.patterns.structural.flyweight;
+
+import java.awt.*;
+
+public class Tree {
+    private int x;
+    private int y;
+    private TreeType type;
+
+    public Tree(int x, int y, TreeType type) {
+        this.x = x;
+        this.y = y;
+        this.type = type;
+    }
+
+    public void draw(Graphics g) {
+        type.draw(g, x, y);
+    }
+}
