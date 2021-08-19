dd3410f3284ee9549cc27f14cc08d6e9e82a259e
diegopacheco <diego.pacheco.it@gmail.com>
2021-08-19 20:33:19 -0300

    metrics fun poc

--- pocs/custom-metrics-error-ob-fun/src/main/java/com/github/diegopacheco/sandboxspring/metrics/OperationMonitor.java
+++ pocs/custom-metrics-error-ob-fun/src/main/java/com/github/diegopacheco/sandboxspring/metrics/OperationMonitor.java
@@ -1,16 +1,21 @@
 package com.github.diegopacheco.sandboxspring.metrics;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.BinaryOperator;
 
 public class OperationMonitor {
 
+    private static Map<String, AtomicInteger> totalOPS = new ConcurrentHashMap<>();
     private static Map<String, AtomicInteger> okCounters = new ConcurrentHashMap<>();
     private static Map<String, AtomicInteger> errorCounters = new ConcurrentHashMap<>();
     private static Map<String, AtomicLong> latestLatencies = new ConcurrentHashMap<>();
     private static Map<String, AtomicInteger> avgLatencies = new ConcurrentHashMap<>();
+    private static Map<String, List<AtomicLong>> cumulativeLatencies = new ConcurrentHashMap<>();
     private static Map<String, Exception> lastExceptions = new ConcurrentHashMap<>();
 
     public static void recordException(String operation,Exception ex){
@@ -28,13 +33,29 @@ public class OperationMonitor {
             counter = new AtomicLong(0);
             latestLatencies.put(operation,counter);
         }
+
+        List<AtomicLong> cumulatives = cumulativeLatencies.get("cumulatives-"+operation);
+        if (null==cumulatives){
+            cumulatives= Arrays.asList(new AtomicLong(1));
+        }else{
+            cumulatives.add(new AtomicLong(timeInMS));
+        }
+
+        AtomicInteger total = totalOPS.get("total-"+operation);
+        if (null==total){
+            total = new AtomicInteger(1);
+        }
+        long avgLatency = total.get() / cumulatives.stream().map( al -> al.get() ).reduce(0L, Long::sum);
+        avgLatencies.put("avg-latency-"+operation,new AtomicInteger(new Long(avgLatency).intValue()));
     }
 
     public static void recordSuccess(String operation){
-        record("sucess-" + operation,okCounters);
+        record("total-" + operation,totalOPS);
+        record("success-" + operation,okCounters);
     }
 
     public static void recordError(String operation){
+        record("total-" + operation,totalOPS);
         record("error-" + operation,errorCounters);
     }
 
@@ -53,6 +74,7 @@ public class OperationMonitor {
 
     public static Map<String,Object> dump(){
         Map<String, Object> allMetrics = new ConcurrentHashMap<>();
+        allMetrics.putAll(totalOPS);
         allMetrics.putAll(okCounters);
         allMetrics.putAll(errorCounters);
         allMetrics.putAll(latestLatencies);
