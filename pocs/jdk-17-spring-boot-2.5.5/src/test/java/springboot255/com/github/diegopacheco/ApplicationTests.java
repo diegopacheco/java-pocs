0fa66cec966b19305486f0176bde2c0d33c9773f
diegopacheco <diego.pacheco.it@gmail.com>
2021-09-24 00:55:22 -0300

    jdk 17 + spring boot 2.5.5

---
+++ pocs/jdk-17-spring-boot-2.5.5/src/test/java/springboot255/com/github/diegopacheco/ApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot255.com.github.diegopacheco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
