de8e7ea1d2dc34f83b6d534929e99c37ed9c871c
diegopacheco <diego.pacheco.it@gmail.com>
2021-10-13 00:11:27 -0300

    initial project

---
+++ pocs/spring-boot-2x-data-jpa-h2-sequences/create.sql
@@ -0,0 +1,4 @@
+create sequence book_schema.book_seq start with 1 increment by 50
+create table book_schema.book (id bigint not null, author varchar(255), title varchar(255), primary key (id))
+create sequence book_schema.book_seq start with 1 increment by 50
+create table book_schema.book (id bigint not null, author varchar(255), title varchar(255), primary key (id))
---
+++ pocs/spring-boot-2x-data-jpa-h2-sequences/src/main/java/com/github/diegopacheco/sandboxspring/model/Book.java
@@ -0,0 +1,74 @@
+package com.github.diegopacheco.sandboxspring.model;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "book",schema = "book_schema")
+public class Book {
+
+    @Id
+    @GeneratedValue(generator = "bookSeqGenerator",strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "bookSeqGenerator", sequenceName = "book_seq", allocationSize = 50, initialValue = 1,
+    schema = "book_schema")
+    private Long id;
+
+    private String title;
+    private String author;
+
+    public Book(){}
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    public Book(Long id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(id, book.id) && Objects.equals(title, book.title) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, author);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
+}
