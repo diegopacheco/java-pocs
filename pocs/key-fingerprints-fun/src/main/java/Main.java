1dfe6b87d02d588a343175d2720f6337a013c69a
diegopacheco <diego.pacheco.it@gmail.com>
2021-10-14 02:22:58 -0300

    FingerPrint in java

--- pocs/key-fingerprints-fun/src/main/java/Main.java
+++ pocs/key-fingerprints-fun/src/main/java/Main.java
@@ -7,8 +7,8 @@ public class Main {
     public static void main(String args[]) {
         SecretKey key = generateKey();
         System.out.println("Key                  : " + toHex(key.getEncoded()));
-        System.out.println("FingerPrint (SHA256) : " + toHex(generateFingerPrint(key,"SHA256")));
         System.out.println("FingerPrint (MD5)    : " + toHex(generateFingerPrint(key,"MD5")));
+        System.out.println("FingerPrint (SHA256) : " + toHex(generateFingerPrint(key,"SHA256")));
     }
 
     private static byte[] generateFingerPrint(SecretKey key,String algo) {
