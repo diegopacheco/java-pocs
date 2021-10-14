6173d063d89cd4f4f673ee3c7cf20d64dd887fdf
diegopacheco <diego.pacheco.it@gmail.com>
2021-10-14 02:34:39 -0300

    FingerPrint in java

--- pocs/key-fingerprints-fun/src/main/java/Main.java
+++ pocs/key-fingerprints-fun/src/main/java/Main.java
@@ -5,13 +5,34 @@ import java.security.MessageDigest;
 public class Main {
 
     public static void main(String args[]) {
-        SecretKey key = generateKey();
-        System.out.println("Key                  : " + toHex(key.getEncoded()));
-        System.out.println("FingerPrint (MD5)    : " + toHex(generateFingerPrint(key,"MD5")));
-        System.out.println("FingerPrint (SHA256) : " + toHex(generateFingerPrint(key,"SHA256")));
+        SecretKey keyA = generateKey();
+        System.out.println("Key A                : " + toHex(keyA.getEncoded()));
+        System.out.println("FingerPrint (MD5)    : " + toHex(generateFingerPrint(keyA, "MD5")));
+        System.out.println("FingerPrint (SHA256) : " + toHex(generateFingerPrint(keyA, "SHA256")));
+
+        SecretKey keyB = generateKey();
+        System.out.println("Key B                : " + toHex(keyB.getEncoded()));
+        System.out.println("FingerPrint (MD5)    : " + toHex(generateFingerPrint(keyB, "MD5")));
+        System.out.println("FingerPrint (SHA256) : " + toHex(generateFingerPrint(keyB, "SHA256")));
+
+        System.out.println("Key A == Key B FingerPrints ? " + validate(keyA,keyB));
+        System.out.println("Key A == Key A FingerPrints ? " + validate(keyA,keyA));
+        System.out.println("Key B == Key B FingerPrints ? " + validate(keyB,keyB));
     }
 
-    private static byte[] generateFingerPrint(SecretKey key,String algo) {
+    private static boolean validate(SecretKey keyA, SecretKey keyB) {
+        try {
+            byte[] finger1 = generateFingerPrint(keyA,"MD5");
+            byte[] finger2 = generateFingerPrint(keyB,"MD5");
+            String hex1    = toHex(finger1);
+            String hex2    = toHex(finger2);
+            return hex1.equals(hex2);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static byte[] generateFingerPrint(SecretKey key, String algo) {
         try {
             MessageDigest digest = MessageDigest.getInstance(algo);
             byte[] result = digest.digest(key.getEncoded());
