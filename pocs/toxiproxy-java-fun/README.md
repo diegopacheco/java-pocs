6af6bc9a4cf8d233690b57bcbbc420d1c5b84d33
diegopacheco <diego.pacheco.it@gmail.com>
2021-10-27 23:04:56 -0300

    toxiproxy fun

--- pocs/toxiproxy-java-fun/README.md
+++ pocs/toxiproxy-java-fun/README.md
@@ -2,7 +2,7 @@
 ```bash
 ./mvnw clean install 
 ```
-### Run toxiproxy server
+### Run toxiproxy server - CLI Version (Optitional) since java client does that already
 ```bash
 ./toxiproxy-server-linux-amd64 
 ``` 
@@ -15,6 +15,4 @@ Add 1s latency per ops (since the app does 2 calls: SET and GET will take 2 seco
 toxiproxy-cli-linux-amd64 toxic add -t latency -a latency=1000 redis 
 ```
 ### Run 
-```bash
-./run.sh 
 ```
--- pocs/toxiproxy-java-fun/src/main/java/Main.java
+++ pocs/toxiproxy-java-fun/src/main/java/Main.java
@@ -1,3 +1,6 @@
+import eu.rekawek.toxiproxy.Proxy;
+import eu.rekawek.toxiproxy.ToxiproxyClient;
+import eu.rekawek.toxiproxy.model.ToxicDirection;
 import io.lettuce.core.RedisClient;
 import io.lettuce.core.api.StatefulRedisConnection;
 import io.lettuce.core.api.sync.RedisCommands;
@@ -6,10 +9,9 @@ import java.util.Date;
 
 public class Main{
   public static void main(String args[]){
-    RedisClient client = RedisClient.create("redis://localhost:26379");
-    StatefulRedisConnection<String, String> connection = client.connect();
-    RedisCommands<String, String> commands = connection.sync();
-    System.out.println("Connected to redis: redis://localhost:26379 via toxiproxy");
+
+    setupChaos();
+    RedisCommands<String, String> commands = setUpRedisConnection();
 
     while(true){
       try{
@@ -21,6 +23,26 @@ public class Main{
         throw new RuntimeException(e);
       }
     }
-
   }
+
+  private static void setupChaos(){
+    try{
+      ToxiproxyClient client = new ToxiproxyClient("127.0.0.1", 8474);
+      Proxy redisProxy = client.createProxy("redis", "127.0.0.1:26379", "127.0.0.1:6379");
+      redisProxy.toxics().latency("1s-latency-toxic", ToxicDirection.DOWNSTREAM, 1000).setJitter(0);
+      System.out.println("Redis Chaos Setup Done. ");
+      System.out.println(" * 1s latency per ops added. ");
+    }catch(Exception e){
+      System.out.println(e);
+    }
+  }
+
+  private static RedisCommands<String, String> setUpRedisConnection(){
+    RedisClient client = RedisClient.create("redis://localhost:26379");
+    StatefulRedisConnection<String, String> connection = client.connect();
+    RedisCommands<String, String> commands = connection.sync();
+    System.out.println("Connected to redis: redis://localhost:26379 via toxiproxy");
+    return commands;
+  }
+
 }
