9fc030189ac145d513ba5d1c4c6d7721c1982d74
diegopacheco <diego.pacheco.it@gmail.com>
2021-10-29 00:23:09 -0300

    Github co pilot fun - added images

--- pocs/github-co-pilot-java-fun/src/main/java/Main.java
+++ pocs/github-co-pilot-java-fun/src/main/java/Main.java
@@ -5,23 +5,28 @@ public class Main {
     public static void main(String[] args) {
        System.out.println("Hello World");     
        System.out.println(isOdd(24));
+       System.out.println(isEven(24));
+       System.out.println(generateTenRandomNames());
     }
 
     private static boolean isOdd(int number) {
         return number % 2 == 1;
     }
     
-    public static List<String> genearteTenNames() {
+    private static boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+    
+    private static List<String> generateTenRandomNames() {
         List<String> names = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
-            names.add(generateName());
+            names.add(generateRandomName());
         }
         return names;
     }
 
-    private static String generateName() {
-        return "Name" + Math.random();
+    private static String generateRandomName() {
+        return "name" + Math.random();
     }
-   
 
 }
