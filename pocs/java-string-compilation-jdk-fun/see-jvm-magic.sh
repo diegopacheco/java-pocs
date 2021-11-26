2f017a28ff515586cde2fa66b2d7fb34feeb1eff
diegopacheco <diego.pacheco.it@gmail.com>
2021-11-26 00:01:22 -0300

    JDK magic String improvement

---
+++ pocs/java-string-compilation-jdk-fun/see-jvm-magic.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+javap -v target.classes.Main 
---
+++ pocs/java-string-compilation-jdk-fun/src/main/java/Main.java
@@ -0,0 +1,10 @@
+
+public class Main{
+  public static void main(String args[]){
+    String sa = "Hi";
+    String sb = "This";
+    String sc = "works";
+    String result = sa + " " + sb + " " + sc;
+    System.out.println(result);
+  }
+}
