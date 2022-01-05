efe53566d3ddbf7a7cb94ddc5a471ee77d9b5468
diegopacheco <diego.pacheco.it@gmail.com>
2022-01-04 20:35:45 -0800

    lucene 7 working

---
+++ pocs/lucune-fun/src/main/java/Main.java
@@ -0,0 +1,50 @@
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.RAMDirectory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String args[]) throws Exception {
+
+        Directory memoryIndex = new RAMDirectory();
+        StandardAnalyzer analyzer = new StandardAnalyzer();
+        IndexWriterConfig indexWriterConfig = new IndexWriterConfig(analyzer);
+        IndexWriter writter = new IndexWriter(memoryIndex, indexWriterConfig);
+        Document document = new Document();
+        document.add(new TextField("title", "Conan the barb", Field.Store.YES));
+        document.add(new TextField("body", "stygian dogs!", Field.Store.YES));
+        writter.addDocument(document);
+        writter.close();
+
+        List<Document> documents =  searchIndex("title", "conan",memoryIndex,analyzer);
+        System.out.println(documents);
+    }
+
+    public static List<Document>  searchIndex(String inField, String queryString,Directory memoryIndex,StandardAnalyzer analyzer) throws Exception {
+        Query query = new QueryParser(inField, analyzer).parse(queryString);
+        IndexReader indexReader = DirectoryReader.open(memoryIndex);
+        IndexSearcher searcher = new IndexSearcher(indexReader);
+        TopDocs topDocs = searcher.search(query, 10);
+        List<Document> documents = new ArrayList<>();
+        for (ScoreDoc scoreDoc : topDocs.scoreDocs) {
+            documents.add(searcher.doc(scoreDoc.doc));
+        }
+        return documents;
+    }
+
+}
