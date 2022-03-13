09cea0af5e2fa6ff15d0f5ea7314601f416928c7
diegopacheco <diego.pacheco.it@gmail.com>
2022-03-12 21:18:38 -0800

    app working - clean up

--- pocs/hibernate-5-sequences-pocs/src/main/java/com/github/diegopacheco/hibernate/conf/HibernateConf.java
+++ pocs/hibernate-5-sequences-pocs/src/main/java/com/github/diegopacheco/hibernate/conf/HibernateConf.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.hibernate5.HibernateTransactionManager;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -26,28 +24,6 @@ public class HibernateConf {
     @Autowired
     DataSource dataSource;
 
-    //
-    // Hibernate
-    //
-
-    /*
-    @Bean(name="entityManagerFactory")
-    public LocalSessionFactoryBean sessionFactory() {
-        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-        sessionFactory.setDataSource(dataSource);
-        sessionFactory.setPackagesToScan(new String[]{"com.github.diegopacheco.hibernate.model"});
-        sessionFactory.setHibernateProperties(hibernateProperties());
-        return sessionFactory;
-    }
-
-    @Bean
-    public HibernateTransactionManager hibernateTransactionManager() {
-        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
-        transactionManager.setSessionFactory(sessionFactory().getObject());
-        return transactionManager;
-    }
-    */
-
     private final Properties hibernateProperties() {
         Properties hibernateProperties = new Properties();
         hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
@@ -55,11 +31,6 @@ public class HibernateConf {
         hibernateProperties.setProperty("hibernate.show_sql", "true");
         return hibernateProperties;
     }
-
-    //
-    // JPA
-    //
-
 
     @Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
