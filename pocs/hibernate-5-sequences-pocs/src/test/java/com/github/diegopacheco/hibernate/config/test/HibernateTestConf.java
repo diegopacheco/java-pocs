41fbcdd89af09e1da0e3dfe5efb60c8a2eb673ce
diegopacheco <diego.pacheco.it@gmail.com>
2022-03-12 18:28:16 -0800

    Added integration test with junit

---
+++ pocs/hibernate-5-sequences-pocs/src/test/java/com/github/diegopacheco/hibernate/config/test/HibernateTestConf.java
@@ -0,0 +1,44 @@
+package com.github.diegopacheco.hibernate.config.test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@EnableTransactionManagement
+public class HibernateTestConf {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+        sessionFactory.setPackagesToScan(new String[]{"com.github.diegopacheco.hibernate.model"});
+        sessionFactory.setHibernateProperties(hibernateProperties());
+        return sessionFactory;
+    }
+
+    private final Properties hibernateProperties() {
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+        hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+        return hibernateProperties;
+    }
+
+    @Bean
+    public PlatformTransactionManager hibernateTransactionManager() {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(sessionFactory().getObject());
+        return transactionManager;
+    }
+
+}
---
+++ pocs/hibernate-5-sequences-pocs/src/test/java/com/github/diegopacheco/hibernate/test/ContactDaoTest.java
@@ -0,0 +1,39 @@
+package com.github.diegopacheco.hibernate.test;
+
+import com.github.diegopacheco.hibernate.config.test.DBTestConf;
+import com.github.diegopacheco.hibernate.config.test.HibernateTestConf;
+import com.github.diegopacheco.hibernate.dao.ContactDao;
+import com.github.diegopacheco.hibernate.dao.DBFeeder;
+import com.github.diegopacheco.hibernate.service.ContactService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.transaction.Transactional;
+
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+        classes = {
+                DBTestConf.class,
+                HibernateTestConf.class,
+                ContactDao.class,
+                DBFeeder.class,
+                ContactService.class
+        }
+)
+public class ContactDaoTest {
+
+    @Autowired
+    ContactDao dao;
+
+    @Autowired
+    DBFeeder dbFeeder;
+
+    @Test
+    @Transactional
+    public void testList(){
+        dbFeeder.feedData();
+        dao.list().forEach(System.out::println);
+    }
+
+}
