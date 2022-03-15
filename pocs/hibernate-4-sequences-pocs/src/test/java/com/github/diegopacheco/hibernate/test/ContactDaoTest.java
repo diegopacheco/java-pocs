332c5b2999ae1083a8362eb9e8470b5c39682cbf
diegopacheco <diego.pacheco.it@gmail.com>
2022-03-14 21:55:28 -0700

    tests passing

--- pocs/hibernate-4-sequences-pocs/src/test/java/com/github/diegopacheco/hibernate/test/ContactDaoTest.java
+++ pocs/hibernate-4-sequences-pocs/src/test/java/com/github/diegopacheco/hibernate/test/ContactDaoTest.java
@@ -5,18 +5,16 @@ import com.github.diegopacheco.hibernate.config.test.HibernateTestConf;
 import com.github.diegopacheco.hibernate.dao.ContactDao;
 import com.github.diegopacheco.hibernate.dao.DBFeeder;
 import com.github.diegopacheco.hibernate.service.ContactService;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.ConfigFileApplicationContextInitializer;
-import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
 import javax.transaction.Transactional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {
@@ -50,7 +48,7 @@ public class ContactDaoTest {
         dao.list().forEach(System.out::println);
 
         System.out.println("Total records: " + dao.count());
-        assertEquals(10,dao.count());
+        assertEquals(new Integer(10),dao.count());
     }
 
 }
