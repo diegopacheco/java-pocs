9dc313a0d85ea8e9e315cb4612faf223642c8712
diegopacheco <diego.pacheco.it@gmail.com>
2022-03-28 15:26:34 -0700

    java hjson

--- pocs/hjson-java-simple-poc/src/main/java/Main.java
+++ pocs/hjson-java-simple-poc/src/main/java/Main.java
@@ -1,6 +1,32 @@
+import org.hjson.JsonObject;
+import org.hjson.JsonValue;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Main{
   public static void main(String args[]){
-    System.out.println("It works!");
+
+    try {
+      String base = new File(".").getCanonicalPath();
+
+      String readerOrHjsonString = new String(
+              Files.readAllBytes(Paths.get(base + "/src/main/resources/data.hjson")));
+
+      String jsonString = JsonValue.readHjson(readerOrHjsonString).toString();
+      System.out.println(jsonString);
+
+      JsonObject jsonObject = JsonValue.readHjson(readerOrHjsonString).asObject();
+      for (JsonObject.Member member : jsonObject) {
+        String name = member.getName();
+        JsonValue value = member.getValue();
+        System.out.println("name: " + name + " value: " + value);
+      }
+
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+
   }
 }
