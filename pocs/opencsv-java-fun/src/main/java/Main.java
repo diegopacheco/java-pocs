38c73663921fe040f1cb1a377aea1fff14926af7
diegopacheco <diego.pacheco.it@gmail.com>
2022-10-26 22:34:07 -0700

    java open csv fun

---
+++ pocs/opencsv-java-fun/src/main/java/Main.java
@@ -0,0 +1,18 @@
+import com.opencsv.CSVReader;
+
+import java.io.File;
+import java.io.FileReader;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main{
+  public static void main(String args[]) throws Exception{
+
+    String path = new File(".").getPath() + "/src/main/resources/";
+    try (CSVReader reader = new CSVReader(new FileReader(path+"cities.csv"))) {
+      List<String[]> r = reader.readAll();
+      r.forEach(x -> System.out.println(Arrays.toString(x)));
+    }
+    
+  }
+}
