3cae85329caa4c4e1db0df30112a206e219d015c
diegopacheco <diego.pacheco.it@gmail.com>
2023-07-07 23:22:07 -0700

    improve the code

--- pocs/hibernate-5-jdbc-driver-observability/src/main/java/com/github/diegopacheco/hibernate/driver/parser/SQLParser.java
+++ pocs/hibernate-5-jdbc-driver-observability/src/main/java/com/github/diegopacheco/hibernate/driver/parser/SQLParser.java
@@ -4,6 +4,7 @@ import net.sf.jsqlparser.parser.CCJSqlParserUtil;
 import net.sf.jsqlparser.statement.Statement;
 import net.sf.jsqlparser.statement.insert.Insert;
 import net.sf.jsqlparser.statement.select.Select;
+import net.sf.jsqlparser.statement.update.Update;
 import net.sf.jsqlparser.util.TablesNamesFinder;
 
 import java.util.List;
@@ -12,19 +13,25 @@ public class SQLParser {
 
     public static List<String>  extractTable(String query){
         try{
+            TablesNamesFinder tablesNamesFinder = new TablesNamesFinder();
             Statement statement = CCJSqlParserUtil.parse(query);
-            if (statement instanceof Select){
+            List<String> tableList = tablesNamesFinder.getTableList(statement);
+            return tableList;
+            /*if (statement instanceof Select){
                 Select selectStatement = (Select) statement;
-                TablesNamesFinder tablesNamesFinder = new TablesNamesFinder();
-                List<String> tableList = tablesNamesFinder.getTableList(selectStatement);
+
                 return tableList;
             } else if (statement instanceof Insert){
                 Insert insertStatement = (Insert) statement;
-                TablesNamesFinder tablesNamesFinder = new TablesNamesFinder();
                 List<String> tableList = tablesNamesFinder.getTableList(insertStatement);
+                return tableList;
+            } else if (statement instanceof Update){
+                Update updateStatement = (Update) statement;
+                List<String> tableList = tablesNamesFinder.getTableList(updateStatement);
                 return tableList;
             }
             return null;
+             */
         }catch(Exception e){
             throw new RuntimeException(e);
         }
