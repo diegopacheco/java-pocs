21af12c2de98db27426bcc5a1b45915b886b2834
diegopacheco <diego.pacheco.it@gmail.com>
2023-07-14 00:24:35 -0700

    .

--- pocs/java-log-splunk-fun/src/main/java/Main.java
+++ pocs/java-log-splunk-fun/src/main/java/Main.java
@@ -1,10 +1,13 @@
-import java.util.logging.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 public class Main{
-  public static void main(String args[]){
-    Logger logger = Logger.getLogger("MainApp");
+  public static void main(String args[]) throws Exception {
+    Logger logger = LoggerFactory.getLogger("splunk.logger.java");
     logger.info("This is going to splunk. app=mainapp env=local test=true");
-    logger.severe("This is a test error for log4j test app=mainapp env=local test=true");
+    logger.error("This is a test error for log4j test app=mainapp env=local test=true");
+    Thread.sleep(5000);
     System.out.println("done");
   }
 }
