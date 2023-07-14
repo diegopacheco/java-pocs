1ff1e5a2c6063a4905dc16ed0d95eaf55f3cf3c7
diegopacheco <diego.pacheco.it@gmail.com>
2023-07-14 02:44:04 -0700

    .

--- pocs/java-log-splunk-fun/src/main/java/Main.java
+++ pocs/java-log-splunk-fun/src/main/java/Main.java
@@ -1,14 +1,83 @@
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import java.io.IOException;
+import com.splunk.Application;
+import com.splunk.Service;
+import com.splunk.ServiceArgs;
 
-public class Main{
-  public static void main(String args[]) throws Exception {
-    Logger logger = LoggerFactory.getLogger("splunk.logger.java");
-    logger.info("This is going to splunk. app=mainapp env=local test=true");
-    logger.error("This is a test error for log4j test app=mainapp env=local test=true");
-    logger.error("here is a stack there " + new RuntimeException(new RuntimeException(new IOException("fake"))));
-    logger.info("DONE! app=mainapp env=local test=true");
-    Thread.sleep(5000);
-  }
+import javax.net.ssl.*;
+import java.io.IOException;
+import java.net.Socket;
+import java.security.cert.CertificateException;
+import java.util.logging.Logger;
+
+public class Main {
+    public static void main(String args[]) throws Exception {
+
+        /*
+        trustAllHosts();
+        ServiceArgs splunkArgs = new ServiceArgs();
+        splunkArgs.setUsername("admin");
+        splunkArgs.setPassword("<password>");
+        splunkArgs.setHost("127.0.0.1");
+        splunkArgs.setToken("5086f1e1-2bfd-4352-9b3f-cb11fcfcf099");
+        splunkArgs.setPort(8000);
+        Service service = Service.connect(splunkArgs);
+        for (Application app : service.getApplications().values()) {
+            System.out.println(app.getName());
+        }
+        */
+
+         */
+        Logger logger = Logger.getLogger(Main.class.getName());
+        logger.info("This is going to splunk. app=mainapp env=local test=true");
+        logger.severe("This is a test error for log4j test app=mainapp env=local test=true");
+        logger.severe("here is a stack there " + new RuntimeException(new RuntimeException(new IOException("fake"))));
+        logger.info("DONE! app=mainapp env=local test=true");
+        Thread.sleep(5000);
+    }
+
+    public static void trustAllHosts() {
+        try {
+            TrustManager[] trustAllCerts = new TrustManager[]{
+                    new X509ExtendedTrustManager() {
+                        @Override
+                        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                            return null;
+                        }
+                        @Override
+                        public void checkClientTrusted(java.security.cert.X509Certificate[] certs, String authType) {
+                        }
+                        @Override
+                        public void checkServerTrusted(java.security.cert.X509Certificate[] certs, String authType) {
+                        }
+                        @Override
+                        public void checkClientTrusted(java.security.cert.X509Certificate[] xcs, String string, Socket socket) throws CertificateException {
+                        }
+                        @Override
+                        public void checkServerTrusted(java.security.cert.X509Certificate[] xcs, String string, Socket socket) throws CertificateException {
+                        }
+                        @Override
+                        public void checkClientTrusted(java.security.cert.X509Certificate[] xcs, String string, SSLEngine ssle) throws CertificateException {
+                        }
+                        @Override
+                        public void checkServerTrusted(java.security.cert.X509Certificate[] xcs, String string, SSLEngine ssle) throws CertificateException {
+                        }
+                    }
+            };
+
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+
+            // Create all-trusting host name verifier
+            HostnameVerifier allHostsValid = new HostnameVerifier() {
+                @Override
+                public boolean verify(String hostname, SSLSession session) {
+                    return true;
+                }
+            };
+            // Install the all-trusting host verifier
+            HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
 }
