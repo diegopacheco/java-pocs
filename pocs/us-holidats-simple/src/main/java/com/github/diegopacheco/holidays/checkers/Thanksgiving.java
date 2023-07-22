4b7a817ea7377eb1669979bf8ae39e4c0aaf6534
diegopacheco <diego.pacheco.it@gmail.com>
2023-07-21 19:30:31 -0700

    fixed Thanksgiving

--- pocs/us-holidats-simple/src/main/java/com/github/diegopacheco/holidays/checkers/Thanksgiving.java
+++ pocs/us-holidats-simple/src/main/java/com/github/diegopacheco/holidays/checkers/Thanksgiving.java
@@ -3,20 +3,27 @@ package com.github.diegopacheco.holidays.checkers;
 import com.github.diegopacheco.holidays.DateUtils;
 import com.github.diegopacheco.holidays.Holiday;
 
+import java.time.DayOfWeek;
 import java.time.Instant;
 import java.time.LocalDate;
+import java.time.Month;
+import java.time.temporal.TemporalAdjusters;
 import java.util.Date;
 
 public class Thanksgiving implements Holiday {
     public boolean isHoliday(Date date) {
-        if (null==date){
+        if (null == date) {
             return false;
         }
         LocalDate localDate = DateUtils.fromDate(date);
-        LocalDate lastMondayOfMonth = DateUtils.lastThursdayOfMonth(date);
+        LocalDate november = LocalDate.of(localDate.getYear(), Month.NOVEMBER, 1);
+        LocalDate fourthThursdayInNov = november.with(TemporalAdjusters.firstInMonth(DayOfWeek.THURSDAY))
+                                                 .with(TemporalAdjusters.next(DayOfWeek.THURSDAY))
+                                                 .with(TemporalAdjusters.next(DayOfWeek.THURSDAY))
+                                                 .with(TemporalAdjusters.next(DayOfWeek.THURSDAY));
 
         Instant parameterInstant = date.toInstant();
-        Instant nthInstant = lastMondayOfMonth.atStartOfDay(DateUtils.getCurrentZoneId()).toInstant();
-        return nthInstant.equals(parameterInstant);
+        Instant holidayInstant = fourthThursdayInNov.atStartOfDay(DateUtils.getCurrentZoneId()).toInstant();
+        return holidayInstant.equals(parameterInstant);
     }
 }
