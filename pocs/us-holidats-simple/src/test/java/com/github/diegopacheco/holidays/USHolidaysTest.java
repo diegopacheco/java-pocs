3b5150745c790d2a49c32ff18c18793336c496b3
diegopacheco <diego.pacheco.it@gmail.com>
2023-07-21 17:09:59 -0700

    mlk passing

--- pocs/us-holidats-simple/src/test/java/com/github/diegopacheco/holidays/USHolidaysTest.java
+++ pocs/us-holidats-simple/src/test/java/com/github/diegopacheco/holidays/USHolidaysTest.java
@@ -2,9 +2,7 @@ package com.github.diegopacheco.holidays;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -41,6 +39,26 @@ public class USHolidaysTest {
         Date notHoliday = new GregorianCalendar(2023, Calendar.JANUARY, 16).getTime();
         boolean result = USHolidays.isHoliday(notHoliday);
         assertTrue(result);
+    }
+
+    @Test
+    public void birthdayMLKFor10Years(){
+        List<Date> mlkHolidays = new ArrayList<>();
+        mlkHolidays.add(new GregorianCalendar(2024, Calendar.JANUARY, 15).getTime());
+        mlkHolidays.add(new GregorianCalendar(2025, Calendar.JANUARY, 20).getTime());
+        mlkHolidays.add(new GregorianCalendar(2026, Calendar.JANUARY, 19).getTime());
+        mlkHolidays.add(new GregorianCalendar(2027, Calendar.JANUARY, 18).getTime());
+        mlkHolidays.add(new GregorianCalendar(2028, Calendar.JANUARY, 17).getTime());
+        mlkHolidays.add(new GregorianCalendar(2029, Calendar.JANUARY, 15).getTime());
+        mlkHolidays.add(new GregorianCalendar(2030, Calendar.JANUARY, 21).getTime());
+        mlkHolidays.add(new GregorianCalendar(2031, Calendar.JANUARY, 20).getTime());
+        mlkHolidays.add(new GregorianCalendar(2032, Calendar.JANUARY, 19).getTime());
+        mlkHolidays.add(new GregorianCalendar(2033, Calendar.JANUARY, 17).getTime());
+
+        for(Date d: mlkHolidays){
+            boolean result = USHolidays.isHoliday(d);
+            assertTrue(result);
+        }
     }
 
 }
