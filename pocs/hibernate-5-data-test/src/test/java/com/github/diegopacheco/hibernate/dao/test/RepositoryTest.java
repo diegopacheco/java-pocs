15f47d726e3c66fe813cc0ec710449d057be082f
diegopacheco <diego.pacheco.it@gmail.com>
2023-08-04 00:54:51 -0700

    .

--- pocs/hibernate-5-data-test/src/test/java/com/github/diegopacheco/hibernate/dao/test/RepositoryTest.java
+++ pocs/hibernate-5-data-test/src/test/java/com/github/diegopacheco/hibernate/dao/test/RepositoryTest.java
@@ -5,31 +5,27 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-
-//@ExtendWith(SpringExtension.class)
-//@DataJpaTest
-/*@SpringBootTest(
-        webEnvironment = SpringBootTest.WebEnvironment.MOCK,
-        classes = {
-                DBTestConf.class,
-                HibernateTestConf.class,
-                ContactRepository.class,
-        }
-)*/
-//@DataJpaTest
-//@ContextConfiguration(classes=ContactRepository.class)
-//@EnableAutoConfiguration
 @ExtendWith(SpringExtension.class)
-@DataJpaTest
-public class ContactRepositoryTest {
-    @Autowired private ContactRepository contactRepository;
+@DataJpaTest(includeFilters=@ComponentScan.Filter(type = FilterType.ANNOTATION, classes = ContactRepository.class))
+public class RepositoryTest {
+
+    @Autowired
+    TestEntityManager entityManager;
+
+    @Autowired
+    ContactRepository contactRepository;
 
     @Test
     public void injectedComponentsAreNotNullTest(){
-        assertThat(contactRepository).isNotNull();
+       assertThat(entityManager).isNotNull();
+       assertThat(contactRepository).isNotNull();
     }
 }
+
---
+++ pocs/hibernate-5-data-test/src/test/java/com/github/diegopacheco/hibernate/dao/test/TestApplication.java
@@ -0,0 +1,8 @@
+package com.github.diegopacheco.hibernate.dao.test;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Really necessary or you will get:
+// Unable to find a @SpringBootConfiguration, you need to use @ContextConfiguration or @SpringBootTest(classes=...)
+@SpringBootApplication
+public class TestApplication {}
