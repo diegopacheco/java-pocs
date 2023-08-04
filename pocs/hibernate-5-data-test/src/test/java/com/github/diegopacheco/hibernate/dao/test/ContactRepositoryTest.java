b1ccad8bd25e3966225db314909572e5fbe70cd7
diegopacheco <diego.pacheco.it@gmail.com>
2023-08-04 00:29:04 -0700

    .

--- pocs/hibernate-5-data-test/src/test/java/com/github/diegopacheco/hibernate/dao/test/ContactRepositoryTest.java
+++ pocs/hibernate-5-data-test/src/test/java/com/github/diegopacheco/hibernate/dao/test/ContactRepositoryTest.java
@@ -1,67 +1,35 @@
 package com.github.diegopacheco.hibernate.dao.test;
 
-import com.github.diegopacheco.hibernate.config.test.DBTestConf;
-import com.github.diegopacheco.hibernate.config.test.HibernateTestConf;
-import com.github.diegopacheco.hibernate.dao.ContactDao;
 import com.github.diegopacheco.hibernate.dao.ContactRepository;
-import com.github.diegopacheco.hibernate.dao.DBFeeder;
-import com.github.diegopacheco.hibernate.service.ContactService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import javax.persistence.EntityManager;
-import javax.sql.DataSource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+
 //@ExtendWith(SpringExtension.class)
-//@DataJpaTest
-/*@SpringBootTest(
-        webEnvironment = SpringBootTest.WebEnvironment.MOCK,
-        classes = {
-                //DBTestConf.class,
-                //HibernateTestConf.class,
-                ContactDao.class,
-                //DBFeeder.class,
-                ContactService.class
-        }
-)*/
 //@DataJpaTest
 /*@SpringBootTest(
         webEnvironment = SpringBootTest.WebEnvironment.MOCK,
         classes = {
                 DBTestConf.class,
                 HibernateTestConf.class,
-                ContactDao.class,
-                ContactService.class
+                ContactRepository.class,
         }
 )*/
-//@ComponentScan("com.github.diegopacheco")
-//@ContextConfiguration(classes={DBTestConf.class,HibernateTestConf.class,ContactRepository.class})
+//@DataJpaTest
+//@ContextConfiguration(classes=ContactRepository.class)
+//@EnableAutoConfiguration
+@ExtendWith(SpringExtension.class)
 @DataJpaTest
-//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.AUTO_CONFIGURED)
-//@Import(HibernateTestConf.class)
-//@ExtendWith(SpringExtension.class)
-//@TestPropertySource(properties = {
-//        "spring.jpa.hibernate.ddl-auto=validate"
-//})
 public class ContactRepositoryTest {
-    @Autowired private TestEntityManager entityManager;
     @Autowired private ContactRepository contactRepository;
 
     @Test
     public void injectedComponentsAreNotNullTest(){
-        assertThat(entityManager).isNotNull();
         assertThat(contactRepository).isNotNull();
     }
 }
