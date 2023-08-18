5e370a32e7779c4648293548de3f474a3d4c5b43
diegopacheco <diego.pacheco.it@gmail.com>
2023-08-18 15:45:45 -0700

    advanced ConcurrentHashMap fun

---
+++ pocs/concurrent-hashmap-fun/src/main/java/Main.java
@@ -0,0 +1,23 @@
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Main{
+  public static void main(String args[]){
+    Map<String,String> capitals = new ConcurrentHashMap<>();
+    capitals.put("RS","Porto Alegre");
+    capitals.put("SC","Florianopolis");
+    capitals.put("CA","Sacramento");
+    capitals.put("NY","New York City");
+    capitals.put("CO","Bolder");
+    System.out.println(capitals.keySet());
+
+    capitals.putIfAbsent("PR","Curitiba");
+    System.out.println(capitals.get("PR"));
+    System.out.println(capitals.keySet());
+
+    capitals.replace("CO","Bolder","Denver");
+    System.out.println(capitals.get("CO"));
+    System.out.println(capitals.keySet());
+
+  }
+}
