eeae266e013928b11aa4a47dd176e4981f8c2972
diegopacheco <diego.pacheco.it@gmail.com>
2023-08-21 00:13:14 -0700

    lazy

--- pocs/lazy-fp-fun/src/main/java/Main.java
+++ pocs/lazy-fp-fun/src/main/java/Main.java
@@ -1,5 +1,11 @@
 
 public class Main {
+    // Book and Order are highly coupled to each other.
+    // However, both can be constructed in an atomic way, without temporal coupling
+    // To do so we need a third object and a bit of functional programing
+    //
+    // A Builder would solve the problem with more simplicity
+    // 
     public static void main(String args[]) {
         final var book = new Lazy<Book>(bookRef -> {
             final var order = new Lazy<>(() -> new Order(bookRef, "en"));
