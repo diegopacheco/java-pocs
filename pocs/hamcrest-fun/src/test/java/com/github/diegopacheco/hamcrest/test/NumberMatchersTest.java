c560756b3467a75c55fc6d30f00fa16d519169c7
diegopacheco <diego.pacheco.it@gmail.com>
2023-08-23 00:50:27 -0700

    hamcrest fun

--- pocs/hamcrest-fun/src/test/java/com/github/diegopacheco/hamcrest/test/NumberMatchersTest.java
+++ pocs/hamcrest-fun/src/test/java/com/github/diegopacheco/hamcrest/test/NumberMatchersTest.java
@@ -3,8 +3,7 @@ package com.github.diegopacheco.hamcrest.test;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.greaterThanOrEqualTo;
-import static org.hamcrest.Matchers.lessThan;
+import static org.hamcrest.Matchers.*;
 
 public class NumberMatchersTest {
     @Test
@@ -16,5 +15,15 @@ public class NumberMatchersTest {
     public void lessThanTest() {
         assertThat(-1, lessThan(0));
     }
-    
+
+    @Test
+    public void lessThanOrEqualToTest() {
+        assertThat(-1, lessThanOrEqualTo(5));
+    }
+
+    @Test
+    public void closeTest() {
+        assertThat(1.2, closeTo(1, 0.5));
+    }
+
 }
