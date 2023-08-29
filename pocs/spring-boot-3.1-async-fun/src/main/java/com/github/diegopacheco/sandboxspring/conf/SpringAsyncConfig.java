1a8c4a09b51d18afde4ccf00cab3bcfab1d61999
diegopacheco <diego.pacheco.it@gmail.com>
2023-08-28 23:25:31 -0700

    async

---
+++ pocs/spring-boot-3.1-async-fun/src/main/java/com/github/diegopacheco/sandboxspring/conf/SpringAsyncConfig.java
@@ -0,0 +1,19 @@
+package com.github.diegopacheco.sandboxspring.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class SpringAsyncConfig {
+
+    @Bean(name = "threadPoolTaskExecutor")
+    public Executor threadPoolTaskExecutor() {
+        return new ThreadPoolTaskExecutor();
+    }
+
+}
--- pocs/spring-boot-3.1-async-fun/src/main/java/com/github/diegopacheco/sandboxspring/controller/HelloController.java
+++ pocs/spring-boot-3.1-async-fun/src/main/java/com/github/diegopacheco/sandboxspring/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.github.diegopacheco.sandboxspring.controller;
 
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,7 +9,18 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
+		asyncMethodWithVoidReturnType();
 		return "Greetings from Spring Boot!";
+	}
+
+	@Async
+	public void asyncMethodWithVoidReturnType() {
+		try{
+			System.out.println("Execute method asynchronously. " + Thread.currentThread().getName());
+			Thread.sleep(5L);
+		}catch(Exception e){
+			throw new RuntimeException(e);
+		}
 	}
 
 }
