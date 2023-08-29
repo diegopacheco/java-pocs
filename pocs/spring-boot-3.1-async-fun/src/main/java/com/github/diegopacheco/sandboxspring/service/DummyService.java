faf1364d8bc6727a2b6b4341b557eee4cb02d3e1
diegopacheco <diego.pacheco.it@gmail.com>
2023-08-29 00:22:28 -0700

    async

--- pocs/spring-boot-3.1-async-fun/src/main/java/com/github/diegopacheco/sandboxspring/service/DummyService.java
+++ pocs/spring-boot-3.1-async-fun/src/main/java/com/github/diegopacheco/sandboxspring/service/DummyService.java
@@ -1,17 +1,22 @@
 package com.github.diegopacheco.sandboxspring.service;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
-
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
 
 @Service
 public class DummyService {
 
+    @Autowired
+    private Executor executor;
+
     @Async
     public CompletableFuture<String> blowIt(){
-        return CompletableFuture.<String>failedFuture(new RuntimeException("I dont like it"));
+        return CompletableFuture.<String>supplyAsync( () -> {
+            throw new RuntimeException("I dont like it");
+        },executor);
     }
 
 }
