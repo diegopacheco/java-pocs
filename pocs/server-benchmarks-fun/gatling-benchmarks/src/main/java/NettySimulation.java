8ad0d036b889f01a57ade6a6fd2a466de7bd26e2
diegopacheco <diego.pacheco.it@gmail.com>
2023-08-31 03:13:12 -0700

    working

---
+++ pocs/server-benchmarks-fun/gatling-benchmarks/src/main/java/NettySimulation.java
@@ -0,0 +1,28 @@
+import io.gatling.javaapi.core.*;
+import io.gatling.javaapi.http.*;
+import static io.gatling.javaapi.core.CoreDsl.*;
+import static io.gatling.javaapi.http.HttpDsl.*;
+
+public class NettySimulation extends Simulation {
+
+    HttpProtocolBuilder httpProtocol = http
+            .baseUrl("http://localhost:8080/")
+            .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+            .doNotTrackHeader("1")
+            .acceptLanguageHeader("en-US,en;q=0.5")
+            .acceptEncodingHeader("gzip, deflate")
+            .userAgentHeader("Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0");
+
+    ScenarioBuilder scn = scenario("NettySimulation")
+            .exec(http("request")
+                    .get("/"))
+            .pause(5);
+
+    {
+        setUp(scn.
+                injectOpen(
+                        constantUsersPerSec(1000).during(60)
+                )
+        ).protocols(httpProtocol);
+    }
+}
