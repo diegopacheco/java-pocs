55d08494c4c4f8f2747f7e4fd76e80019eb5ec98
diegopacheco <diego.pacheco.it@gmail.com>
2023-08-31 02:42:00 -0700

    working

--- pocs/server-benchmarks-fun/server-micronaut/src/main/java/com/github/diegopacheco/micronaut/UUIDController.java
+++ pocs/server-benchmarks-fun/server-micronaut/src/main/java/com/github/diegopacheco/micronaut/UUIDController.java
@@ -3,15 +3,13 @@ package com.github.diegopacheco.micronaut;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
-import io.micronaut.http.annotation.Produces;
 
 import java.util.UUID;
 
 @Controller("/uuid")
 public class UUIDController {
 
-    @Get("/")
-    @Produces(MediaType.TEXT_PLAIN)
+    @Get(produces = MediaType.TEXT_PLAIN)
     public String index() {
         return UUID.randomUUID().toString();
     }
