77d5f33ed225bc817e54d2c372c633b56b12c840
diegopacheco <diego.pacheco.it@gmail.com>
2023-08-31 01:23:14 -0700

    micronaut progress

---
+++ pocs/server-benchmarks-fun/server-micronaut/src/main/java/UUIDController.java
@@ -0,0 +1,14 @@
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+
+import java.util.UUID;
+
+@Controller("/")
+public class UUIDController {
+
+    @Get("/")
+    public String index() {
+        return UUID.randomUUID().toString();
+    }
+
+}
