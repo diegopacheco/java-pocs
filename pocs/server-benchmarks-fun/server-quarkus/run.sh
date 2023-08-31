ec6f0f2b3d0248fe2010ecbdb613a88efe7eb0fa
diegopacheco <diego.pacheco.it@gmail.com>
2023-08-31 02:55:54 -0700

    working

---
+++ pocs/server-benchmarks-fun/server-quarkus/run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+
+quarkus dev
--- pocs/server-benchmarks-fun/server-quarkus/src/main/java/UUIDResource.java
+++ pocs/server-benchmarks-fun/server-quarkus/src/main/java/UUIDResource.java
@@ -1,2 +1,16 @@
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+import java.util.UUID;
+
+@Path("/")
 public class UUIDResource {
+
+        @GET
+        @Produces(MediaType.TEXT_PLAIN)
+        public String index(){
+            return UUID.randomUUID().toString();
+        }
 }
