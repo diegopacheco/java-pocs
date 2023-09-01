9ed3702ed684493515ee97d9723dcaa223ca7bb3
diegopacheco <diego.pacheco.it@gmail.com>
2023-08-31 22:39:30 -0700

    nodejs/express version

---
+++ pocs/server-benchmarks-fun/nodejs-20-express/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const uuid = require('uuid');
+
+const v4options = {
+    random: [
+      0x10, 0x91, 0x56, 0xbe, 0xc4, 0xfb, 0xc1, 0xea, 0x71, 0xb4, 0xef, 0xe1, 0x67, 0x1c, 0x58, 0x36,
+    ],
+  };
+
+app.get('/', (req, res) => {
+  res.send(uuid.v4(v4options))
+})
+
+app.listen(port, () => {
+  console.log(`NodeJS 20 - Express, listening on port ${port}`)
+})
