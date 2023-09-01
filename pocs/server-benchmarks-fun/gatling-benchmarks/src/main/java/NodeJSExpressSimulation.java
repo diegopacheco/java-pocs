34204f38757ecd60317201d2b848fbe6f31318e6
diegopacheco <diego.pacheco.it@gmail.com>
2023-08-31 23:15:25 -0700

    nodejs/express version

---
+++ pocs/server-benchmarks-fun/gatling-benchmarks/src/main/java/NodeJSExpressSimulation.java
@@ -0,0 +1,31 @@
+import io.gatling.javaapi.core.ScenarioBuilder;
+import io.gatling.javaapi.core.Simulation;
+import io.gatling.javaapi.http.HttpProtocolBuilder;
+
+import static io.gatling.javaapi.core.CoreDsl.constantUsersPerSec;
+import static io.gatling.javaapi.core.CoreDsl.scenario;
+import static io.gatling.javaapi.http.HttpDsl.http;
+
+public class NodeJSExpressSimulation extends Simulation {
+
+    HttpProtocolBuilder httpProtocol = http
+            .baseUrl("http://localhost:3000/")
+            .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+            .doNotTrackHeader("1")
+            .acceptLanguageHeader("en-US,en;q=0.5")
+            .acceptEncodingHeader("gzip, deflate")
+            .userAgentHeader("Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0");
+
+    ScenarioBuilder scn = scenario("NodeJSExpressSimulation")
+            .exec(http("request")
+                    .get("/"))
+            .pause(5);
+
+    {
+        setUp(scn.
+                injectOpen(
+                        constantUsersPerSec(1_000).during(60)
+                )
+        ).protocols(httpProtocol);
+    }
+}
