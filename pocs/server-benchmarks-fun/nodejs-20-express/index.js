6f77f81d799c12ac6a1bc85be383e9c9a7f02a8b
diegopacheco <diego.pacheco.it@gmail.com>
2023-08-31 23:18:40 -0700

    nodejs/express version

--- pocs/server-benchmarks-fun/nodejs-20-express/index.js
+++ pocs/server-benchmarks-fun/nodejs-20-express/index.js
@@ -1,16 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const uuid = require('uuid');
-
-const v4options = {
-    random: [
-      0x10, 0x91, 0x56, 0xbe, 0xc4, 0xfb, 0xc1, 0xea, 0x71, 0xb4, 0xef, 0xe1, 0x67, 0x1c, 0x58, 0x36,
-    ],
-  };
+const crypto = require('crypto');
 
 app.get('/', (req, res) => {
-  res.send(uuid.v1(v4options))
+  const uuid = crypto.randomUUID();
+  res.send(uuid);
 })
 
 app.listen(port, () => {
