a86d045d4a836b46e1176b988feb33b368f49425
diegopacheco <diego.pacheco.it@gmail.com>
2023-09-01 00:27:11 -0700

    Netty SpringWebFlux Mono

--- pocs/server-benchmarks-fun/server-boot-netty/src/main/java/com/github/diegopacheco/sandboxspring/controller/UUIDController.java
+++ pocs/server-benchmarks-fun/server-boot-netty/src/main/java/com/github/diegopacheco/sandboxspring/controller/UUIDController.java
@@ -2,16 +2,22 @@ package com.github.diegopacheco.sandboxspring.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 import java.util.UUID;
 
 @RestController
 public class UUIDController {
 
-	@RequestMapping("/")
-	public String index() {
-		return UUID.randomUUID().toString();
-	}
+    @RequestMapping("/")
+    public String index() {
+        return UUID.randomUUID().toString();
+    }
+
+    @RequestMapping("/r")
+    public Mono<String> reactive() {
+        return Mono.just(UUID.randomUUID().toString());
+    }
 
 }
 
