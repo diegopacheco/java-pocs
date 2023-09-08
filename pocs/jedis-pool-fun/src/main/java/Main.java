92385c4ebb0bfd27b720972da3d8779d641ad7a1
diegopacheco <diego.pacheco.it@gmail.com>
2023-09-08 10:16:38 -0700

    initial commit

--- pocs/jedis-pool-fun/src/main/java/Main.java
+++ pocs/jedis-pool-fun/src/main/java/Main.java
@@ -1,6 +1,16 @@
+import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisCluster;
 
-public class Main{
-  public static void main(String args[]){
-    System.out.println("It works!");
-  }
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+    public static void main(String args[]) {
+        Set<HostAndPort> jedisClusterNodes = new HashSet<HostAndPort>();
+        jedisClusterNodes.add(new HostAndPort("127.0.0.1", 6379));
+        JedisCluster cluster = new JedisCluster(jedisClusterNodes);
+
+
+    }
 }
