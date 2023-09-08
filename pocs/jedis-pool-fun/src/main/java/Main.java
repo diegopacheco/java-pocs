4034dd76f3666a4cade1f5594bf504a3f06f83ad
diegopacheco <diego.pacheco.it@gmail.com>
2023-09-08 10:27:40 -0700

    jedis pool

--- pocs/jedis-pool-fun/src/main/java/Main.java
+++ pocs/jedis-pool-fun/src/main/java/Main.java
@@ -1,11 +1,25 @@
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPooled;
 
 public class Main {
     public static void main(String args[]) {
         poolTryWithResource();
+        poolNoTry();
+    }
+
+    private static void poolNoTry() {
+        System.out.println("poolNoTry");
+        JedisPool pool = new JedisPool("localhost", 6379);
+        try (Jedis jedis = pool.getResource()) {
+            jedis.set("www", "444");
+            System.out.println(jedis.get("www"));
+        }
+        pool.close();
     }
 
     private static void poolTryWithResource(){
+        System.out.println("poolTryWithResource");
         try (JedisPooled pool = new JedisPooled("localhost", 6379)) {
             pool.set("x", "10");
             pool.set("y", "20");
