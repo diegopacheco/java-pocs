9b6dfb5c481c9929ba62d9686580b1ca87495aa2
diegopacheco <diego.pacheco.it@gmail.com>
2023-09-13 00:53:31 -0700

    jedis cluster poc

---
+++ pocs/jedis-cluster-docker/redis-cluster-clean-up.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+docker volume rm redis-cluster_redis-cluster_data-0
+docker volume rm redis-cluster_redis-cluster_data-1
+docker volume rm redis-cluster_redis-cluster_data-2
+docker volume rm redis-cluster_redis-cluster_data-3
+docker volume rm redis-cluster_redis-cluster_data-4
+docker volume rm redis-cluster_redis-cluster_data-5
+docker volume rm redis-cluster_redis-cluster_data-6
