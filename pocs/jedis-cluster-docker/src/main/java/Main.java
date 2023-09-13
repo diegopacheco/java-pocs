62aeed7f78a3510dc493a671b7f099645e9abec4
diegopacheco <diego.pacheco.it@gmail.com>
2023-09-12 23:26:35 -0700

    first commit

--- pocs/jedis-cluster-docker/src/main/java/Main.java
+++ pocs/jedis-cluster-docker/src/main/java/Main.java
@@ -1,6 +1,28 @@
+import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.JedisCluster;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main{
+
+  private static final JedisCluster jedisCluster;
+
+  static{
+    synchronized (Main.class){
+      Set<HostAndPort> jedisClusterNodes = new HashSet<HostAndPort>();
+      jedisClusterNodes.add(new HostAndPort("127.0.0.1", 6371));
+      jedisClusterNodes.add(new HostAndPort("127.0.0.1", 6372));
+      jedisClusterNodes.add(new HostAndPort("127.0.0.1", 6373));
+      jedisClusterNodes.add(new HostAndPort("127.0.0.1", 6374));
+      jedisClusterNodes.add(new HostAndPort("127.0.0.1", 6375));
+      jedisClusterNodes.add(new HostAndPort("127.0.0.1", 6376));
+      jedisCluster = new JedisCluster(jedisClusterNodes,null,"bitnami");
+    }
+  }
+
   public static void main(String args[]){
-    
+      jedisCluster.set("x","value_x");
+      System.out.println(jedisCluster.get("x"));
   }
 }
