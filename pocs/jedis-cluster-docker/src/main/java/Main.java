f1dfa840f54864179edd1a00d35d9a4722415173
diegopacheco <diego.pacheco.it@gmail.com>
2023-09-13 00:07:28 -0700

    jedis cluster poc

--- pocs/jedis-cluster-docker/src/main/java/Main.java
+++ pocs/jedis-cluster-docker/src/main/java/Main.java
@@ -4,7 +4,6 @@ import redis.clients.jedis.JedisCluster;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -29,14 +28,15 @@ public class Main {
     private static void runStats() {
         for (String key : Arrays.asList("192.168.32.2:6379", "192.168.32.3:6379", "192.168.32.4:6379",
                 "192.168.32.5:6379", "192.168.32.6:6379", "192.168.32.7:6379")) {
-            System.out.println("key : " + key + " ***");
-            System.out.println(" max-total           : " + jedisCluster.getClusterNodes().get(key).getMaxTotal());
-            System.out.println(" active              : " + jedisCluster.getClusterNodes().get(key).getNumActive());
-            System.out.println(" mean-active-ms      : " + jedisCluster.getClusterNodes().get(key).getMeanActiveTimeMillis());
-            System.out.println(" borrow              : " + jedisCluster.getClusterNodes().get(key).getBorrowedCount());
-            System.out.println(" mean-borrow-wait-ms : " + jedisCluster.getClusterNodes().get(key).getMeanBorrowWaitTimeMillis());
-            System.out.println(" max-wait-ms         : " + jedisCluster.getClusterNodes().get(key).getMaxWaitMillis());
+            System.out.print("key : " + key + " - ");
+            System.out.print(" max-total           : " + jedisCluster.getClusterNodes().get(key).getMaxTotal());
+            System.out.print(" active              : " + jedisCluster.getClusterNodes().get(key).getNumActive());
+            System.out.print(" mean-active-ms      : " + jedisCluster.getClusterNodes().get(key).getMeanActiveTimeMillis());
+            System.out.print(" borrow              : " + jedisCluster.getClusterNodes().get(key).getBorrowedCount());
+            System.out.print(" mean-borrow-wait-ms : " + jedisCluster.getClusterNodes().get(key).getMeanBorrowWaitTimeMillis());
+            System.out.print(" max-wait-ms         : " + jedisCluster.getClusterNodes().get(key).getMaxWaitMillis());
         }
+        System.out.println("***");
     }
 
     private static void generateLoad(int amount) {
@@ -48,9 +48,11 @@ public class Main {
     public static void main(String args[]) {
         ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
         scheduler.scheduleAtFixedRate(() -> {
-            generateLoad(100);
-            System.out.println(jedisCluster.get("x:x1"));
-            runStats();
-        }, 0, 5, TimeUnit.SECONDS);
+            generateLoad(1_000);
+            jedisCluster.get("x:x1");
+        }, 0, 1, TimeUnit.SECONDS);
+
+        ScheduledExecutorService schedulerStats = Executors.newScheduledThreadPool(1);
+        schedulerStats.scheduleAtFixedRate(Main::runStats, 0, 1, TimeUnit.SECONDS);
     }
 }
