626412bae84d10e8122e6f084323fb7544b48d43
diegopacheco <diego.pacheco.it@gmail.com>
2023-09-12 23:49:44 -0700

    jedis cluster poc

--- pocs/jedis-cluster-docker/src/main/java/Main.java
+++ pocs/jedis-cluster-docker/src/main/java/Main.java
@@ -1,6 +1,7 @@
 import redis.clients.jedis.HostAndPort;
 import redis.clients.jedis.JedisCluster;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -21,8 +22,23 @@ public class Main{
     }
   }
 
+  private static void runStats(){
+    for(String key: Arrays.asList("192.168.32.2:6379","192.168.32.3:6379","192.168.32.4:6379",
+            "192.168.32.5:6379","192.168.32.6:6379","192.168.32.7:6379")){
+      System.out.println("key : " + key + " ********************");
+      System.out.println(" active              : " + jedisCluster.getClusterNodes().get(key).getNumActive());
+      System.out.println(" mean-active-ms      : " + jedisCluster.getClusterNodes().get(key).getMeanActiveTimeMillis());
+      System.out.println(" borrow              : " + jedisCluster.getClusterNodes().get(key).getBorrowedCount());
+      System.out.println(" mean-borrow-wait-ms : " + jedisCluster.getClusterNodes().get(key).getMeanBorrowWaitTimeMillis());
+      System.out.println(" max-wait-ms         : " + jedisCluster.getClusterNodes().get(key).getMaxWaitMillis());
+    }
+  }
+
   public static void main(String args[]){
-      jedisCluster.set("x","value_x");
-      System.out.println(jedisCluster.get("x"));
+      jedisCluster.set("x:x1","value_x");
+      jedisCluster.set("x:x1","value_y");
+      jedisCluster.set("x:x1","value_x");
+      System.out.println(jedisCluster.get("x:x1"));
+      runStats();
   }
 }
