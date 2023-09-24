97734ffae9aeae34fb8ae7cefb207bf7fc6daa7f
diegopacheco <diego.pacheco.it@gmail.com>
2023-09-24 04:19:17 -0700

    lets blade java

--- lets-blade-fun/src/main/java/Main.java
+++ lets-blade-fun/src/main/java/Main.java
@@ -1,6 +1,7 @@
+import com.hellokaton.blade.Blade;
 
-public class Main{
-  public static void main(String args[]){
-    System.out.println("It works!");
-  }
+public class Main {
+    public static void main(String[] args) {
+        Blade.create().get("/", ctx -> ctx.text("Hello Blade")).start();
+    }
 }
