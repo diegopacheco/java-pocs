647c6d5a943027e7e79ea56efac42d9d7d6006d1
diegopacheco <diego.pacheco.it@gmail.com>
2023-10-11 00:22:14 -0700

    for range

--- pocs/for-range/src/main/java/Main.java
+++ pocs/for-range/src/main/java/Main.java
@@ -4,12 +4,23 @@ import java.util.stream.IntStream;
 
 public class Main{
   public static void main(String args[]){
-
-    List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
-
-    for (int i: IntStream.range(0,numbers.size()).toArray()){
-      System.out.println(i);
-    }
-
+    approachOne();
+    approachTwo();
   }
+
+  private static void approachOne(){
+    List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+    for (int i: IntStream.range(0,numbers.size()).toArray()){
+      System.out.print(i);
+    }
+    System.out.println("");
+  }
+
+  private static void approachTwo(){
+    IntStream.range(0, 10).forEachOrdered(n -> {
+      System.out.print(n);
+    });
+    System.out.println("");
+  }
+
 }
