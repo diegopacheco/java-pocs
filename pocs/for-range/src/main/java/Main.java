ca1a11e4a5a2be60350686957ea9f93c88f829fb
diegopacheco <diego.pacheco.it@gmail.com>
2023-10-11 00:24:44 -0700

    for range

--- pocs/for-range/src/main/java/Main.java
+++ pocs/for-range/src/main/java/Main.java
@@ -2,25 +2,33 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.IntStream;
 
-public class Main{
-  public static void main(String args[]){
-    approachOne();
-    approachTwo();
-  }
-
-  private static void approachOne(){
-    List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
-    for (int i: IntStream.range(0,numbers.size()).toArray()){
-      System.out.print(i);
+public class Main {
+    public static void main(String args[]) {
+        approachOne();
+        approachTwo();
+        approachThree();
     }
-    System.out.println("");
-  }
 
-  private static void approachTwo(){
-    IntStream.range(0, 10).forEachOrdered(n -> {
-      System.out.print(n);
-    });
-    System.out.println("");
-  }
+    private static void approachOne() {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        for (int i : IntStream.range(0, numbers.size()).toArray()) {
+            System.out.print(i);
+        }
+        System.out.println("");
+    }
+
+    private static void approachTwo() {
+        IntStream.range(0, 10).forEachOrdered(n -> {
+            System.out.print(n);
+        });
+        System.out.println("");
+    }
+
+    private static void approachThree(){
+      for(int i: Range.of(10)){
+        System.out.print(i);
+      }
+      System.out.println("");
+    }
 
 }
--- pocs/for-range/src/main/java/Range.java
+++ pocs/for-range/src/main/java/Range.java
@@ -1,2 +1,43 @@
-public class Range {
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+class Range implements Iterable<Integer> {
+
+    private int limit;
+
+    public Range(int limit) {
+        this.limit = limit;
+    }
+
+    public static Range of(int to){
+        return new Range(to);
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        final int max = limit;
+        return new Iterator<Integer>() {
+
+            private int current = 0;
+
+            @Override
+            public boolean hasNext() {
+                return current < max;
+            }
+
+            @Override
+            public Integer next() {
+                if (hasNext()) {
+                    return current++;
+                } else {
+                    throw new NoSuchElementException("Range reached the end");
+                }
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException("Can't remove values from a Range");
+            }
+        };
+    }
 }
