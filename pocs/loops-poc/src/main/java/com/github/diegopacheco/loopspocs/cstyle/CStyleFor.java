53cbd438b2fc14b6c86d5751745e284fe7062618
diegopacheco <diego.pacheco.it@gmail.com>
2023-10-24 21:18:57 -0700

    added loops

--- pocs/loops-poc/src/main/java/com/github/diegopacheco/loopspocs/cstyle/CStyleFor.java
+++ pocs/loops-poc/src/main/java/com/github/diegopacheco/loopspocs/cstyle/CStyleFor.java
@@ -2,9 +2,24 @@ package com.github.diegopacheco.loopspocs.cstyle;
 
 public class CStyleFor {
     public static void main(String[] args) {
+        doubleVar();
+        infiniteLoop();
+    }
+
+    private static void doubleVar() {
         int count=0;
-        for(int i=0;i<100;i++){
+        int x=0;
+        for(int i=0;i<100;i++,x++){
             count++;
+        }
+        System.out.println("total is " + count);
+    }
+
+    private static void infiniteLoop() {
+        int count=0;
+        for(;;){
+            count++;
+            if (count==100) break;
         }
         System.out.println("total is " + count);
     }
