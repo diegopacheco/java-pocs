69743cfa5eff57747e9b6baaf69b76e9dc9bcdde
diegopacheco <diego.pacheco.it@gmail.com>
2023-10-24 20:55:33 -0700

    added loops

---
+++ pocs/loops-poc/src/main/java/com/github/diegopacheco/loopspocs/cstyle/CStyleFor.java
@@ -0,0 +1,11 @@
+package com.github.diegopacheco.loopspocs.cstyle;
+
+public class CStyleFor {
+    public static void main(String[] args) {
+        int count=0;
+        for(int i=0;i<100;i++){
+            count++;
+        }
+        System.out.println("total is " + count);
+    }
+}
---
+++ pocs/loops-poc/src/main/java/com/github/diegopacheco/loopspocs/whilestyle/WhileStyle.java
@@ -0,0 +1,13 @@
+package com.github.diegopacheco.loopspocs.whilestyle;
+
+public class WhileStyle {
+    public static void main(String[] args) {
+        int i=0;
+        int count=0;
+        while(i<100){
+            count++;
+            i++;
+        }
+        System.out.println("total is " + count);
+    }
+}
