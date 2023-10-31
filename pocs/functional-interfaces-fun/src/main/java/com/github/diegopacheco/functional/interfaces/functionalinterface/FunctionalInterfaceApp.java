e949e2d71abdec32432ab621209dd606720a0cb8
diegopacheco <diego.pacheco.it@gmail.com>
2023-10-31 00:19:29 -0700

    functional interface

--- pocs/functional-interfaces-fun/src/main/java/com/github/diegopacheco/functional/interfaces/functionalinterface/FunctionalInterfaceApp.java
+++ pocs/functional-interfaces-fun/src/main/java/com/github/diegopacheco/functional/interfaces/functionalinterface/FunctionalInterfaceApp.java
@@ -2,7 +2,29 @@ package com.github.diegopacheco.functional.interfaces.functionalinterface;
 
 public class FunctionalInterfaceApp {
     public static void main(String[] args) {
+        innerClassStyle();
+        lambdaStyle();
+        methodReference();
+    }
+
+    public static void innerClassStyle(){
+        DoubleToInt dbl = new DoubleToInt() {
+            @Override
+            public Integer convert(Double input) {
+                return input.intValue();
+            }
+        };
+        System.out.println(dbl.convert(42.0));
+    }
+
+    public static void lambdaStyle(){
+        DoubleToInt dbl = (Double v) -> v.intValue();
+        System.out.println(dbl.convert(42.0));
+    }
+
+    public static void methodReference(){
         DoubleToInt dbl = Double::intValue;
         System.out.println(dbl.convert(42.0));
     }
+
 }
