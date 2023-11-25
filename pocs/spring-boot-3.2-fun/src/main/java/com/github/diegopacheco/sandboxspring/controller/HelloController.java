76537c513fae864a98cb45bf712faa643ffc972d
diegopacheco <diego.pacheco.it@gmail.com>
2023-11-25 02:29:03 -0800

    spring boot 3.2.0

--- pocs/spring-boot-3.2-fun/src/main/java/com/github/diegopacheco/sandboxspring/controller/HelloController.java
+++ pocs/spring-boot-3.2-fun/src/main/java/com/github/diegopacheco/sandboxspring/controller/HelloController.java
@@ -1,14 +1,20 @@
 package com.github.diegopacheco.sandboxspring.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.concurrent.ExecutorService;
 
 @RestController
 public class HelloController {
 
+    @Autowired
+    private ExecutorService executor;
+
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "Greetings from Spring Boot 3.2.0! " + executor.toString();
     }
 
 }
