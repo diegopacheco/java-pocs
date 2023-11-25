0c39cf5d3fa9248e03b86268dfe6e24c8326b4d2
diegopacheco <diego.pacheco.it@gmail.com>
2023-11-25 02:03:37 -0800

    Pattern Matcher with Sealed interfaces vs IFs

--- pocs/spring-boot-3.2-fun/src/main/java/Main.java
+++ pocs/spring-boot-3.2-fun/src/main/java/Main.java
@@ -1,6 +1,31 @@
 
 public class Main{
   public static void main(String args[]){
-    System.out.println("It works!");
+    Guitar g = new FlyingV("white");
+    System.out.println(getFamousPlayerV1(g));
+    System.out.println(getFamousPlayerV2(g));
   }
+
+  private static String getFamousPlayerV1(Guitar guitar){
+    if (guitar instanceof LesPaul){
+        return "Famous LesPaul player is Slash";
+    }
+    if (guitar instanceof FlyingV){
+      return "Famous FlyingV player is James Hatfield";
+    }
+    return null;
+  }
+
+  private static String getFamousPlayerV2(Guitar guitar){
+    return switch (guitar){
+      case FlyingV(var color) -> "Famous FlyingV "  + color + "player is James Hatfield";
+      case LesPaul ignored -> "Famous LesPaul player is Slash";
+      // if comment out LesPaul - Error - 'switch' expression does not cover all possible input values
+    };
+  }
+
 }
+
+sealed interface Guitar permits LesPaul, FlyingV{};
+final class LesPaul implements Guitar{};
+record FlyingV(String color) implements Guitar{};
