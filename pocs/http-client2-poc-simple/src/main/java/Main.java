ac24f442acf1f4171a4079d22a6b44293b4c0f8b
diegopacheco <diego.pacheco.it@gmail.com>
2023-12-06 01:34:13 -0800

    start coding http client v2

--- pocs/http-client2-poc-simple/src/main/java/Main.java
+++ pocs/http-client2-poc-simple/src/main/java/Main.java
@@ -1,6 +1,43 @@
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 
-public class Main{
-  public static void main(String args[]){
-    System.out.println("It works!");
-  }
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Main {
+    public static void main(String args[]) {
+
+    }
+
+    private void httpClientV2CallZip() {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet("https://www.example.com/file.zip");
+        try (CloseableHttpResponse response = httpClient.execute(httpGet)) {
+            HttpEntity entity = response.getEntity();
+            if (entity != null) {
+                try (InputStream inputStream = entity.getContent();
+                     FileOutputStream fileOutputStream = new FileOutputStream("file.zip")) {
+                    int read;
+                    byte[] buffer = new byte[4096];
+                    while ((read = inputStream.read(buffer)) != -1) {
+                        fileOutputStream.write(buffer, 0, read);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                httpClient.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println("done.");
+        }
+    }
+
 }
