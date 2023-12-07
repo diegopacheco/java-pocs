2107e9084b26fc6627ae5c66705244f932110120
diegopacheco <diego.pacheco.it@gmail.com>
2023-12-06 17:35:58 -0800

    .

--- pocs/selenium-download-file/src/main/java/Main.java
+++ pocs/selenium-download-file/src/main/java/Main.java
@@ -1,25 +1,37 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.CapabilityType;
 
-public class Main{
-  public static void main(String args[]){
+import java.io.File;
+import java.util.HashMap;
 
+public class Main {
+    public static void main(String args[]) throws Exception {
+        donwloadFile();
+    }
 
+    private static void donwloadFile() throws Exception {
 
+        HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
+        String downloadPath = new File(".").getCanonicalPath() + "/target/";
+        chromePrefs.put("profile.default_content_settings.popups", 0);
+        chromePrefs.put("download.default_directory", downloadPath);
 
-  }
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("headless");
 
-  private static void donwloadFile(){
-    ChromeOptions options = new ChromeOptions();
-    options.addArguments("headless");
-    WebDriver driver = new ChromeDriver(options);
+        options.setExperimentalOption("prefs", chromePrefs);
+        options.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
+        options.setCapability(ChromeOptions.CAPABILITY, options);
 
-    driver.get("https://simplemaps.com/static/data/us-cities/1.77/" +
-    "basic/simplemaps_uscities_basicv1.77.zip");
-    
-    driver.quit();
-    System.out.println("done");
-  }
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.get("https://simplemaps.com/static/data/us-cities/1.77/" +
+                "basic/simplemaps_uscities_basicv1.77.zip");
+
+        driver.quit();
+        System.out.println("done");
+    }
 
 }
