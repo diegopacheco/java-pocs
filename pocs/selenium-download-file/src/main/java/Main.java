4eb2af6adbac7afad1caf55ee6d76736b14f0a5b
diegopacheco <diego.pacheco.it@gmail.com>
2023-12-06 17:29:21 -0800

    .

--- pocs/selenium-download-file/src/main/java/Main.java
+++ pocs/selenium-download-file/src/main/java/Main.java
@@ -1,6 +1,25 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Main{
   public static void main(String args[]){
-    System.out.println("It works!");
+
+
+
+
   }
+
+  private static void donwloadFile(){
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("headless");
+    WebDriver driver = new ChromeDriver(options);
+
+    driver.get("https://simplemaps.com/static/data/us-cities/1.77/" +
+    "basic/simplemaps_uscities_basicv1.77.zip");
+    
+    driver.quit();
+    System.out.println("done");
+  }
+
 }
