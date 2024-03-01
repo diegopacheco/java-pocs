8a15a2a0499f88da0a3677412af9f7695c345909
diegopacheco <diego.pacheco.it@gmail.com>
2024-03-01 01:22:38 -0800

    java poc - credit card validations - improve poc code

--- pocs/credit-card-validation/src/main/java/CreditCard.java
+++ pocs/credit-card-validation/src/main/java/CreditCard.java
@@ -1,24 +1,30 @@
 public enum CreditCard {
-    VISA("VISA"),
-    MASTER("MASTER"),
-    AMEX("AMEX"),
-    DINER("DINER"),
-    DISCOVERY("DISCOVERY"),
-    JCB("JCB"),
-    OTHER("OTHER");
+    VISA("VISA","^4[0-9]{12}(?:[0-9]{3})?$"),
+    MASTER("MASTER","^5[1-5][0-9]{14}$"),
+    AMEX("AMEX", "^3[47][0-9]{13}$"),
+    DINER("DINER", "^3(?:0[0-5]|[68][0-9])[0-9]{11}$"),
+    DISCOVERY("DISCOVERY", "^6(?:011|5[0-9]{2})[0-9]{12}$"),
+    JCB("JCB", "^(?:2131|1800|35\\d{3})\\d{11}$"),
+    OTHER("OTHER","");
 
     private String company;
+    private String regex;
 
-    CreditCard(String company){
+    CreditCard(String company,String regex){
         this.company = company;
+        this.regex = regex;
     }
 
     public String getCompany(){
         return company;
+    }
+    public String getRegex() {
+        return regex;
     }
 
     @Override
     public String toString() {
         return company;
     }
+
 }
--- pocs/credit-card-validation/src/main/java/Main.java
+++ pocs/credit-card-validation/src/main/java/Main.java
@@ -16,7 +16,7 @@ public class Main{
     String commonRegex      = "^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\\d{3})\\d{11})$";
     try {
       ccNumber = ccNumber.replaceAll("\\D", "");
-      if (ccNumber.matches(visaRegex)){
+      if (ccNumber.matches(CreditCard.VISA.getRegex())){
          return CreditCard.VISA;
       }
       if (ccNumber.matches(masterRegex)){
