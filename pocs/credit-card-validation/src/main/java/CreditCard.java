f33eb10abc36ff36c896e55bc5322fbfdbec2f6a
diegopacheco <diego.pacheco.it@gmail.com>
2024-03-01 01:30:44 -0800

    java poc - credit card validations - improve poc code

--- pocs/credit-card-validation/src/main/java/CreditCard.java
+++ pocs/credit-card-validation/src/main/java/CreditCard.java
@@ -1,3 +1,5 @@
+import java.util.Optional;
+
 public enum CreditCard {
     VISA("VISA","^4[0-9]{12}(?:[0-9]{3})?$"),
     MASTER("MASTER","^5[1-5][0-9]{14}$"),
@@ -8,6 +10,7 @@ public enum CreditCard {
     OTHER("OTHER","");
 
     private String company;
+    private String number;
     private String regex;
 
     CreditCard(String company,String regex){
@@ -15,11 +18,49 @@ public enum CreditCard {
         this.regex = regex;
     }
 
+    public static Optional<CreditCard> discover(String ccNumber){
+        try {
+            CreditCard cc = CreditCard.OTHER;
+            cc.number = ccNumber;
+            ccNumber = ccNumber.replaceAll("\\D", "");
+            if (ccNumber.matches(CreditCard.VISA.regex)){
+                cc.company = CreditCard.VISA.company;
+                return Optional.of(cc);
+            }
+            if (ccNumber.matches(CreditCard.MASTER.regex)){
+                cc.company = CreditCard.MASTER.company;
+                return Optional.of(cc);
+            }
+            if (ccNumber.matches(CreditCard.AMEX.regex)){
+                cc.company = CreditCard.AMEX.company;
+                return Optional.of(cc);
+            }
+            if (ccNumber.matches(CreditCard.DINER.regex)){
+                cc.company = CreditCard.DINER.company;
+                return Optional.of(cc);
+            }
+            if (ccNumber.matches(CreditCard.DISCOVERY.regex)){
+                cc.company = CreditCard.DISCOVERY.company;
+                return Optional.of(cc);
+            }
+            if (ccNumber.matches(CreditCard.JCB.regex)){
+                cc.company = CreditCard.JCB.company;
+                return Optional.of(cc);
+            }
+            return Optional.empty();
+        } catch (Exception e) {
+            throw new IllegalStateException("IDK this CC");
+        }
+    }
+
     public String getCompany(){
         return company;
     }
     public String getRegex() {
         return regex;
+    }
+    public String getNumber() {
+        return number;
     }
 
     @Override
--- pocs/credit-card-validation/src/main/java/Main.java
+++ pocs/credit-card-validation/src/main/java/Main.java
@@ -1,36 +1,12 @@
 /**
  * Source: https://stackoverflow.com/questions/19510416/credit-card-validator-for-java
  */
-public class Main{
-  public static void main(String args[]){
-    System.out.println("CC 4111111111111111 = " + getCCType("4111111111111111"));
-  }
-
-  public static CreditCard getCCType(String ccNumber){
-    String commonRegex      = "^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\\d{3})\\d{11})$";
-    try {
-      ccNumber = ccNumber.replaceAll("\\D", "");
-      if (ccNumber.matches(CreditCard.VISA.getRegex())){
-         return CreditCard.VISA;
-      }
-      if (ccNumber.matches(CreditCard.MASTER.getRegex())){
-        return CreditCard.MASTER;
-      }
-      if (ccNumber.matches(CreditCard.AMEX.getRegex())){
-        return CreditCard.AMEX;
-      }
-      if (ccNumber.matches(CreditCard.DINER.getRegex())){
-        return CreditCard.DINER;
-      }
-      if (ccNumber.matches(CreditCard.DISCOVERY.getRegex())){
-        return CreditCard.DISCOVERY;
-      }
-      if (ccNumber.matches(CreditCard.JCB.getRegex())){
-        return CreditCard.JCB;
-      }
-      return CreditCard.OTHER;
-    } catch (Exception e) {
-      throw new IllegalStateException("IDK this CC");
+public class Main {
+    public static void main(String args[]) {
+        System.out.println("CC 4111111111111111 = " + getCCType("4111111111111111"));
     }
-  }
+
+    public static CreditCard getCCType(String ccNumber) {
+        return CreditCard.discover(ccNumber).get();
+    }
 }
