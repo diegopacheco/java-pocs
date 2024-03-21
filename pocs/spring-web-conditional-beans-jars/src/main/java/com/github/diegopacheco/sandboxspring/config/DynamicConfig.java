e928458c49aa775b5e38a871529d57cc7a682764
diegopacheco <diego.pacheco.it@gmail.com>
2024-03-21 00:12:59 -0700

    Condditional bean poc

---
+++ pocs/spring-web-conditional-beans-jars/src/main/java/com/github/diegopacheco/sandboxspring/config/DynamicConfig.java
@@ -0,0 +1,8 @@
+package com.github.diegopacheco.sandboxspring.config;
+
+import org.springframework.context.annotation.Configuration;
+
+// @ConditionalOnClass(name = "this.clazz.does.not.Exist")
+@Configuration
+public class DynamicConfig {
+}
