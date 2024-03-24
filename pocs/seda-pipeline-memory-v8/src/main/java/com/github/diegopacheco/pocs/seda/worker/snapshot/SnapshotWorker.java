3f45359085882089edecbceada6797f8e66ff350
diegopacheco <diego.pacheco.it@gmail.com>
2024-03-24 00:21:53 -0700

    seda poc v8 - snapshoting

--- pocs/seda-pipeline-memory-v8/src/main/java/com/github/diegopacheco/pocs/seda/worker/snapshot/SnapshotWorker.java
+++ pocs/seda-pipeline-memory-v8/src/main/java/com/github/diegopacheco/pocs/seda/worker/snapshot/SnapshotWorker.java
@@ -5,15 +5,19 @@ import com.github.diegopacheco.pocs.seda.worker.Worker;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Map;
 import java.util.UUID;
 
 public class SnapshotWorker implements Worker {
 
-    private Map<Queues, Map<UUID,Runnable>> processes;
+    private Map<Queues, Map<UUID, Runnable>> processes;
     private Gson gson;
 
-    public SnapshotWorker(Map<Queues, Map<UUID,Runnable>> processes) {
+    public SnapshotWorker(Map<Queues, Map<UUID, Runnable>> processes) {
         this.processes = processes;
 
         GsonBuilder builder = new GsonBuilder();
@@ -24,13 +28,27 @@ public class SnapshotWorker implements Worker {
     @Override
     public void run() {
         System.out.println("[SnapshotWorker] Snapshotting... ");
-        for (Queues queue : processes.keySet()){
+        for (Queues queue : processes.keySet()) {
             System.out.println("Total [" + processes.get(queue).keySet().size() +
                     "] workers pending per queue: " + queue);
-            for (UUID workerID : processes.get(queue).keySet()){
-                Worker worker = (Worker)processes.get(queue).get(workerID);
+            for (UUID workerID : processes.get(queue).keySet()) {
+                Worker worker = (Worker) processes.get(queue).get(workerID);
                 String json = gson.toJson(worker);
+                persist(queue+"_"+workerID.toString(),json);
             }
+        }
+    }
+
+    private void persist(String fileName, String content) {
+        String path = null;
+        try {
+            path = new File(".").getCanonicalPath() + "/target/snapshot/";
+            BufferedWriter writer = new BufferedWriter(new FileWriter(path + fileName));
+            writer.write(content);
+            writer.close();
+            System.out.println("[SnapshotWorker] persisted " + fileName);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
