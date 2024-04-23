220cfa7133a9838a640bcedd3e8e072fad206704
diegopacheco <diego.pacheco.it@gmail.com>
2024-04-22 18:02:15 -0700

    Kafka Streams poc

--- pocs/kafka-streams-window-dedup/README.md
+++ pocs/kafka-streams-window-dedup/README.md
@@ -6,3 +6,5 @@
 Java: 21
 Kafka: kafka_2.13-3.6.2
 Zookeeper: zookeeper-3.4.14
+### Result
+<img src="result.png" />
--- pocs/kafka-streams-window-dedup/produce-message-kafka.sh
+++ pocs/kafka-streams-window-dedup/produce-message-kafka.sh
@@ -3,5 +3,7 @@
 HERE=$(pwd)
 KAFKA_HOME=/home/diego/bin/kafka_2.13-3.6.2/
 cd $KAFKA_HOME 
-echo "diego likes kafka" | bin/kafka-console-producer.sh --topic inputTopic --bootstrap-server localhost:29092
+echo "diego diego likes like kafka kafka " | bin/kafka-console-producer.sh --topic inputTopic --bootstrap-server localhost:29092
+echo "diego diego likes like kafka kafka " | bin/kafka-console-producer.sh --topic inputTopic --bootstrap-server localhost:29092
+echo "diego diego likes like kafka kafka " | bin/kafka-console-producer.sh --topic inputTopic --bootstrap-server localhost:29092
 cd $HERE 
