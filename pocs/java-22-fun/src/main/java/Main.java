195f1a7203d8de1f0692dea1640fc7aa82df8638
diegopacheco <diego.pacheco.it@gmail.com>
2024-05-18 02:49:58 -0700

    java 22

--- pocs/java-22-fun/src/main/java/Main.java
+++ pocs/java-22-fun/src/main/java/Main.java
@@ -1,6 +1,9 @@
 import com.github.diegopacheco.jeps.*;
 
 // 463: Implicitly Declared Classes and Instance Main Methods (Second Preview)
+//
+// To run on IntelliJ idea need to add VM Options: --add-modules jdk.incubator.vector
+//
 void main(){
     UnnamedVariablePatterns.run();
     StringTemplates.run();
