dc40abbebfedaaec2651ad77437ae351a881327a
diegopacheco <diego.pacheco.it@gmail.com>
2024-05-22 00:17:06 -0700

    added events

---
+++ pocs/bookkeeping-auditability-records/src/main/java/com/github/diegopacheco/bookkeeping/events/SignUpEvent.java
@@ -0,0 +1,51 @@
+package com.github.diegopacheco.bookkeeping.events;
+
+import java.time.Instant;
+import java.util.Objects;
+import java.util.UUID;
+
+public class SignUpEvent {
+
+    private UUID userId;
+    private Instant timestamp;
+    private String eventDescription;
+
+    public SignUpEvent(UUID userId, Instant timestamp, String eventDescription) {
+        this.userId = userId;
+        this.timestamp = timestamp;
+        this.eventDescription = eventDescription;
+    }
+
+    public UUID getUserId() {
+        return userId;
+    }
+    public Instant getTimestamp() {
+        return timestamp;
+    }
+    public String getEventDescription() {
+        return eventDescription;
+    }
+
+    @Override
+    public String toString() {
+        return "SignUpEvent{" +
+                "userId=" + userId +
+                ", timestamp=" + timestamp +
+                ", eventDescription='" + eventDescription + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SignUpEvent that = (SignUpEvent) o;
+        return Objects.equals(userId, that.userId) && Objects.equals(timestamp, that.timestamp) && Objects.equals(eventDescription, that.eventDescription);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, timestamp, eventDescription);
+    }
+}
