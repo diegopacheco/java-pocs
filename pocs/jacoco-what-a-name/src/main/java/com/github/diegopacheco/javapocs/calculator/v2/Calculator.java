a14a7529354ace2687a17edb0f3e37a2151c7653
diegopacheco <diego.pacheco.it@gmail.com>
2024-05-25 01:07:52 -0700

    jacoco

---
+++ pocs/jacoco-what-a-name/src/main/java/com/github/diegopacheco/javapocs/calculator/v2/Calculator.java
@@ -0,0 +1,8 @@
+package com.github.diegopacheco.javapocs.calculator.v2;
+
+public interface Calculator {
+    int sum(int a, int b);
+    int sub(int a, int b);
+    int mult(int a, int b);
+    int div(int a, int b);
+}
