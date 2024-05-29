631f21a5e86397e6312b17d9cc949dca85df3d83
diegopacheco <diego.pacheco.it@gmail.com>
2024-05-28 23:07:27 -0700

    poc

--- pocs/java-21-spring-boot-3.x-spring-data-mysql/src/main/java/com/github/diegopacheco/sandboxspring/infra/ConnectionRefresher.java
+++ pocs/java-21-spring-boot-3.x-spring-data-mysql/src/main/java/com/github/diegopacheco/sandboxspring/infra/ConnectionRefresher.java
@@ -1,19 +1,31 @@
 package com.github.diegopacheco.sandboxspring.infra;
 
+import com.github.diegopacheco.sandboxspring.conf.DBConfiguration;
 import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Component
-public class ConnectionRefresher {
+public class ConnectionRefresher implements ApplicationContextAware {
+
+    ApplicationContext ctx;
 
     @Autowired
     private PlatformTransactionManager txManager;
 
     @Autowired
     private HikariDataSource ds;
+
+    @Autowired
+    private DBConfiguration configuration;
 
     private boolean done = false;
 
@@ -23,6 +35,14 @@ public class ConnectionRefresher {
             System.out.println("Refreshing connection... TX manager: " + txManager + " - DS: " + ds);
             ds.getHikariPoolMXBean().softEvictConnections();
             done = true;
+            System.out.println("Connection soft evicted!");
+
+            DefaultListableBeanFactory bf = (DefaultListableBeanFactory)ctx.getAutowireCapableBeanFactory();
+            DefaultListableBeanFactory defaultListableBeanFactory = bf;
+
+            defaultListableBeanFactory.removeBeanDefinition("transactionManager");
+            defaultListableBeanFactory.registerSingleton("transactionManager", configuration.txManager());
+            txManager = (PlatformTransactionManager) defaultListableBeanFactory.getBean("transactionManager");
             System.out.println("Connection refreshed!");
         }
         System.out.println("*** TX manager: " + txManager + " - DS: " + ds);
@@ -31,4 +51,8 @@ public class ConnectionRefresher {
         System.out.println(" Total connections  : " + ds.getHikariPoolMXBean().getTotalConnections());
     }
 
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        ctx = applicationContext;
+    }
 }
