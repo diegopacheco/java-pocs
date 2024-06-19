97650a7fc3d0ffcdaf568df8403c6c71cdbfd55e
diegopacheco <diego.pacheco.it@gmail.com>
2024-06-19 00:55:21 -0700

    JPype calling java from python

---
+++ pocs/JPype-java8/src/main/java/Calculator.java
@@ -0,0 +1,8 @@
+public class Calculator {
+
+    public Integer sum(int v1,int v2){
+        System.out.println("[java_8] Summing " + v1 + " + " + v2 + " = " + (v1 + v2));
+        return v1 + v2;
+    }
+
+}
---
+++ pocs/JPype-java8/src/main/java/Logger.java
@@ -0,0 +1,7 @@
+public class Logger {
+
+    public static void log(String message) {
+        System.out.println("[java_8] " + message);
+    }
+
+}
