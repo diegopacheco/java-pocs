f349bd0df37f2a6630f6139e9f7c68fedc4f1738
diegopacheco <diego.pacheco.it@gmail.com>
2024-06-25 21:07:31 -0700

    pool party poc

--- pocs/connection-pools-party/src/main/java/com/github/diegopacheco/cpparty/adapter/HikariConfigAdapter.java
+++ pocs/connection-pools-party/src/main/java/com/github/diegopacheco/cpparty/adapter/HikariConfigAdapter.java
@@ -51,6 +51,60 @@ public class HikariConfigAdapter extends HikariConfig {
     }
 
     @Override
+    public String getCatalog() {
+        System.out.println("[HikariConfigAdapter]HikariConfigAdapter.getCatalog() called.");
+        return realConfig.getCatalog();
+    }
+
+    @Override
+    public void setCatalog(String catalog) {
+        System.out.println("[HikariConfigAdapter]HikariConfigAdapter.setCatalog() called.");
+        realConfig.setCatalog(catalog);
+    }
+
+    @Override
+    public long getConnectionTimeout() {
+        System.out.println("[HikariConfigAdapter]HikariConfigAdapter.getConnectionTimeout() called.");
+        return realConfig.getConnectionTimeout();
+    }
+
+    @Override
+    public void setConnectionTimeout(long connectionTimeoutMs) {
+        System.out.println("[HikariConfigAdapter]HikariConfigAdapter.setConnectionTimeout() called.");
+        realConfig.setConnectionTimeout(connectionTimeoutMs);
+    }
+
+    @Override
+    public long getIdleTimeout() {
+        System.out.println("[HikariConfigAdapter]HikariConfigAdapter.getIdleTimeout() called.");
+        return realConfig.getIdleTimeout();
+    }
+
+    @Override
+    public void setIdleTimeout(long idleTimeoutMs) {
+        System.out.println("[HikariConfigAdapter]HikariConfigAdapter.setIdleTimeout() called.");
+        realConfig.setIdleTimeout(idleTimeoutMs);
+    }
+
+    @Override
+    public long getLeakDetectionThreshold() {
+        System.out.println("[HikariConfigAdapter]HikariConfigAdapter.getLeakDetectionThreshold() called.");
+        return realConfig.getLeakDetectionThreshold();
+    }
+
+    @Override
+    public void setLeakDetectionThreshold(long leakDetectionThresholdMs) {
+        System.out.println("[HikariConfigAdapter]HikariConfigAdapter.setLeakDetectionThreshold() called.");
+        realConfig.setLeakDetectionThreshold(leakDetectionThresholdMs);
+    }
+
+    @Override
+    public long getMaxLifetime() {
+        System.out.println("[HikariConfigAdapter]HikariConfigAdapter.getMaxLifetime() called.");
+        return realConfig.getMaxLifetime();
+    }
+
+    @Override
     public void validate() {
         System.out.println("[HikariConfigAdapter]HikariConfigAdapter.validate() called.");
         realConfig.validate();
