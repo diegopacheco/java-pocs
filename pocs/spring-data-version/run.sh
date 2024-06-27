1303408b6cb64533d02456abb1777107bb4db7e6
diegopacheco <diego.pacheco.it@gmail.com>
2024-06-27 08:06:53 -0700

    progress on poc

--- pocs/spring-data-version/run.sh
+++ pocs/spring-data-version/run.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+echo "use person; DROP TABLE person;" | mysql -uroot -ppass -h127.0.0.1 -P3325
+
+echo "use person; CREATE TABLE IF NOT EXISTS person (
+          id INT AUTO_INCREMENT PRIMARY KEY,
+          first_name VARCHAR(255) NOT NULL,
+          last_name VARCHAR(255) NOT NULL,
+          version int NOT NULL DEFAULT 0,
+          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      ) ENGINE=INNODB;;" | mysql -uroot -ppass -h127.0.0.1 -P3325
+
 ./mvnw spring-boot:run
 
