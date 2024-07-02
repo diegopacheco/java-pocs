319dbb754f59e4b31b7847c55c970f51bae9d93d
diegopacheco <diego.pacheco.it@gmail.com>
2024-07-02 00:13:38 -0700

    RT poc

---
+++ pocs/spring-boot-rest-client-pool/downstream-slow-service/install-deps.sh
@@ -0,0 +1,3 @@
+#!/bin/bash 
+
+/bin/pip install -r requirements.txt
---
+++ pocs/spring-boot-rest-client-pool/src/main/java/com/github/diegopacheco/sandboxspring/config/RestConfig.java
@@ -0,0 +1,42 @@
+package com.github.diegopacheco.sandboxspring.config;
+
+import org.apache.hc.client5.http.config.RequestConfig;
+import org.apache.hc.client5.http.impl.DefaultHttpRequestRetryStrategy;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
+import org.apache.hc.core5.util.TimeValue;
+import org.apache.hc.core5.util.Timeout;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestConfig {
+
+    @Bean
+    public CloseableHttpClient httpClient() {
+        var connectionManager = PoolingHttpClientConnectionManagerBuilder.create()
+                .setMaxConnPerRoute(1)
+                .setMaxConnTotal(1)
+                .build();
+
+        var reqConfig = RequestConfig.custom()
+                //.setConnectionRequestTimeout(Timeout.ofSeconds(10))
+                .build();
+
+        return HttpClients.custom()
+                .setRetryStrategy(new DefaultHttpRequestRetryStrategy(3, TimeValue.ofSeconds(1)))
+                .setConnectionManager(connectionManager)
+                .setDefaultRequestConfig(reqConfig)
+                .setConnectionManagerShared(true)
+                .build();
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate(new HttpComponentsClientHttpRequestFactory(httpClient()));
+    }
+
+}
