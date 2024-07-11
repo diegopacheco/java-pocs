75c6b748a3f9914026efe8719cbff30ac0716b1d
diegopacheco <diego.pacheco.it@gmail.com>
2024-07-11 00:52:31 -0700

    working

--- pocs/spring-boot-custom-logger/src/main/java/com/github/diegopacheco/sandboxspring/config/MyNettyWebServerCustomizer.java
+++ pocs/spring-boot-custom-logger/src/main/java/com/github/diegopacheco/sandboxspring/config/MyNettyWebServerCustomizer.java
@@ -1,4 +1,26 @@
 package com.github.diegopacheco.sandboxspring.config;
 
-public class MyNettyWebServerCustomizer {
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.netty.NettyReactiveWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.stereotype.Component;
+import reactor.netty.http.server.logging.AccessLog;
+
+@Component
+public class MyNettyWebServerCustomizer
+        implements WebServerFactoryCustomizer<NettyReactiveWebServerFactory> {
+
+    @Value("${access.log.format}")
+    private String accessLogFormat;
+
+    @Override
+    public void customize(NettyReactiveWebServerFactory factory) {
+        factory.addServerCustomizers(httpServer ->
+                httpServer.accessLog(true, x ->
+                        AccessLog.create(
+                                accessLogFormat,
+                                x.method(), x.uri(), x.requestHeader("MYHEADER"),
+                                x.requestHeader("X-FORWARDED-FOR"))
+                ));
+    }
 }
--- pocs/spring-boot-custom-logger/test.sh
+++ pocs/spring-boot-custom-logger/test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
 
-curl -H "X-FORWARDED-FOR: 10.99.0.100" -H "Content-Type: application/json" -H "CUSTOM-HEADER-Z: 42" http://127.0.0.1:8080/actuator/health
+curl -H "MYHEADER: 100 " -H "X-FORWARDED-FOR: 10.99.0.100" -H "Content-Type: application/json" -H "CUSTOM-HEADER-Z: 42" http://127.0.0.1:8080/actuator/health
