5ec0ab9678822488aa7fde88228b43f8210bbb6b
diegopacheco <diego.pacheco.it@gmail.com>
2024-07-11 00:55:06 -0700

    working

--- pocs/spring-boot-custom-logger-log4j/src/main/java/com/github/diegopacheco/sandboxspring/config/MyNettyWebServerCustomizer.java
+++ pocs/spring-boot-custom-logger-log4j/src/main/java/com/github/diegopacheco/sandboxspring/config/MyNettyWebServerCustomizer.java
@@ -1,5 +1,6 @@
 package com.github.diegopacheco.sandboxspring.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.embedded.netty.NettyReactiveWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.stereotype.Component;
@@ -8,12 +9,16 @@ import reactor.netty.http.server.logging.AccessLog;
 @Component
 public class MyNettyWebServerCustomizer
         implements WebServerFactoryCustomizer<NettyReactiveWebServerFactory> {
+
+    @Value("${access.log.format}")
+    private String accessLogFormat;
+
     @Override
     public void customize(NettyReactiveWebServerFactory factory) {
         factory.addServerCustomizers(httpServer ->
                 httpServer.accessLog(true, x ->
                         AccessLog.create(
-                                "method={}, uri={} myheader={} x-forwarded-for={}",
+                                accessLogFormat,
                                 x.method(), x.uri(), x.requestHeader("MYHEADER"),
                                 x.requestHeader("X-FORWARDED-FOR"))
                 ));
