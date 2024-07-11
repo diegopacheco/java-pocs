09645c0ef5d92b585e3262cd4d2b473e590f39f3
diegopacheco <diego.pacheco.it@gmail.com>
2024-07-11 00:30:29 -0700

    poc

---
+++ pocs/spring-boot-custom-logger-log4j/src/main/java/com/github/diegopacheco/sandboxspring/config/CustomHeaderLoggingFilter.java
@@ -0,0 +1,30 @@
+package com.github.diegopacheco.sandboxspring.config;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.ThreadContext;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+@Component
+public class CustomHeaderLoggingFilter implements WebFilter {
+    private static final Logger logger = LogManager.getLogger(CustomHeaderLoggingFilter.class);
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        logger.info("CustomHeaderLoggingFilter running... ");
+        ServerHttpRequest request = exchange.getRequest();
+        request.getHeaders().forEach((name, values) -> {
+            String value = String.join(", ", values);
+            logger.info("Header '{}': {}", name, value); // Debug log to verify header
+            ThreadContext.put(name, value);
+        });
+
+        return chain.filter(exchange)
+                .doFinally(signalType -> ThreadContext.clearAll());
+    }
+}
---
+++ pocs/spring-boot-custom-logger-log4j/test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash 
+
+curl -H "X-FORWARDED-FOR: 10.99.0.100" -H "Content-Type: application/json" -H "MYHEADER: 100" -H "CUSTOM-HEADER-Z: 42" http://127.0.0.1:8080/actuator/health
