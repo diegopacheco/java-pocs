9975f3acc4e9788a49a21f011b957bfec2bbd811
diegopacheco <diego.pacheco.it@gmail.com>
2024-08-19 01:05:45 -0700

    .

--- pocs/sqs-localstack-java21/src/main/java/Main.java
+++ pocs/sqs-localstack-java21/src/main/java/Main.java
@@ -3,5 +3,7 @@ public class Main{
   public static void main(String args[]){
     SQSClient sqsClient = new SQSClient();
     sqsClient.sendMessage("test-queue", "Hello, World!");
+    System.out.println(sqsClient.getQueueStats("test-queue"));
+    System.out.println(sqsClient.readMessage("test-queue"));
   }
 }
