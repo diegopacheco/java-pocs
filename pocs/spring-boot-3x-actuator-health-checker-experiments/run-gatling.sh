4cd3c5762cb4720209dd1f28fbc822105367c4fb
diegopacheco <diego.pacheco.it@gmail.com>
2024-08-23 00:16:35 -0700

    test - and proof that is not necessary - micrometer already does this

--- pocs/spring-boot-3x-actuator-health-checker-experiments/run-gatling.sh
+++ pocs/spring-boot-3x-actuator-health-checker-experiments/run-gatling.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/bash
 
+ulimit -n 65000
+sudo sysctl -w net.ipv4.ip_local_port_range="1025 65535"
+echo 300000 | sudo tee /proc/sys/fs/nr_open
+echo 300000 | sudo tee /proc/sys/fs/file-max
+
 ./mvnw gatling:test -Dgatling.simulation=ActuatorHealthCheckerSimulation
