5d310c39aaef46858404af1443cd1b9abcb50937
diegopacheco <diego.pacheco.it@gmail.com>
2024-08-23 00:16:25 -0700

    test - and proof that is not necessary - micrometer already does this

--- pocs/spring-boot-3x-actuator-health-checker-experiments/k8s-kind-deploy-app.sh
+++ pocs/spring-boot-3x-actuator-health-checker-experiments/k8s-kind-deploy-app.sh
@@ -3,3 +3,10 @@
 kind load docker-image app:latest
 kubectl apply -f specs/deployment.yaml --force
 kubectl apply -f specs/service.yaml --force
+
+kubectl apply -f specs/grafana-deployment.yaml --force
+kubectl apply -f specs/grafana-service.yaml --force
+
+kubectl apply -f specs/prometheus-deployment.yaml --force
+kubectl apply -f specs/prometheus-service.yaml --force
+kubectl apply -f specs/prometheus-sb-configmap.yaml --force
