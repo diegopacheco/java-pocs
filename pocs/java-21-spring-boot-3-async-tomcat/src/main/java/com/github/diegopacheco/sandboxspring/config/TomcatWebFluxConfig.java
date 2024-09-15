8a9b99fec2a30af650d41c3af3f175f2f017ddf9
diegopacheco <diego.pacheco.it@gmail.com>
2024-09-15 00:51:18 -0700

    java-21-spring-boot-3-async-tomcat

--- pocs/java-21-spring-boot-3-async-tomcat/src/main/java/com/github/diegopacheco/sandboxspring/config/TomcatWebFluxConfig.java
+++ pocs/java-21-spring-boot-3-async-tomcat/src/main/java/com/github/diegopacheco/sandboxspring/config/TomcatWebFluxConfig.java
@@ -5,6 +5,9 @@ import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Not used on the benchmarks
+ */
 @Configuration
 public class TomcatWebFluxConfig {
 
@@ -12,16 +15,16 @@ public class TomcatWebFluxConfig {
     public WebServerFactoryCustomizer<TomcatReactiveWebServerFactory> tomcatCustomizer() {
         return factory -> factory.addConnectorCustomizers(connector -> {
             /*
-                connector.setProperty("maxThreads", "200");
-                connector.setProperty("minSpareThreads", "25");
-                connector.setProperty("maxConnections", "128");
+                connector.setProperty("maxThreads", "600");
+                connector.setProperty("minSpareThreads", "50");
+                connector.setProperty("maxConnections", "8192");
                 connector.setProperty("connectionTimeout", "20000");
                 connector.setProperty("keepAliveTimeout", "60000");
-                connector.setProperty("maxKeepAliveRequests", "100");
+                connector.setProperty("maxKeepAliveRequests", "200");
                 connector.setProperty("compression", "on");
-                connector.setProperty("compressionMinSize", "2048");
+                connector.setProperty("compressionMinSize", "1048");
                 connector.setProperty("compressableMimeType", "text/html,text/xml,text/plain,text/css,text/javascript,application/javascript,application/json");
-            */
+             */
         });
     }
 }
