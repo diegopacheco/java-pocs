3239fc81ddb4a2a90b294ac3d5b2fdb1e9bd5aa1
diegopacheco <diego.pacheco.it@gmail.com>
2024-09-24 00:21:03 -0700

    java-23

--- pocs/jdk-23-features/src/main/java/module-info.java
+++ pocs/jdk-23-features/src/main/java/module-info.java
@@ -1,4 +1,8 @@
 module com.github.diegopacheco.java23.features.JEP_469 {
     requires jdk.incubator.vector;
     requires org.objectweb.asm;
+    requires jdk.unsupported;
+    requires jdk.httpserver;
+
+    exports com.github.diegopacheco.java23.features.JEP_476;
 }
