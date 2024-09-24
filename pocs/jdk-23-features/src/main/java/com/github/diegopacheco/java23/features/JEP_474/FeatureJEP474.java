5c92104bd8975fb5e23dc56116338bc3ed836450
diegopacheco <diego.pacheco.it@gmail.com>
2024-09-24 00:21:17 -0700

    java-23

---
+++ pocs/jdk-23-features/src/main/java/com/github/diegopacheco/java23/features/JEP_474/FeatureJEP474.java
@@ -0,0 +1,11 @@
+package com.github.diegopacheco.java23.features.JEP_474;
+
+/**
+ * JEP 474: ZGC: Generational Mode by Default
+ * https://openjdk.org/jeps/474
+ */
+public class FeatureJEP474 {
+    public static void main(String[] args) {
+        // call java using -XX:+UseZGC
+    }
+}
