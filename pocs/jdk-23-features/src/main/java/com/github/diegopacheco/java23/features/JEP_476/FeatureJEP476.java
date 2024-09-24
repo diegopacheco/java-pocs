0b8d6e65a9c3ad5c2673194c175ac1043c3b36b2
diegopacheco <diego.pacheco.it@gmail.com>
2024-09-24 00:21:22 -0700

    java-23

---
+++ pocs/jdk-23-features/src/main/java/com/github/diegopacheco/java23/features/JEP_476/FeatureJEP476.java
@@ -0,0 +1,13 @@
+package com.github.diegopacheco.java23.features.JEP_476;
+
+/**
+ * JEP 476: Module Import Declarations (Preview)
+ * https://openjdk.org/jeps/476
+ *
+ */
+public class FeatureJEP476 {
+
+    public static void main(String[] args) {
+        // look module-info.java
+    }
+}
