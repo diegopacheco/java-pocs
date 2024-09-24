8d696bc0235bebdef15fddc45297d7932ce22fab
diegopacheco <diego.pacheco.it@gmail.com>
2024-09-24 00:21:29 -0700

    java-23

---
+++ pocs/jdk-23-features/src/main/java/com/github/diegopacheco/java23/features/JEP_477/FeatureJEP477.java
@@ -0,0 +1,8 @@
+/**
+ * JEP 477: Implicitly Declared Classes and Instance Main Methods (Third Preview)
+ * https://openjdk.org/jeps/477
+ *
+ */
+//void main() {
+//    println("Hello, World!");
+//}
