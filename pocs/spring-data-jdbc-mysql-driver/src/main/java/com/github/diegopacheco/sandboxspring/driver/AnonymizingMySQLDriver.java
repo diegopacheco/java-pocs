cddfafc916c084ad6da645e37508c4464411fd3f
diegopacheco <diego.pacheco.it@gmail.com>
2025-01-19 00:21:59 -0800

    spring-data-boot 3.4.1 mysql driver

---
+++ pocs/spring-data-jdbc-mysql-driver/src/main/java/com/github/diegopacheco/sandboxspring/driver/AnonymizingMySQLDriver.java
@@ -0,0 +1,55 @@
+package com.github.diegopacheco.sandboxspring.driver;
+
+import java.sql.*;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+public class AnonymizingMySQLDriver implements Driver {
+    static {
+        try {
+            DriverManager.registerDriver(new AnonymizingMySQLDriver());
+        } catch (SQLException e) {
+            throw new RuntimeException("Failed to register AnonymizingMySQLDriver", e);
+        }
+    }
+
+    private final Driver mysqlDriver = new com.mysql.cj.jdbc.Driver();
+
+    public AnonymizingMySQLDriver() throws SQLException {
+    }
+
+    @Override
+    public Connection connect(String url, Properties info) throws SQLException {
+        return new AnonymizingConnection(mysqlDriver.connect(url, info));
+    }
+
+    @Override
+    public boolean acceptsURL(String url) throws SQLException {
+        return mysqlDriver.acceptsURL(url);
+    }
+
+    @Override
+    public DriverPropertyInfo[] getPropertyInfo(String url, Properties info) throws SQLException {
+        return mysqlDriver.getPropertyInfo(url, info);
+    }
+
+    @Override
+    public int getMajorVersion() {
+        return mysqlDriver.getMajorVersion();
+    }
+
+    @Override
+    public int getMinorVersion() {
+        return mysqlDriver.getMinorVersion();
+    }
+
+    @Override
+    public boolean jdbcCompliant() {
+        return mysqlDriver.jdbcCompliant();
+    }
+
+    @Override
+    public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+        return mysqlDriver.getParentLogger();
+    }
+}
