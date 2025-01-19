fc4292bb83f68f79713e866faf2865d34b83e590
diegopacheco <diego.pacheco.it@gmail.com>
2025-01-18 23:55:01 -0800

    spring-data-boot 3.4.1 mysql driver

---
+++ pocs/spring-data-jdbc-mysql-driver/src/main/java/com/github/diegopacheco/sandboxspring/dao/DBLoader.java
@@ -0,0 +1,35 @@
+package com.github.diegopacheco.sandboxspring.dao;
+
+import com.github.diegopacheco.sandboxspring.model.Person;
+import com.github.diegopacheco.sandboxspring.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DBLoader {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    private PersonService service;
+
+    public void insertData() {
+        try{
+            jdbcTemplate.execute("INSERT INTO person(first_name,last_name) VALUES('Victor', 'Hugo')");
+            jdbcTemplate.execute("INSERT INTO person(first_name,last_name) VALUES('Dante', 'Alighieri')");
+            jdbcTemplate.execute("INSERT INTO person(first_name,last_name) VALUES('Stefan', 'Zweig')");
+            jdbcTemplate.execute("INSERT INTO person(first_name,last_name) VALUES('Oscar', 'Wilde')");
+
+            Person p = new Person();
+            p.setFirstName("Diego");
+            p.setLastName("Pacheco");
+            service.save(p);
+
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+
+}
---
+++ pocs/spring-data-jdbc-mysql-driver/src/main/java/com/github/diegopacheco/sandboxspring/service/PersonService.java
@@ -0,0 +1,26 @@
+package com.github.diegopacheco.sandboxspring.service;
+
+import com.github.diegopacheco.sandboxspring.dao.PersonRepository;
+import com.github.diegopacheco.sandboxspring.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class PersonService {
+
+    @Autowired
+    PersonRepository repository;
+
+    public List<Person> getAllPeople(){
+        List<Person> result = new ArrayList<Person>();
+        repository.findAll().forEach(result::add);
+        return result;
+    }
+
+    public void save(Person p){
+        repository.save(p);
+    }
+
+}
