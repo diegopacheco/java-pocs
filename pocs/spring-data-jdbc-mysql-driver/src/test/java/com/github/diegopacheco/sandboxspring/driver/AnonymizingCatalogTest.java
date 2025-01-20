c9d0e01107db93847c84735bdef98861bc13ee2d
diegopacheco <diego.pacheco.it@gmail.com>
2025-01-19 20:09:53 -0800

    spring-data-boot 3.4.1 mysql driver

--- pocs/spring-data-jdbc-mysql-driver/src/test/java/com/github/diegopacheco/sandboxspring/driver/AnonymizingCatalogTest.java
+++ pocs/spring-data-jdbc-mysql-driver/src/test/java/com/github/diegopacheco/sandboxspring/driver/AnonymizingCatalogTest.java
@@ -2,12 +2,68 @@ package com.github.diegopacheco.sandboxspring.driver;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class AnonymizingCatalogTest {
 
     @Test
     public void testGetInstance(){
         AnonymizingCatalog instance = AnonymizingCatalog.getInstance();
         assert(instance!=null);
+    }
+
+    @Test
+    public void testIsAnonymizingField() {
+        AnonymizingCatalog catalog = AnonymizingCatalog.getInstance();
+        assertFalse(catalog.isAnonymizingField("person", "last_name"));
+    }
+
+
+    @Test
+    public void testApplyStrategy() {
+        AnonymizingCatalog catalog = AnonymizingCatalog.getInstance();
+        Anonymizing anonymizing = new Anonymizing() {
+            @Override
+            public Class<? extends java.lang.annotation.Annotation> annotationType() {
+                return Anonymizing.class;
+            }
+
+            @Override
+            public String dbFieldName() {
+                return "first_name";
+            }
+
+            @Override
+            public AnonymizingStrategy strategy() {
+                return AnonymizingStrategy.STAR;
+            }
+        };
+        catalog.addAnonymizingField("person", "first_name", anonymizing);
+        String result = catalog.applyStrategy("John", "person", "first_name");
+        assertEquals("****", result);
+    }
+
+    @Test
+    public void testAddAnonymizingField() {
+        AnonymizingCatalog catalog = AnonymizingCatalog.getInstance();
+        Anonymizing anonymizing = new Anonymizing() {
+            @Override
+            public Class<? extends java.lang.annotation.Annotation> annotationType() {
+                return Anonymizing.class;
+            }
+
+            @Override
+            public String dbFieldName() {
+                return "first_name";
+            }
+
+            @Override
+            public AnonymizingStrategy strategy() {
+                return AnonymizingStrategy.STAR;
+            }
+        };
+        catalog.addAnonymizingField("person", "first_name", anonymizing);
+        assertTrue(catalog.isAnonymizingField("person", "first_name"));
     }
 
 }
