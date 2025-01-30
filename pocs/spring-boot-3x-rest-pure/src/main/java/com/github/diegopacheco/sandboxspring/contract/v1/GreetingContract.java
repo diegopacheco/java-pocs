47ed5fbfc657cd187ca8438d056c9ba803f9afa5
diegopacheco <diego.pacheco.it@gmail.com>
2025-01-29 23:02:14 -0800

    sb3 rest pure poc

--- pocs/spring-boot-3x-rest-pure/src/main/java/com/github/diegopacheco/sandboxspring/contract/v1/GreetingContract.java
+++ pocs/spring-boot-3x-rest-pure/src/main/java/com/github/diegopacheco/sandboxspring/contract/v1/GreetingContract.java
@@ -2,5 +2,5 @@ package com.github.diegopacheco.sandboxspring.contract.v1;
 
 public interface GreetingContract {
     String greet(String name);
-    //String findGreetingLanguage(String lang);
+    String findGreetingLanguage(String lang);
 }
--- pocs/spring-boot-3x-rest-pure/src/main/java/com/github/diegopacheco/sandboxspring/controller/HelloController.java
+++ pocs/spring-boot-3x-rest-pure/src/main/java/com/github/diegopacheco/sandboxspring/controller/HelloController.java
@@ -1,7 +1,9 @@
 package com.github.diegopacheco.sandboxspring.controller;
 
 import com.github.diegopacheco.sandboxspring.contract.v1.GreetingContract;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -17,19 +19,22 @@ public class HelloController implements GreetingContract {
 		langs.put("pt","Eae");
 	}
 
+	//  http://localhost:8080/lang?langName=pt
 	@Override
-	@GetMapping
-	@RequestMapping("/lang/{langName}")
-	public String greet(@RequestParam("langName") String langName) {
+	@GetMapping("/lang")
+	public String greet(@RequestParam(value = "langName", defaultValue = "en") String langName) {
 		return langs.getOrDefault(langName, langs.get("en"));
 	}
 
-	//@Override
-	//@GetMapping
-	/*
-	public String findGreetingLanguage(String lang) {
-		return "";
+	//  http://localhost:8080/find?langName=it
+	@Override
+	@GetMapping("/find")
+	public String findGreetingLanguage(@RequestParam(value = "langName") String lang) {
+		if (!langs.containsKey(lang)) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Language not found");
+		}
+		return langs.get(lang);
 	}
-	*/
+
 }
 
