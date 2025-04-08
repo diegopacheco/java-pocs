39a2890739f30c644bd1c23d914042180b88b2b1
diegopacheco <diego.pacheco.it@gmail.com>
2025-04-08 00:19:19 -0700

    liquid-unit Refactoring

--- pocs/liquid-unit-poc/src/test/java/com/github/diegopacheco/liquidunit/LiquidUnitJunitTests.java
+++ pocs/liquid-unit-poc/src/test/java/com/github/diegopacheco/liquidunit/LiquidUnitJunitTests.java
@@ -1,3 +1,12 @@
 package com.github.diegopacheco.liquidunit;
 
-public class LiquidUnitTestsJunit extends LiquidUnitTests  {}
+import com.github.diegopacheco.liquidunit.execution.engine.LiquidUnitTests;
+import org.junit.jupiter.api.Order;
+
+//
+// This class just extends form LiquidUnitTests
+// I did this to be able to run the tests ans Junit so the LiquidUnitTests is the source of truth
+// That way I can run Junit for tests and Runtime.
+//
+@Order(1)
+public class LiquidUnitJunitTests extends LiquidUnitTests {}
