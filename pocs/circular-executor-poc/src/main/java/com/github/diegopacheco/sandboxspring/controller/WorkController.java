6cadec6eefddc2fc3298bb67f52adb816131c8ab
diegopacheco <diego.pacheco.it@gmail.com>
2025-07-30 21:43:02 -0700

    v1 or the poc - improve docs

--- pocs/circular-executor-poc/src/main/java/com/github/diegopacheco/sandboxspring/controller/WorkController.java
+++ pocs/circular-executor-poc/src/main/java/com/github/diegopacheco/sandboxspring/controller/WorkController.java
@@ -5,8 +5,6 @@ import com.github.diegopacheco.sandboxspring.service.WorkTask;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.lang.reflect.Executable;
 import java.util.concurrent.ExecutorService;
 
 @RestController
