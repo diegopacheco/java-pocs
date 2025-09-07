4440cbfe2c9ccee5746d89f84be7391630f056f3
diegopacheco <diego.pacheco.it@gmail.com>
2025-09-07 16:50:54 -0700

    code organization poc

--- pocs/java-23-code-organization/src/main/java/com/github/diegopacheco/sandboxspring/controller/StatusController.java
+++ pocs/java-23-code-organization/src/main/java/com/github/diegopacheco/sandboxspring/controller/StatusController.java
@@ -6,12 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
-public class HelloController {
-    
-    @GetMapping("/hello")
-    public Map<String, String> hello() {
-        return Map.of("message", "Hello from Spring Boot!");
-    }
+public class StatusController {
     
     @GetMapping("/api/status")
     public Map<String, String> status() {
