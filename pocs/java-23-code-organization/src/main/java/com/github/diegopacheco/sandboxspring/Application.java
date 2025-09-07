8e39200bd0d7ae12710fb36e98761310f1cefba6
diegopacheco <diego.pacheco.it@gmail.com>
2025-09-07 16:23:17 -0700

    code organization poc

--- pocs/java-23-code-organization/src/main/java/com/github/diegopacheco/sandboxspring/Application.java
+++ pocs/java-23-code-organization/src/main/java/com/github/diegopacheco/sandboxspring/Application.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 
 @SpringBootApplication(scanBasePackages = {
     "com.github.diegopacheco.sandboxspring", 
