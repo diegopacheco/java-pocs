0b64a21e53e5f87da317f0263921f8168703cfa8
diegopacheco <diego.pacheco.it@gmail.com>
2025-09-07 17:06:45 -0700

    code organization poc

--- pocs/java-23-code-organization/src/main/java/com/github/diegopacheco/javapocs/codeorg/ddd/infrastructure/config/WebConfig.java
+++ pocs/java-23-code-organization/src/main/java/com/github/diegopacheco/javapocs/codeorg/ddd/infrastructure/config/WebConfig.java
@@ -1,5 +1,9 @@
 package com.github.diegopacheco.javapocs.codeorg.ddd.infrastructure.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
