88d6fbaf2a22300d760904d6409c671956ab479f
diegopacheco <diego.pacheco.it@gmail.com>
2025-09-16 22:16:41 -0700

    java 25 hello world

--- pocs/java-25-helloworld/src/main/java/Main.java
+++ pocs/java-25-helloworld/src/main/java/Main.java
@@ -2,5 +2,6 @@
 public class Main{
   public static void main(String args[]){
     System.out.println("It works!");
+    System.out.println("Java version: " + System.getProperty("java.version"));
   }
 }
