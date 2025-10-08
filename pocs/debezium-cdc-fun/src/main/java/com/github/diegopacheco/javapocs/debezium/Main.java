64172956175c8cb388483c3f683d0ce84a0f166f
diegopacheco <diego.pacheco.it@gmail.com>
2025-10-08 01:11:27 -0700

    fix

--- pocs/debezium-cdc-fun/src/main/java/com/github/diegopacheco/javapocs/debezium/Main.java
+++ pocs/debezium-cdc-fun/src/main/java/com/github/diegopacheco/javapocs/debezium/Main.java
@@ -3,7 +3,11 @@ package com.github.diegopacheco.javapocs.debezium;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+    org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration.class,
+    org.springframework.boot.autoconfigure.data.r2dbc.R2dbcDataAutoConfiguration.class,
+    org.springframework.boot.autoconfigure.data.r2dbc.R2dbcRepositoriesAutoConfiguration.class
+})
 public class Main{
   public static void main(String args[]){
     SpringApplication.run(Main.class, args);
