2b3362910c249c316ff88b5a014d714f58043532
diegopacheco <diego.pacheco.it@gmail.com>
2025-10-10 22:40:56 -0700

    upgrade to hava 25

--- pocs/java-25-junit-tag-control/src/test/java/NonBadTestSuite.java
+++ pocs/java-25-junit-tag-control/src/test/java/NonBadTestSuite.java
@@ -1,9 +1,8 @@
 import org.junit.platform.suite.api.Suite;
-import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.ExcludeTags;
 
 @Suite
-@SelectClasses(CalculatorTest.class)
+@SelectPackages("")
 @ExcludeTags("BAD")
-public class NonBadTestSuite {
-}
+public class NonBadTestSuite {}
