3c20c5aa2763511085106e868c2320c8dc51f0bd
diegopacheco <diego.pacheco.it@gmail.com>
2025-11-04 21:21:26 -0800

    test container junit reuse

--- pocs/java-25-test-containers-reuse/README.md
+++ pocs/java-25-test-containers-reuse/README.md
@@ -7,6 +7,27 @@
 ### Result
 
 ```
+First test class (RedisStringOperationsTest):
+- Container ID before start: null (new container being created)
+- Container ID after start: e246bb793527dd9e38b57333dfa0b79cc571c928adc1006d8a5ed63c248d7f05
+- Time elapsed: 2.059s (includes container startup)
+
+Second test class (RedisHashOperationsTest):
+- Container ID before start: e246bb793527dd9e38b57333dfa0b79cc571c928adc1006d8a5ed63c248d7f05 (same
+container!)
+- Container ID after start: e246bb793527dd9e38b57333dfa0b79cc571c928adc1006d8a5ed63c248d7f05
+- Time elapsed: 0.029s (just reusing existing container)
+
+Third test class (RedisListOperationsTest):
+- Container ID before start: e246bb793527dd9e38b57333dfa0b79cc571c928adc1006d8a5ed63c248d7f05 (same
+container!)
+- Container ID after start: e246bb793527dd9e38b57333dfa0b79cc571c928adc1006d8a5ed63c248d7f05
+- Time elapsed: 0.017s (just reusing existing container)
+
+The logs clearly demonstrate that:
+1. The container starts only once during the first test class
+2. All subsequent test classes reuse the same container (same Container ID)
+3. Performance improvement is significant: first test ~2s vs subsequent tests ~0.02s
 ```
 
 
--- pocs/java-25-test-containers-reuse/src/test/java/AbstractRedisTest.java
+++ pocs/java-25-test-containers-reuse/src/test/java/AbstractRedisTest.java
@@ -1,3 +1,5 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.BeforeAll;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.utility.DockerImageName;
@@ -5,6 +7,8 @@ import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
 public abstract class AbstractRedisTest {
+
+    private static final Logger log = LogManager.getLogger(AbstractRedisTest.class);
 
     protected static final GenericContainer<?> REDIS_CONTAINER =
             new GenericContainer<>(
@@ -16,7 +20,20 @@ public abstract class AbstractRedisTest {
 
     @BeforeAll
     static void startRedis() {
+        log.info("========================================");
+        log.info("STARTING REDIS CONTAINER");
+        log.info("Container ID: {}", REDIS_CONTAINER.getContainerId());
+        log.info("Reuse enabled: {}", REDIS_CONTAINER.isShouldBeReused());
+        log.info("========================================");
+
         REDIS_CONTAINER.start();
+
+        log.info("========================================");
+        log.info("REDIS CONTAINER STARTED");
+        log.info("Container ID: {}", REDIS_CONTAINER.getContainerId());
+        log.info("Running: {}", REDIS_CONTAINER.isRunning());
+        log.info("========================================");
+
         String address = REDIS_CONTAINER.getHost();
         Integer port = REDIS_CONTAINER.getFirstMappedPort();
         jedisPool = new JedisPool(address, port);
