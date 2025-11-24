e299cc7e3fb8ab339e4e0835d243f22dae76f88a
diegopacheco <diego.pacheco.it@gmail.com>
2025-11-23 23:09:07 -0800

    kafka/streams

--- pocs/java-25-kafka-streams-windoning-eo-purchases/start.sh
+++ pocs/java-25-kafka-streams-windoning-eo-purchases/start.sh
@@ -7,3 +7,6 @@ while ! podman exec $KAFKA_CONTAINER kafka-broker-api-versions --bootstrap-serve
   sleep 1
 done
 echo "Kafka is ready"
+echo "Creating purchases topic..."
+podman exec $KAFKA_CONTAINER kafka-topics --create --topic purchases --bootstrap-server localhost:9092 --partitions 3 --replication-factor 1 --if-not-exists
+echo "Topic created"
