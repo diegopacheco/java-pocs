158024505c83bf7eddab0e009654054f489ad5cf
diegopacheco <diego.pacheco.it@gmail.com>
2026-01-31 20:03:17 -0800

    improve code and layout

--- pocs/java-25-spring-ws-fun/src/main/java/com/github/diegopacheco/javapocs/handler/PrimeWebSocketHandler.java
+++ pocs/java-25-spring-ws-fun/src/main/java/com/github/diegopacheco/javapocs/handler/PrimeWebSocketHandler.java
@@ -8,10 +8,6 @@ import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -24,35 +20,14 @@ import java.util.concurrent.atomic.AtomicLong;
 @Component
 public class PrimeWebSocketHandler extends TextWebSocketHandler {
 
-    private static final Path STATE_FILE = Paths.get(System.getProperty("user.home"), ".prime-discovery-state");
     private final CopyOnWriteArraySet<WebSocketSession> sessions = new CopyOnWriteArraySet<>();
     private final ObjectMapper objectMapper = new ObjectMapper();
     private final List<PrimeNumber> discoveredPrimes = Collections.synchronizedList(new ArrayList<>());
-    private final AtomicLong currentNumber;
+    private final AtomicLong currentNumber = new AtomicLong(2);
     private final ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
 
     public PrimeWebSocketHandler() {
-        long startFrom = loadState();
-        currentNumber = new AtomicLong(startFrom);
         scheduler.scheduleWithFixedDelay(this::discoverNextPrime, 0, 500, TimeUnit.MILLISECONDS);
-    }
-
-    private long loadState() {
-        try {
-            if (Files.exists(STATE_FILE)) {
-                String content = Files.readString(STATE_FILE).trim();
-                return Long.parseLong(content);
-            }
-        } catch (IOException | NumberFormatException e) {
-        }
-        return 2;
-    }
-
-    private void saveState(long number) {
-        try {
-            Files.writeString(STATE_FILE, String.valueOf(number));
-        } catch (IOException e) {
-        }
     }
 
     private void discoverNextPrime() {
@@ -60,7 +35,6 @@ public class PrimeWebSocketHandler extends TextWebSocketHandler {
         if (isPrime(num)) {
             PrimeNumber prime = new PrimeNumber(num, System.currentTimeMillis());
             discoveredPrimes.add(prime);
-            saveState(num + 1);
             broadcast(prime);
         }
     }
